c8943f913583d82d258efc9303f9601b979cf542
Michael Lukashov <michael.lukashov@gmail.com>
2011-07-09 14:38:25 +0400

    ﻿add Logging.[cpp|h]

---
+++ NetBox/Logging.h
@@ -0,0 +1,37 @@
+/**************************************************************************
+ *  NetBox plugin for FAR 2.0 (http://code.google.com/p/farplugs)         *
+ *  Copyright (C) 2011 by Artem Senichev <artemsen@gmail.com>             *
+ *  Copyright (C) 2011 by Michael Lukashov <michael.lukashov@gmail.com>   *
+ *                                                                        *
+ *  This program is free software: you can redistribute it and/or modify  *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful,       *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ **************************************************************************/
+
+#pragma once
+
+
+//! logging
+class CLogger
+{
+public:
+    CLogger();
+
+    /**
+     * Initialize logger
+     */
+    static void Initialize();
+
+private:
+};
+
+extern CLogger _Logger;
