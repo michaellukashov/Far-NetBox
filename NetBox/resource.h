a9ca18b69f56e49c7442ebcd44f13436e4447451
Michael Lukashov <michael.lukashov@gmail.com>
2011-07-11 22:55:52 +0400

    ﻿ version 1.15

--- NetBox/resource.h
+++ NetBox/resource.h
@@ -1,18 +1,5 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by DiskMenuEditor.rc
+#pragma once
 
-// Next default values for new objects
-//
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
-
-#define PLUGIN_VERSION_NUM 1,14,0,0
-#define PLUGIN_VERSION_TXT "1.14"
-#define PLUGIN_VERSION_WTXT L"1.14"
+#define PLUGIN_VERSION_NUM 1,15,0,0
+#define PLUGIN_VERSION_TXT "1.15"
+#define PLUGIN_VERSION_WTXT L"1.15"
