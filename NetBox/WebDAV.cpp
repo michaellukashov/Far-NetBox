81ad567eef16c50f63150edea3b39372fef2e387
Michael Lukashov <michael.lukashov@gmail.com>
2011-07-19 16:27:17 +0400

    ﻿ Log2 --> DEBUG_PRINTF

--- NetBox/WebDAV.cpp
+++ NetBox/WebDAV.cpp
@@ -57,7 +57,7 @@ bool CWebDAV::Connect(HANDLE abortEvent, wstring &errorInfo)
     assert(abortEvent);
 
     const wchar_t *url = _Session.GetURL();
-    Log2(L"WebDAV: connecting to %s", url);
+    DEBUG_PRINTF(L"NetBox: WebDAV: connecting to %s", url);
     //Initialize curl
     _CURL.Initialize(url, _Session.GetUserName(), _Session.GetPassword(),
         _Session.GetProxySettings());
