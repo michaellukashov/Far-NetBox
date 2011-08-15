1a1550f26c9c63893fbc7c3fae67c611d469fea7
Michael Lukashov <michael.lukashov@gmail.com>
2011-08-15 13:36:09 +0400

    ﻿TConfiguration

--- Common/Configuration.h
+++ Common/Configuration.h
@@ -53,75 +53,22 @@ private:
   bool FForceBanners;
   bool FDisableAcceptingHostKeys;
 
-  wstring GetOSVersionStr();
   // TVSFixedFileInfo *GetFixedApplicationInfo();
-  void * GetApplicationInfo();
-  virtual wstring GetVersionStr();
-  virtual wstring GetVersion();
-  wstring GetProductVersion();
-  wstring GetProductName();
-  wstring GetCompanyName();
   wstring TrimVersion(wstring Version);
-  wstring GetStoredSessionsSubKey();
-  wstring GetPuttySessionsKey();
-  void SetRandomSeedFile(wstring value);
-  wstring GetRandomSeedFileName();
-  void SetPuttyRegistryStorageKey(wstring value);
-  wstring GetSshHostKeysSubKey();
-  wstring GetRootKeyStr();
-  wstring GetConfigurationSubKey();
-  TEOLType GetLocalEOLType();
-  void SetLogging(bool value);
-  void SetLogFileName(wstring value);
-  void SetLogToFile(bool value);
-  bool GetLogToFile();
-  void SetLogWindowLines(int value);
-  void SetLogWindowComplete(bool value);
-  bool GetLogWindowComplete();
-  void SetLogFileAppend(bool value);
-  void SetLogProtocol(int value);
-  void SetLogActions(bool value);
-  wstring GetDefaultLogFileName();
-  wstring GetTimeFormat();
-  void SetStorage(TStorage value);
-  wstring GetRegistryStorageKey();
-  wstring GetIniFileStorageName();
-  void SetIniFileStorageName(wstring value);
-  wstring GetPartialExt() const;
-  wstring GetFileInfoString(const wstring Key);
-  bool GetGSSAPIInstalled();
-  void SetSessionReopenAuto(int value);
-  void SetSessionReopenBackground(int value);
-  void SetSessionReopenTimeout(int value);
-  void SetTunnelLocalPortNumberLow(int value);
-  void SetTunnelLocalPortNumberHigh(int value);
-  void SetCacheDirectoryChangesMaxSize(int value);
-  void SetShowFtpWelcomeMessage(bool value);
-  int GetCompoundVersion();
   void UpdateActualLogProtocol();
 
 protected:
   TStorage FStorage;
   TCriticalSection * FCriticalSection;
 
-  virtual TStorage GetStorage();
   virtual void Changed();
   virtual void SaveData(THierarchicalStorage * Storage, bool All);
   virtual void LoadData(THierarchicalStorage * Storage);
   virtual void CopyData(THierarchicalStorage * Source, THierarchicalStorage * Target);
   virtual void LoadAdmin(THierarchicalStorage * Storage);
-  virtual wstring GetDefaultKeyFile();
   virtual void Saved();
   void CleanupRegistry(wstring CleanupSubKey);
   wstring BannerHash(const wstring & Banner);
-
-  virtual bool GetConfirmOverwriting();
-  virtual void SetConfirmOverwriting(bool value);
-  bool GetConfirmResume();
-  void SetConfirmResume(bool value);
-  bool GetAutoReadDirectoryAfterOp();
-  void SetAutoReadDirectoryAfterOp(bool value);
-  virtual bool GetRememberPassword();
 
   virtual wstring ModuleFileName();
 
@@ -132,9 +79,13 @@ protected:
   wstring GetFileProductName(const wstring FileName);
   wstring GetFileCompanyName(const wstring FileName);
 
-  __property bool PermanentLogging  = { read=FPermanentLogging, write=SetLogging };
-  __property wstring PermanentLogFileName  = { read=FPermanentLogFileName, write=SetLogFileName };
-  __property bool PermanentLogActions  = { read=FPermanentLogActions, write=SetLogActions };
+  // __property bool PermanentLogging  = { read=FPermanentLogging, write=SetLogging };
+  bool GetPermanentLogging() { return FPermanentLogging; }
+  void SetLogging(bool value);
+    // __property wstring PermanentLogFileName  = { read=FPermanentLogFileName, write=SetLogFileName };
+  wstring GetPermanentLogFileName() { return FPermanentLogFileName; }
+  // __property bool PermanentLogActions  = { read=FPermanentLogActions, write=SetLogActions };
+  bool GetPermanentLogActions() { return FPermanentLogActions; }
 
 public:
   TConfiguration();
@@ -162,58 +113,135 @@ public:
   virtual wstring StronglyRecryptPassword(wstring Password, wstring Key);
 
   // __property TVSFixedFileInfo *FixedApplicationInfo  = { read=GetFixedApplicationInfo };
-  __property void * ApplicationInfo  = { read=GetApplicationInfo };
-  __property wstring StoredSessionsSubKey = {read=GetStoredSessionsSubKey};
-  __property wstring PuttyRegistryStorageKey  = { read=FPuttyRegistryStorageKey, write=SetPuttyRegistryStorageKey };
-  __property wstring PuttySessionsKey  = { read=GetPuttySessionsKey };
-  __property wstring RandomSeedFile  = { read=FRandomSeedFile, write=SetRandomSeedFile };
-  __property wstring RandomSeedFileName  = { read=GetRandomSeedFileName };
-  __property wstring SshHostKeysSubKey  = { read=GetSshHostKeysSubKey };
-  __property wstring RootKeyStr  = { read=GetRootKeyStr };
-  __property wstring ConfigurationSubKey  = { read=GetConfigurationSubKey };
-  __property TEOLType LocalEOLType = { read = GetLocalEOLType };
-  __property wstring VersionStr = { read=GetVersionStr };
-  __property wstring Version = { read=GetVersion };
-  __property int CompoundVersion = { read=GetCompoundVersion };
-  __property wstring ProductVersion = { read=GetProductVersion };
-  __property wstring ProductName = { read=GetProductName };
-  __property wstring CompanyName = { read=GetCompanyName };
-  __property wstring FileInfoString[wstring Key] = { read = GetFileInfoString };
-  __property wstring OSVersionStr = { read = GetOSVersionStr };
-  __property bool Logging  = { read=FLogging, write=SetLogging };
-  __property wstring LogFileName  = { read=FLogFileName, write=SetLogFileName };
-  __property bool LogToFile  = { read=GetLogToFile, write=SetLogToFile };
-  __property bool LogFileAppend  = { read=FLogFileAppend, write=SetLogFileAppend };
-  __property int LogProtocol  = { read=FLogProtocol, write=SetLogProtocol };
-  __property int ActualLogProtocol  = { read=FActualLogProtocol };
-  __property bool LogActions  = { read=FLogActions, write=SetLogActions };
-  __property int LogWindowLines  = { read=FLogWindowLines, write=SetLogWindowLines };
-  __property bool LogWindowComplete  = { read=GetLogWindowComplete, write=SetLogWindowComplete };
-  __property wstring DefaultLogFileName  = { read=GetDefaultLogFileName };
-  __property TNotifyEvent OnChange = { read = FOnChange, write = FOnChange };
-  __property bool ConfirmOverwriting = { read = GetConfirmOverwriting, write = SetConfirmOverwriting};
-  __property bool ConfirmResume = { read = GetConfirmResume, write = SetConfirmResume};
-  __property bool AutoReadDirectoryAfterOp = { read = GetAutoReadDirectoryAfterOp, write = SetAutoReadDirectoryAfterOp};
-  __property bool RememberPassword = { read = GetRememberPassword };
-  __property wstring PartialExt = {read=GetPartialExt};
-  __property int SessionReopenAuto = { read = FSessionReopenAuto, write = SetSessionReopenAuto };
-  __property int SessionReopenBackground = { read = FSessionReopenBackground, write = SetSessionReopenBackground };
-  __property int SessionReopenTimeout = { read = FSessionReopenTimeout, write = SetSessionReopenTimeout };
-  __property int TunnelLocalPortNumberLow = { read = FTunnelLocalPortNumberLow, write = SetTunnelLocalPortNumberLow };
-  __property int TunnelLocalPortNumberHigh = { read = FTunnelLocalPortNumberHigh, write = SetTunnelLocalPortNumberHigh };
-  __property int CacheDirectoryChangesMaxSize = { read = FCacheDirectoryChangesMaxSize, write = SetCacheDirectoryChangesMaxSize };
-  __property bool ShowFtpWelcomeMessage = { read = FShowFtpWelcomeMessage, write = SetShowFtpWelcomeMessage };
+  // __property void * ApplicationInfo  = { read=GetApplicationInfo };
+  void * GetApplicationInfo();
+  // __property wstring StoredSessionsSubKey = {read=GetStoredSessionsSubKey};
+  wstring GetStoredSessionsSubKey();
+  // __property wstring PuttyRegistryStorageKey  = { read=FPuttyRegistryStorageKey, write=SetPuttyRegistryStorageKey };
+  wstring GetPuttyRegistryStorageKey() { return FPuttyRegistryStorageKey; }
+  void SetPuttyRegistryStorageKey(wstring value);
+  // __property wstring PuttySessionsKey  = { read=GetPuttySessionsKey };
+  wstring GetPuttySessionsKey();
+  // __property wstring RandomSeedFile  = { read=FRandomSeedFile, write=SetRandomSeedFile };
+  wstring GetRandomSeedFile() { return FRandomSeedFile; }
+  void SetRandomSeedFile(wstring value);
+  // __property wstring RandomSeedFileName  = { read=GetRandomSeedFileName };
+  wstring GetRandomSeedFileName();
+  // __property wstring SshHostKeysSubKey  = { read=GetSshHostKeysSubKey };
+  wstring GetSshHostKeysSubKey();
+  // __property wstring RootKeyStr  = { read=GetRootKeyStr };
+  wstring GetRootKeyStr();
+  // __property wstring ConfigurationSubKey  = { read=GetConfigurationSubKey };
+  wstring GetConfigurationSubKey();
+  // __property TEOLType LocalEOLType = { read = GetLocalEOLType };
+  TEOLType GetLocalEOLType();
+  // __property wstring VersionStr = { read=GetVersionStr };
+  virtual wstring GetVersionStr();
+  // __property wstring Version = { read=GetVersion };
+  virtual wstring GetVersion();
+  // __property int CompoundVersion = { read=GetCompoundVersion };
+  int GetCompoundVersion();
+  // __property wstring ProductVersion = { read=GetProductVersion };
+  wstring GetProductVersion();
+  // __property wstring ProductName = { read=GetProductName };
+  wstring GetProductName();
+  // __property wstring CompanyName = { read=GetCompanyName };
+  wstring GetCompanyName();
+  // __property wstring FileInfoString[wstring Key] = { read = GetFileInfoString };
+  wstring GetFileInfoString(const wstring Key);
+  // __property wstring OSVersionStr = { read = GetOSVersionStr };
+  wstring GetOSVersionStr();
+  // __property bool Logging  = { read=FLogging, write=SetLogging };
+  bool GetLogging() { return FLogging; }
+  // __property wstring LogFileName  = { read=FLogFileName, write=SetLogFileName };
+  wstring GetLogFileName() { return FLogFileName; }
+  void SetLogFileName(wstring value);
+  // __property bool LogToFile  = { read=GetLogToFile, write=SetLogToFile };
+  bool GetLogToFile();
+  void SetLogToFile(bool value);
+  // __property bool LogFileAppend  = { read=FLogFileAppend, write=SetLogFileAppend };
+  bool GetLogFileAppend() { return FLogFileAppend; }
+  void SetLogFileAppend(bool value);
+  // __property int LogProtocol  = { read=FLogProtocol, write=SetLogProtocol };
+  int GetLogProtocol() { return FLogProtocol; }
+  void SetLogProtocol(int value);
+  // __property int ActualLogProtocol  = { read=FActualLogProtocol };
+  int GetActualLogProtocol() { return FActualLogProtocol; }
+  // __property bool LogActions  = { read=FLogActions, write=SetLogActions };
+  bool GetLogActions() { return FLogActions; }
+  void SetLogActions(bool value);
+  // __property int LogWindowLines  = { read=FLogWindowLines, write=SetLogWindowLines };
+  int GetLogWindowLines() { return FLogWindowLines; }
+  void SetLogWindowLines(int value);
+  // __property bool LogWindowComplete  = { read=GetLogWindowComplete, write=SetLogWindowComplete };
+  bool GetLogWindowComplete();
+  void SetLogWindowComplete(bool value);
+  // __property wstring DefaultLogFileName  = { read=GetDefaultLogFileName };
+  wstring GetDefaultLogFileName();
+  // __property TNotifyEvent OnChange = { read = FOnChange, write = FOnChange };
+  TNotifyEvent GetOnChange() { return FOnChange; }
+  void SetOnChange(TNotifyEvent value) { FOnChange = value; }
+  // __property bool ConfirmOverwriting = { read = GetConfirmOverwriting, write = SetConfirmOverwriting};
+  virtual bool GetConfirmOverwriting();
+  virtual void SetConfirmOverwriting(bool value);
+  // __property bool ConfirmResume = { read = GetConfirmResume, write = SetConfirmResume};
+  bool GetConfirmResume();
+  void SetConfirmResume(bool value);
+  // __property bool AutoReadDirectoryAfterOp = { read = GetAutoReadDirectoryAfterOp, write = SetAutoReadDirectoryAfterOp};
+  bool GetAutoReadDirectoryAfterOp();
+  void SetAutoReadDirectoryAfterOp(bool value);
+  // __property bool RememberPassword = { read = GetRememberPassword };
+  virtual bool GetRememberPassword();
+  // __property wstring PartialExt = {read=GetPartialExt};
+  wstring GetPartialExt() const;
+  // __property int SessionReopenAuto = { read = FSessionReopenAuto, write = SetSessionReopenAuto };
+  int GetSessionReopenAuto() { return FSessionReopenAuto; }
+  void SetSessionReopenAuto(int value);
+  // __property int SessionReopenBackground = { read = FSessionReopenBackground, write = SetSessionReopenBackground };
+  int GetSessionReopenBackground() { return FSessionReopenBackground; }
+  void SetSessionReopenBackground(int value);
+  // __property int SessionReopenTimeout = { read = FSessionReopenTimeout, write = SetSessionReopenTimeout };
+  int GetSessionReopenTimeout() { return FSessionReopenTimeout; }
+  void SetSessionReopenTimeout(int value);
+  // __property int TunnelLocalPortNumberLow = { read = FTunnelLocalPortNumberLow, write = SetTunnelLocalPortNumberLow };
+  int GetTunnelLocalPortNumberLow() { return FTunnelLocalPortNumberLow; }
+  void SetTunnelLocalPortNumberLow(int value);
+  // __property int TunnelLocalPortNumberHigh = { read = FTunnelLocalPortNumberHigh, write = SetTunnelLocalPortNumberHigh };
+  int GetTunnelLocalPortNumberHigh() { return FTunnelLocalPortNumberHigh; }
+  void SetTunnelLocalPortNumberHigh(int value);
+  // __property int CacheDirectoryChangesMaxSize = { read = FCacheDirectoryChangesMaxSize, write = SetCacheDirectoryChangesMaxSize };
+  int GetCacheDirectoryChangesMaxSize() { return FCacheDirectoryChangesMaxSize; }
+  void SetCacheDirectoryChangesMaxSize(int value);
+  // __property bool ShowFtpWelcomeMessage = { read = FShowFtpWelcomeMessage, write = SetShowFtpWelcomeMessage };
+  bool GetShowFtpWelcomeMessage() { return FShowFtpWelcomeMessage; }
+  void SetShowFtpWelcomeMessage(bool value);
 
-  __property wstring TimeFormat = { read = GetTimeFormat };
-  __property TStorage Storage  = { read=GetStorage, write=SetStorage };
-  __property wstring RegistryStorageKey  = { read=GetRegistryStorageKey };
-  __property wstring IniFileStorageName  = { read=GetIniFileStorageName, write=SetIniFileStorageName };
-  __property wstring DefaultKeyFile = { read = GetDefaultKeyFile };
+  // __property wstring TimeFormat = { read = GetTimeFormat };
+  wstring GetTimeFormat();
+  // __property TStorage Storage  = { read=GetStorage, write=SetStorage };
+  virtual TStorage GetStorage();
+  void SetStorage(TStorage value);
+  // __property wstring RegistryStorageKey  = { read=GetRegistryStorageKey };
+  wstring GetRegistryStorageKey();
+  // __property wstring IniFileStorageName  = { read=GetIniFileStorageName, write=SetIniFileStorageName };
+  wstring GetIniFileStorageName();
+  void SetIniFileStorageName(wstring value);
+  // __property wstring DefaultKeyFile = { read = GetDefaultKeyFile };
+  virtual wstring GetDefaultKeyFile();
 
-  __property bool DisablePasswordStoring = { read = FDisablePasswordStoring };
-  __property bool ForceBanners = { read = FForceBanners };
-  __property bool DisableAcceptingHostKeys = { read = FDisableAcceptingHostKeys };
-  __property bool GSSAPIInstalled = { read = GetGSSAPIInstalled };
+  // __property bool DisablePasswordStoring = { read = FDisablePasswordStoring };
+  bool GetDisablePasswordStoring() { return FDisablePasswordStoring; }
+  // __property bool ForceBanners = { read = FForceBanners };
+  bool GetForceBanners() { return FForceBanners; }
+  // __property bool DisableAcceptingHostKeys = { read = FDisableAcceptingHostKeys };
+  bool GetDisableAcceptingHostKeys() { return FDisableAcceptingHostKeys; }
+  // __property bool GSSAPIInstalled = { read = GetGSSAPIInstalled };
+  bool GetGSSAPIInstalled();
+};
+//---------------------------------------------------------------------------
+// FIXME
+class TShortCut
+{
 };
 //---------------------------------------------------------------------------
 class TShortCuts
