d8621d34be9acffbe477b273ef437c457b833726
Michael Lukashov <michael.lukashov@gmail.com>
2011-08-25 13:25:37 +0400

    ﻿#include "stdafx.h"

--- Common/Exceptions.h
+++ Common/Exceptions.h
@@ -1,6 +1,7 @@
 //---------------------------------------------------------------------------
 #pragma once
 
+#include "stdafx.h"
 #include "Classes.h"
 // #include <SysUtils.hpp>
 // #include <SysInit.hpp>
