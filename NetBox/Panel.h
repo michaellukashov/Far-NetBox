8e2682d8ceca615d67db47b5bda7209035b8d675
Michael Lukashov <michael.lukashov@gmail.com>
2011-08-26 19:04:32 +0400

    ﻿move from stdafx to FarUtil

--- NetBox/Panel.h
+++ NetBox/Panel.h
@@ -17,12 +17,14 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>. *
  **************************************************************************/
 
+#pragma once
+
+#include "FarUtil.h"
 #include "Settings.h"
 #include "Protocol.h"
 #include "Strings.h"
 #include "SessionManager.h"
 
-#pragma once
 
 
 class CPanel
