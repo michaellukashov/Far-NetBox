10b97957000df411d73c40c0ad9378e1e45bc0bf
Michael Lukashov <michael.lukashov@gmail.com>
2011-09-01 01:35:09 +0400

    ﻿convert from C++ Builder to VC++

--- NetBox/WinSCPFileSystem.h
+++ NetBox/WinSCPFileSystem.h
@@ -142,7 +142,9 @@ protected:
   void ToggleSynchronizeBrowsing();
   bool IsSynchronizedBrowsing();
   bool PropertiesDialog(TStrings * FileList,
-    const wstring Directory, const TRemoteTokenList *GroupList, const TRemoteTokenList *UserList,
+    const wstring Directory, 
+    // const TRemoteTokenList *GroupList, const TRemoteTokenList *UserList,
+    TStrings * GroupList, TStrings * UserList,
     TRemoteProperties * Properties, int AllowedChanges);
   bool ExecuteCommand(const wstring Command);
   void TerminalCaptureLog(const wstring & AddedLine, bool StdError);
