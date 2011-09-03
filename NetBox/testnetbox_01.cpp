ff36333995b531f6d4a6c7799e5850aa5c6b39a5
Michael Lukashov <michael.lukashov@gmail.com>
2011-09-03 15:11:51 +0400

    ﻿testnetbox_01.cpp: // #define BOOST_TEST_DYN_LINK

--- NetBox/testnetbox_01.cpp
+++ NetBox/testnetbox_01.cpp
@@ -10,7 +10,7 @@
 #define BOOST_TEST_MODULE "testnetbox_01"
 #define BOOST_TEST_MAIN
 #ifdef _WIN32
-#define BOOST_TEST_DYN_LINK
+// #define BOOST_TEST_DYN_LINK
 #endif
 #include <boost/test/unit_test.hpp>
 
