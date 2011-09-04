41b5a1773fd57137c6dae44093c9a0256575dd9b
Michael Lukashov <michael.lukashov@gmail.com>
2011-09-04 10:42:06 +0400

    ﻿ add test for strftime2

--- NetBox/testnetbox_01.cpp
+++ NetBox/testnetbox_01.cpp
@@ -5,6 +5,7 @@
 //------------------------------------------------------------------------------
 
 #include "stdafx.h"
+#include <time.h>
 
 #include "boostdefines.hpp"
 #define BOOST_TEST_MODULE "testnetbox_01"
@@ -14,7 +15,8 @@
 #endif
 #include <boost/test/unit_test.hpp>
 
-#include "stdafx.h"
+#include "winstuff.h"
+#include "puttyexp.h"
 #include "FarUtil.h"
 
 using namespace boost::unit_test;
@@ -41,6 +43,15 @@ BOOST_FIXTURE_TEST_CASE(test1, base_fixture_t)
     std::wstring path = L"C:\\test";
     AppendPathDelimiterW(path);
     BOOST_CHECK(path == L"C:\\test\\");
+}
+
+BOOST_FIXTURE_TEST_CASE(test2, base_fixture_t)
+{
+    char buf[256];
+    struct tm tm = ltime();
+    strftime2(buf, sizeof(buf) - 1, "%Y.%m.%d %H:%M:%S", &tm);
+    BOOST_TEST_MESSAGE("buf = %s" << buf);
+    // BOOST_CHECK(buf == "2011.09.04 10:07:37");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
