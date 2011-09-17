fc874e82c4ec1db4f67691d3e8ef2473499616ad
Michael Lukashov <michael.lukashov@gmail.com>
2011-09-17 15:41:43 +0400

    ﻿testnetbox_01

--- NetBox/testnetbox_01.cpp
+++ NetBox/testnetbox_01.cpp
@@ -47,34 +47,52 @@ BOOST_AUTO_TEST_SUITE(testnetbox_01)
 
 BOOST_FIXTURE_TEST_CASE(test1, base_fixture_t)
 {
+    std::wstring str;
     if (1)
     {
         TStringList strings;
         BOOST_CHECK_EQUAL(0, strings.GetCount());
-        strings.Add(L"line 1");
+        BOOST_CHECK_EQUAL(0, strings.Add(L"line 1"));
         BOOST_CHECK_EQUAL(1, strings.GetCount());
         {
-            std::wstring str = strings.GetString(0);
+            str = strings.GetString(0);
+            // DEBUG_PRINTF(L"str = %s", str.c_str());
             BOOST_CHECK_EQUAL(::W2MB(str.c_str()), "line 1");
         }
         strings.PutString(0, L"line 0");
         {
-            std::wstring str = strings.GetString(0);
+            str = strings.GetString(0);
             BOOST_CHECK_EQUAL(::W2MB(str.c_str()), "line 0");
         }
+        {
+            strings.SetString(0, L"line 00");
+            BOOST_CHECK_EQUAL(::W2MB(strings.GetString(0).c_str()), "line 00");
+        }
+        {
+            strings.SetString(1, L"line 11");
+            BOOST_CHECK_EQUAL(::W2MB(strings.GetString(1).c_str()), "line 11");
+        }
     }
-    if (0)
+    TStringList strings;
+    if (1)
     {
-        TStringList strings;
         BOOST_CHECK_EQUAL(0, strings.GetCount());
         strings.Add(L"line 1");
-        BOOST_CHECK_EQUAL(::W2MB(strings.GetText().c_str()).c_str(), "line 1");
+        str = strings.GetText();
+        // DEBUG_PRINTF(L"str = %s", str.c_str());
+        BOOST_CHECK_EQUAL(::W2MB(str.c_str()).c_str(), "line 1\n");
+    }
+    if (1)
+    {
         strings.Add(L"line 2");
         BOOST_CHECK_EQUAL(2, strings.GetCount());
-        BOOST_CHECK_EQUAL(::W2MB(strings.GetText().c_str()).c_str(), "line 1\nline 2");
+        str = strings.GetText();
+        // DEBUG_PRINTF(L"str = %s", str.c_str());
+        BOOST_CHECK_EQUAL(::W2MB(str.c_str()).c_str(), "line 1\nline 2\n");
         strings.Insert(0, L"line 0");
         BOOST_CHECK_EQUAL(3, strings.GetCount());
-        BOOST_CHECK_EQUAL(::W2MB(strings.GetText().c_str()).c_str(), "line 0\nline 1\nline 2");
+        str = strings.GetText();
+        BOOST_CHECK_EQUAL(::W2MB(str.c_str()).c_str(), "line 0\nline 1\nline 2\n");
         strings.PutObject(0, NULL);
         std::wstring str = strings.GetString(0);
         BOOST_CHECK_EQUAL(::W2MB(str.c_str()), "line 0");
