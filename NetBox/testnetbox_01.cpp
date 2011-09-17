837f27b9b0e37df30fa3b4024fb9c092d385d677
Michael Lukashov <michael.lukashov@gmail.com>
2011-09-17 21:49:34 +0400

    testnetbox_01: tests for TList

--- NetBox/testnetbox_01.cpp
+++ NetBox/testnetbox_01.cpp
@@ -47,6 +47,60 @@ BOOST_AUTO_TEST_SUITE(testnetbox_01)
 
 BOOST_FIXTURE_TEST_CASE(test1, base_fixture_t)
 {
+    TList list;
+    BOOST_CHECK_EQUAL(0, list.GetCount());
+    TObject obj1;
+    TObject obj2;
+    if (1)
+    {
+        list.Add(&obj1);
+        BOOST_CHECK_EQUAL(1, list.GetCount());
+        list.Add(&obj2);
+        BOOST_CHECK_EQUAL(2, list.GetCount());
+        BOOST_CHECK_EQUAL(0, list.IndexOf(&obj1));
+        BOOST_CHECK_EQUAL(1, list.IndexOf(&obj2));
+    }
+    list.Clear();
+    if (1)
+    {
+        BOOST_CHECK_EQUAL(0, list.GetCount());
+        list.Insert(0, &obj1);
+        BOOST_CHECK_EQUAL(1, list.GetCount());
+        list.Insert(0, &obj2);
+        BOOST_CHECK_EQUAL(2, list.GetCount());
+        BOOST_CHECK_EQUAL(1, list.IndexOf(&obj1));
+        BOOST_CHECK_EQUAL(0, list.IndexOf(&obj2));
+    }
+    if (1)
+    {
+        list.Delete(1);
+        BOOST_CHECK_EQUAL(1, list.GetCount());
+        BOOST_CHECK_EQUAL(-1, list.IndexOf(&obj1));
+        BOOST_CHECK_EQUAL(0, list.IndexOf(&obj2));
+        BOOST_CHECK_EQUAL(1, list.Add(&obj1));
+        list.Delete(0);
+        BOOST_CHECK_EQUAL(0, list.IndexOf(&obj1));
+        BOOST_CHECK_EQUAL(0, list.Remove(&obj1));
+        BOOST_CHECK_EQUAL(-1, list.IndexOf(&obj1));
+        BOOST_CHECK_EQUAL(0, list.GetCount());
+    }
+    if (1)
+    {
+        list.Add(&obj1);
+        list.Add(&obj2);
+        list.Extract(&obj1);
+        BOOST_CHECK_EQUAL(1, list.GetCount());
+        BOOST_CHECK_EQUAL(0, list.IndexOf(&obj2));
+        list.Add(&obj1);
+        BOOST_CHECK_EQUAL(2, list.GetCount());
+        list.Move(0, 1);
+        BOOST_CHECK_EQUAL(0, list.IndexOf(&obj1));
+        BOOST_CHECK_EQUAL(1, list.IndexOf(&obj2));
+    }
+}
+
+BOOST_FIXTURE_TEST_CASE(test2, base_fixture_t)
+{
     std::wstring str;
     if (1)
     {
@@ -96,7 +150,7 @@ BOOST_FIXTURE_TEST_CASE(test1, base_fixture_t)
     }
 }
 
-BOOST_FIXTURE_TEST_CASE(test2, base_fixture_t)
+BOOST_FIXTURE_TEST_CASE(test3, base_fixture_t)
 {
     std::wstring Text = L"text text text text text1\ntext text text text text2\n";
     TStringList Lines;
@@ -108,8 +162,20 @@ BOOST_FIXTURE_TEST_CASE(test2, base_fixture_t)
     BOOST_CHECK_EQUAL("text text text text text2", ::W2MB(Lines.GetString(1).c_str()).c_str());
 }
 
-BOOST_FIXTURE_TEST_CASE(test3, base_fixture_t)
+BOOST_FIXTURE_TEST_CASE(test4, base_fixture_t)
 {
+    std::wstring Text = L"text, text text, text text1\ntext text text, text text2\n";
+    TStringList Lines;
+    Lines.SetCommaText(Text);
+    BOOST_CHECK_EQUAL(6, Lines.GetCount());
+    BOOST_CHECK_EQUAL("text", ::W2MB(Lines.GetString(0).c_str()).c_str());
+    BOOST_CHECK_EQUAL(" text text", ::W2MB(Lines.GetString(1).c_str()).c_str());
+    BOOST_CHECK_EQUAL(" text text1", ::W2MB(Lines.GetString(2).c_str()).c_str());
+    BOOST_CHECK_EQUAL("text text text", ::W2MB(Lines.GetString(3).c_str()).c_str());
+    BOOST_CHECK_EQUAL(" text text2", ::W2MB(Lines.GetString(4).c_str()).c_str());
+    std::wstring Text2 = Lines.GetCommaText();
+    BOOST_TEST_MESSAGE("Text2 = " << ::W2MB(Text2.c_str()));
+    BOOST_CHECK_EQUAL("\"text\",\" text text\",\" text text1\",\"text text text\",\" text text2\",\"\"", ::W2MB(Text2.c_str()).c_str());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
