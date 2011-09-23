916303928ca29df7dbbbf6afb3b40be0759d6c12
Michael Lukashov <michael.lukashov@gmail.com>
2011-09-24 00:38:24 +0400

    ﻿testutils.h

---
+++ NetBox/testutils.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "stdafx.h"
+#include <time.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+
+#define TEST_CASE_TODO(exp) \
+    std::cerr << "TODO: " << #exp << std::endl
+
