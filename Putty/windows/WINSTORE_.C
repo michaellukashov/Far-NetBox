5c4ea0d6c971fab07b048689c151e8518e7b3c4b
Michael Lukashov <michael.lukashov@gmail.com>
2011-09-25 19:09:15 +0400

    ﻿WINSTORE_: remove #define RegOpenKeyA reg_open_winscp_key

--- Putty/windows/WINSTORE_.C
+++ Putty/windows/WINSTORE_.C
@@ -1,12 +1,12 @@
 #include "puttyexp.h"
 
 #pragma warning(disable:4005)
-#define RegOpenKeyA reg_open_winscp_key
-#define RegCreateKeyA reg_create_winscp_key
-#define RegCreateKeyA reg_create_winscp_key
-#define RegQueryValueExA reg_query_winscp_value_ex
-#define RegSetValueExA reg_set_winscp_value_ex
-#define RegCloseKey reg_close_winscp_key
+// #define RegOpenKeyA reg_open_winscp_key
+// #define RegCreateKeyA reg_create_winscp_key
+// #define RegCreateKeyA reg_create_winscp_key
+// #define RegQueryValueExA reg_query_winscp_value_ex
+// #define RegSetValueExA reg_set_winscp_value_ex
+// #define RegCloseKey reg_close_winscp_key
 #pragma warning(pop)
 
 #include "winstore.c"
