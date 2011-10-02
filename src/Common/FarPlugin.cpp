016b1fb0dbcc95fda254940e7e9b4d244c35ea22
Michael Lukashov <michael.lukashov@gmail.com>
2011-10-03 00:54:32 +0400

    ﻿TCustomFarPlugin::Menu

--- src/Common/FarPlugin.cpp
+++ src/Common/FarPlugin.cpp
@@ -1199,7 +1199,8 @@ int TCustomFarPlugin::Menu(unsigned int Flags, const std::wstring Title,
             if (FLAGCLEAR(Flags, MIF_HIDDEN))
             {
                 memset(&MenuItems[Count], 0, sizeof(MenuItems[Count]));
-                std::wstring Text = Items->GetString(i).substr(1, sizeof(MenuItems[i].Text)-1);
+                std::wstring Text = Items->GetString(i); // .substr(1, sizeof(MenuItems[i].Text)-1);
+                DEBUG_PRINTF(L"Text = %s", Text.c_str());
                 MenuItems[Count].Flags = Flags;
                 if (MenuItems[Count].Flags & MIF_SELECTED)
                 {
@@ -1212,6 +1213,7 @@ int TCustomFarPlugin::Menu(unsigned int Flags, const std::wstring Title,
                 MenuItems[Count].Text = TCustomFarPlugin::DuplicateStr(Str);
                 MenuItems[Count].UserData = i;
                 Count++;
+                DEBUG_PRINTF(L"Count = %d", Count);
             }
         }
 
