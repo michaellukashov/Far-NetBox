3939571a53346f1b68f404b0d76ca11f1c495e05
Michael Lukashov <michael.lukashov@gmail.com>
2011-10-10 22:01:48 +0400

    Security: fix compilation

--- src/core/Security.cpp
+++ src/core/Security.cpp
@@ -1,99 +1,108 @@
 //---------------------------------------------------------------------------
-#include <vcl.h>
-#pragma hdrstop
+#include "stdafx.h"
 
 #include "Common.h"
 #include "Security.h"
 //---------------------------------------------------------------------------
-#pragma package(smart_init)
-//---------------------------------------------------------------------------
 #define PWALG_SIMPLE_INTERNAL 0x00
 #define PWALG_SIMPLE_EXTERNAL 0x01
 //---------------------------------------------------------------------------
-AnsiString SimpleEncryptChar(unsigned char Ch)
+int random(int range)
+{
+    return (double)rand() / ((double)RAND_MAX / (double)range);
+}
+
+//---------------------------------------------------------------------------
+std::string SimpleEncryptChar(unsigned char Ch)
 {
   Ch = (unsigned char)((~Ch) ^ PWALG_SIMPLE_MAGIC);
   return
-    PWALG_SIMPLE_STRING.SubString(((Ch & 0xF0) >> 4) + 1, 1) +
-    PWALG_SIMPLE_STRING.SubString(((Ch & 0x0F) >> 0) + 1, 1);
+    PWALG_SIMPLE_STRING.substr(((Ch & 0xF0) >> 4), 1) +
+    PWALG_SIMPLE_STRING.substr(((Ch & 0x0F) >> 0), 1);
 }
 //---------------------------------------------------------------------------
-unsigned char SimpleDecryptNextChar(AnsiString &Str)
+unsigned char SimpleDecryptNextChar(std::string &Str)
 {
-  if (Str.Length() > 0)
+  if (Str.size() > 0)
   {
     unsigned char Result = (unsigned char)
-      ~((((PWALG_SIMPLE_STRING.Pos(Str.c_str()[0])-1) << 4) +
-         ((PWALG_SIMPLE_STRING.Pos(Str.c_str()[1])-1) << 0)) ^ PWALG_SIMPLE_MAGIC);
-    Str.Delete(1, 2);
+      ~((((PWALG_SIMPLE_STRING.find_first_of(Str.c_str()[0])) << 4) +
+         ((PWALG_SIMPLE_STRING.find_first_of(Str.c_str()[1])) << 0)) ^ PWALG_SIMPLE_MAGIC);
+    Str.erase(0, 2);
     return Result;
   }
   else return 0x00;
 }
 //---------------------------------------------------------------------------
-AnsiString EncryptPassword(AnsiString Password, AnsiString Key, Integer /* Algorithm */)
+std::wstring EncryptPassword(std::wstring Password, std::wstring Key, int /* Algorithm */)
 {
-  AnsiString Result("");
+  std::string Result("");
   int Shift, Index;
 
-  if (!RandSeed) Randomize();
-  Password = Key + Password;
-  Shift = (Password.Length() < PWALG_SIMPLE_MAXLEN) ?
-    (unsigned char)random(PWALG_SIMPLE_MAXLEN - Password.Length()) : 0;
-  Result += SimpleEncryptChar((Char)PWALG_SIMPLE_FLAG); // Flag
-  Result += SimpleEncryptChar((Char)PWALG_SIMPLE_INTERNAL); // Dummy
-  Result += SimpleEncryptChar((Char)Password.Length());
-  Result += SimpleEncryptChar((Char)Shift);
+  // if (!RandSeed) Randomize();
+  std::string Password2 = ::W2MB((Key + Password).c_str());
+  Shift = (Password2.size() < PWALG_SIMPLE_MAXLEN) ?
+    (unsigned char)random(PWALG_SIMPLE_MAXLEN - Password2.size()) : 0;
+  // DEBUG_PRINTF(L"Shift = %d", Shift);
+  Result += SimpleEncryptChar((char)PWALG_SIMPLE_FLAG); // Flag
+  Result += SimpleEncryptChar((char)PWALG_SIMPLE_INTERNAL); // Dummy
+  Result += SimpleEncryptChar((char)Password2.size());
+  Result += SimpleEncryptChar((char)Shift);
   for (Index = 0; Index < Shift; Index++)
     Result += SimpleEncryptChar((unsigned char)random(256));
-  for (Index = 0; Index < Password.Length(); Index++)
-    Result += SimpleEncryptChar(Password.c_str()[Index]);
-  while (Result.Length() < PWALG_SIMPLE_MAXLEN * 2)
+  for (Index = 0; Index < Password2.size(); Index++)
+    Result += SimpleEncryptChar(Password2.c_str()[Index]);
+  while (Result.size() < PWALG_SIMPLE_MAXLEN * 2)
     Result += SimpleEncryptChar((unsigned char)random(256));
-  return Result;
+  return ::MB2W(Result.c_str());
 }
 //---------------------------------------------------------------------------
-AnsiString DecryptPassword(AnsiString Password, AnsiString Key, Integer /* Algorithm */)
+std::wstring DecryptPassword(std::wstring Password, std::wstring Key, int /* Algorithm */)
 {
-  AnsiString Result("");
-  Integer Index;
+  std::string Result("");
+  int Index;
   unsigned char Length, Flag;
-
-  Flag = SimpleDecryptNextChar(Password);
-  if (Flag == PWALG_SIMPLE_FLAG)
+  std::string Password2 = ::W2MB(Password.c_str());
+  std::string Key2 = ::W2MB(Key.c_str());
+  Flag = SimpleDecryptNextChar(Password2);
+  // DEBUG_PRINTF(L"Flag = %x, PWALG_SIMPLE_FLAG = %x", Flag, PWALG_SIMPLE_FLAG);
+  if (Flag == (unsigned char)PWALG_SIMPLE_FLAG)
   {
-    /* Dummy = */ SimpleDecryptNextChar(Password);
-    Length = SimpleDecryptNextChar(Password);
+    /* Dummy = */ SimpleDecryptNextChar(Password2);
+    Length = SimpleDecryptNextChar(Password2);
+    // DEBUG_PRINTF(L"Length = %d", Length);
   }
-    else Length = Flag;
-  Password.Delete(1, ((Integer)SimpleDecryptNextChar(Password))*2);
+  else Length = PWALG_SIMPLE_FLAG;
+  // DEBUG_PRINTF(L"Length = %d", Length);
+  Password2.erase(0, ((int)SimpleDecryptNextChar(Password2))*2);
   for (Index = 0; Index < Length; Index++)
-    Result += (char)SimpleDecryptNextChar(Password);
+    Result += (char)SimpleDecryptNextChar(Password2);
   if (Flag == PWALG_SIMPLE_FLAG)
   {
-    if (Result.SubString(1, Key.Length()) != Key) Result = "";
-      else Result.Delete(1, Key.Length());
+    if (Result.substr(0, Key.size()) != Key2) Result = "";
+      else Result.erase(0, Key2.size());
   }
-  return Result;
+  return ::MB2W(Result.c_str());
 }
 //---------------------------------------------------------------------------
-AnsiString SetExternalEncryptedPassword(AnsiString Password)
+std::wstring SetExternalEncryptedPassword(std::wstring Password)
 {
-  AnsiString Result;
-  Result += SimpleEncryptChar((Char)PWALG_SIMPLE_FLAG);
-  Result += SimpleEncryptChar((Char)PWALG_SIMPLE_EXTERNAL);
-  Result += StrToHex(Password);
-  return Result;
+  std::string Result;
+  Result += SimpleEncryptChar((char)PWALG_SIMPLE_FLAG);
+  Result += SimpleEncryptChar((char)PWALG_SIMPLE_EXTERNAL);
+  Result += ::W2MB(StrToHex(Password).c_str());
+  return ::MB2W(Result.c_str());
 }
 //---------------------------------------------------------------------------
-bool GetExternalEncryptedPassword(AnsiString Encrypted, AnsiString & Password)
+bool GetExternalEncryptedPassword(std::wstring Encrypted, std::wstring & Password)
 {
+  std::string Encrypted2 = ::W2MB(Encrypted.c_str());
   bool Result =
-    (SimpleDecryptNextChar(Encrypted) == PWALG_SIMPLE_FLAG) &&
-    (SimpleDecryptNextChar(Encrypted) == PWALG_SIMPLE_EXTERNAL);
+    (SimpleDecryptNextChar(Encrypted2) == PWALG_SIMPLE_FLAG) &&
+    (SimpleDecryptNextChar(Encrypted2) == PWALG_SIMPLE_EXTERNAL);
   if (Result)
   {
-    Password = HexToStr(Encrypted);
+    Password = ::HexToStr(::MB2W(Encrypted2.c_str()));
   }
   return Result;
 }
--- src/core/Security.h
+++ src/core/Security.h
@@ -4,12 +4,13 @@
 //---------------------------------------------------------------------------
 #define PWALG_SIMPLE 1
 #define PWALG_SIMPLE_MAGIC 0xA3
-#define PWALG_SIMPLE_STRING ((AnsiString)"0123456789ABCDEF")
+#define PWALG_SIMPLE_STRING ((std::string)"0123456789ABCDEF")
 #define PWALG_SIMPLE_MAXLEN 50
 #define PWALG_SIMPLE_FLAG 0xFF
-AnsiString EncryptPassword(AnsiString Password, AnsiString Key, Integer Algorithm = PWALG_SIMPLE);
-AnsiString DecryptPassword(AnsiString Password, AnsiString Key, Integer Algorithm = PWALG_SIMPLE);
-AnsiString SetExternalEncryptedPassword(AnsiString Password);
-bool GetExternalEncryptedPassword(AnsiString Encrypted, AnsiString & Password);
+int random(int range);
+std::wstring EncryptPassword(std::wstring Password, std::wstring Key, int Algorithm = PWALG_SIMPLE);
+std::wstring DecryptPassword(std::wstring Password, std::wstring Key, int Algorithm = PWALG_SIMPLE);
+std::wstring SetExternalEncryptedPassword(std::wstring Password);
+bool GetExternalEncryptedPassword(std::wstring Encrypted, std::wstring & Password);
 //---------------------------------------------------------------------------
 #endif
