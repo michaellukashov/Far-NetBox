f68496b8ad150041b9bcc49bd50b19c21b360555
Michael Lukashov <michael.lukashov@gmail.com>
2011-10-15 01:20:48 +0400

    ﻿testnetbox_02

--- src/NetBox/testnetbox_02.cpp
+++ src/NetBox/testnetbox_02.cpp
@@ -274,12 +274,16 @@ BOOST_FIXTURE_TEST_CASE(test8, base_fixture_t)
 {
    std::wstring RootKey = L"Software\\Michael Lukashov\\TestNetBox";
    TRegistryStorage Storage(RootKey);
+   Storage.SetAccessMode(smReadWrite);
    BOOST_CHECK(Storage.OpenRootKey(true));
    std::wstring SubKey = L"SubKey1";
    Storage.DeleteSubKey(SubKey);
    BOOST_CHECK(!Storage.KeyExists(SubKey));
    BOOST_CHECK(Storage.OpenSubKey(SubKey, true));
+   Storage.SetAccessMode(smReadWrite);
    Storage.Writeint(L"IntVal", 1234);
+   // BOOST_TEST_MESSAGE("Storage.GetFailed = " << Storage.GetFailed());
+   Storage.CloseSubKey();
    BOOST_CHECK(Storage.KeyExists(SubKey));
 }
 
