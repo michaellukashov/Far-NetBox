80fc6de99c08e5272890286661e7a2dbc1f6878b
Michael Lukashov <michael.lukashov@gmail.com>
2011-12-10 13:57:20 +0300

    ﻿ #define CRTDBG_MAP_ALLOC

--- src/NetBox/leak_detector.h
+++ src/NetBox/leak_detector.h
@@ -1,18 +1,15 @@
 #pragma once
 
-#define GC_DEBUG
 #define CRTDBG_MAP_ALLOC
 #include <stdlib.h>
 #include <crtdbg.h>
 
+// #define GC_DEBUG
 // #include "gc.h"
-// #undef _GLOBAL_USING
-// #define _INC_CRTDBG
-// #define GC_API extern
-#include "gc_cpp.h"
+// #include "gc_cpp.h"
 
-#define malloc(n) GC_MALLOC(n)
-#define calloc(m,n) GC_MALLOC((m)*(n))
-#define free(p) GC_FREE(p)
-#define realloc(p,n) GC_REALLOC((p),(n))
-#define CHECK_LEAKS() GC_gcollect()
+// #define malloc(n) GC_MALLOC(n)
+// #define calloc(m,n) GC_MALLOC((m)*(n))
+// #define free(p) GC_FREE(p)
+// #define realloc(p,n) GC_REALLOC((p),(n))
+// #define CHECK_LEAKS() GC_gcollect()
