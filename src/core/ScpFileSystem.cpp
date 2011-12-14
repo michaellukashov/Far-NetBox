ae1dacaac97dc292aa1ea1c9a0602ffcee95ccd7
Michael Lukashov <michael.lukashov@gmail.com>
2011-12-14 23:47:06 +0300

    ﻿rename WinSCP --> NetBox

--- src/core/ScpFileSystem.cpp
+++ src/core/ScpFileSystem.cpp
@@ -52,8 +52,8 @@ struct TCommandType
 
 // Only one character! See TSCPFileSystem::ReadCommandOutput()
 #define LastLineSeparator L":"
-#define LAST_LINE L"WinSCP: this is end-of-file"
-#define FIRST_LINE L"WinSCP: this is begin-of-file"
+#define LAST_LINE L"NetBox: this is end-of-file"
+#define FIRST_LINE L"NetBox: this is begin-of-file"
 extern const TCommandType DefaultCommandSet[];
 
 #define NationalVarCount 10
