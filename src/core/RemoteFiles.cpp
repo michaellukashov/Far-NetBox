b8f3404540e5f9367414a549fafdb3210c270837
Michael Lukashov <michael.lukashov@gmail.com>
2011-12-15 00:58:05 +0300

    ﻿SetListingStr

--- src/core/RemoteFiles.cpp
+++ src/core/RemoteFiles.cpp
@@ -983,6 +983,7 @@ void TRemoteFile::SetListingStr(std::wstring value)
     // Do we need to do this (is ever TAB is LS output)?
     Line = ReplaceChar(Line, '\t', ' ');
     DEBUG_PRINTF(L"Line = %s", Line.c_str());
+    FTerminal->LogEvent(FORMAT(L"Line = %s", Line.c_str()));
 
     SetType(Line[0]);
     Line.erase(0, 1);
