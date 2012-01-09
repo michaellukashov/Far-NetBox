15f5c3fa31be4c624777a57e296786ecb81da943
Michael Lukashov <michael.lukashov@gmail.com>
2012-01-09 03:18:28 +0300

    NetBox.rc, version.h: update

--- src/NetBox/version.h
+++ src/NetBox/version.h
@@ -1,6 +1,5 @@
 //------------------------------------------------------------------------------
 // version.h
-// Информация о версии программы
 //
 //------------------------------------------------------------------------------
 #pragma once
@@ -11,9 +10,9 @@
 #define PLUGIN_VERSION_MINOR         0
 #define PLUGIN_VERSION_PATCH         5
 #define PLUGIN_VERSION_BUILD         0
-#define PLUGIN_VERSION_GIT_REVISION  0a32208
+#define PLUGIN_VERSION_GIT_REVISION  902fff7
 
 static const std::wstring CONST_VERSION_STR("2.0.5");
-static const std::wstring CONST_GIT_REVISION_STR("0a32208");
-static const std::wstring CONST_COMPILATION_TIME("09.01.2012 03:15:35");
+static const std::wstring CONST_GIT_REVISION_STR("902fff7");
+static const std::wstring CONST_COMPILATION_TIME("09.01.2012 03:18:50");
 
