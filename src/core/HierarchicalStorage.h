297badaf07a234f765ecbe35b68095132c2ba2b0
Michael Lukashov <michael.lukashov@gmail.com>
2012-01-09 13:14:04 +0300

    ﻿ remove TIniFileStorage

--- src/core/HierarchicalStorage.h
+++ src/core/HierarchicalStorage.h
@@ -2,10 +2,8 @@
 #ifndef HierarchicalStorageH
 #define HierarchicalStorageH
 
-// #include <registry.hpp>
 #include "Classes.h"
 //---------------------------------------------------------------------------
-class TMemIniFile;
 class TRegistry;
 //---------------------------------------------------------------------------
 enum TStorage { stDetect, stRegistry, stIniFile };
@@ -136,50 +134,6 @@ private:
   int FFailed;
 
   void Init();
-};
-//---------------------------------------------------------------------------
-class TIniFileStorage : public THierarchicalStorage
-{
-public:
-  TIniFileStorage(const std::wstring &FileName);
-  virtual ~TIniFileStorage();
-
-  virtual bool OpenSubKey(const std::wstring &SubKey, bool CanCreate, bool Path = false);
-  virtual bool DeleteSubKey(const std::wstring &SubKey);
-  virtual bool DeleteValue(const std::wstring &Name);
-  virtual void GetSubKeyNames(TStrings* Strings);
-  virtual bool KeyExists(const std::wstring &SubKey);
-  virtual bool ValueExists(const std::wstring &Value);
-
-  virtual int BinaryDataSize(const std::wstring &Name);
-
-  virtual bool Readbool(const std::wstring &Name, bool Default);
-  virtual int Readint(const std::wstring &Name, int Default);
-  virtual __int64 ReadInt64(const std::wstring &Name, __int64 Default);
-  virtual TDateTime ReadDateTime(const std::wstring &Name, TDateTime Default);
-  virtual double ReadFloat(const std::wstring &Name, double Default);
-  virtual std::wstring ReadStringRaw(const std::wstring &Name, const std::wstring &Default);
-  virtual int ReadBinaryData(const std::wstring &Name, void * Buffer, int Size);
-
-  virtual void Writebool(const std::wstring &Name, bool Value);
-  virtual void Writeint(const std::wstring &Name, int Value);
-  virtual void WriteInt64(const std::wstring &Name, __int64 Value);
-  virtual void WriteDateTime(const std::wstring &Name, TDateTime Value);
-  virtual void WriteFloat(const std::wstring &Name, double Value);
-  virtual void WriteStringRaw(const std::wstring &Name, const std::wstring &Value);
-  virtual void WriteBinaryData(const std::wstring &Name, const void * Buffer, int Size);
-
-  virtual void GetValueNames(TStrings* Strings);
-
-private:
-  TMemIniFile * FIniFile;
-  TStrings * FOriginal;
-  TIniFileStorage *Self;
-  void ApplyOverrides();
-protected:
-  // __property std::wstring CurrentSection  = { read=GetCurrentSection };
-  std::wstring GetCurrentSection();
-  virtual std::wstring GetSource();
 };
 //---------------------------------------------------------------------------
 std::wstring PuttyMungeStr(const std::wstring &Str);
