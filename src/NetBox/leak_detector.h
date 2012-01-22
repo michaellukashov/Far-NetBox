bdba10dce7d6b4d3bce32b09b522108179b4aaf7
Michael Lukashov <michael.lukashov@gmail.com>
2012-01-22 13:50:38 +0300

    ﻿ leak_detector.h: remove GC

--- src/NetBox/leak_detector.h
+++ src/NetBox/leak_detector.h
@@ -5,13 +5,3 @@
 #include <stdlib.h>
 #include <crtdbg.h>
 #endif
-
-// #define GC_DEBUG
-// #include "gc.h"
-// #include "gc_cpp.h"
-
-// #define malloc(n) GC_MALLOC(n)
-// #define calloc(m,n) GC_MALLOC((m)*(n))
-// #define free(p) GC_FREE(p)
-// #define realloc(p,n) GC_REALLOC((p),(n))
-// #define CHECK_LEAKS() GC_gcollect()
