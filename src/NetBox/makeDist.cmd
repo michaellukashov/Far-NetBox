4d0b409cd7c0d33e30b458c3c9cddb7590a4b9f0
Michael Lukashov <michael.lukashov@gmail.com>
2012-01-28 18:43:52 +0300

    ﻿ makeDist.cmd: create archive Far%PLUGINNAME%-%PLUGINVER%_%FARVER%_x86_x64.7z

--- src/NetBox/makeDist.cmd
+++ src/NetBox/makeDist.cmd
@@ -8,7 +8,7 @@
 
 set PLUGINNAME=NetBox
 set PLUGINARCH=%1
-if "%PLUGINARCH%" equ "" set PLUGINARCH=src
+if "%PLUGINARCH%" equ "" set PLUGINARCH=x86
 set FARVER=%2
 if "%FARVER%" equ "" set FARVER=Far2
 
@@ -18,33 +18,29 @@ if "%PLUGIN_VERSION_TXT%" equ "" echo Undefined version & exit 1
 set PLUGINVER=%PLUGIN_VERSION_TXT%
 
 :: Package name
-set PKGNAME=Far%PLUGINNAME%-%PLUGINVER%_%FARVER%_%PLUGINARCH%.7z
-if exist %PKGNAME% del %PKGNAME%
+set PKGNAME=Far%PLUGINNAME%-%PLUGINVER%_%FARVER%_x86_x64.7z
 
 :: Create temp directory
 set PKGDIR=%PLUGINNAME%
-if "%PLUGINARCH%" equ "src" set PKGDIR=%PKGDIR%-%PLUGINVER%
-if exist %PKGDIR% rmdir /S /Q %PKGDIR%
+set PKGDIRARCH=%PLUGINNAME%\%PLUGINARCH%
+if exist %PKGDIRARCH% rmdir /S /Q %PKGDIRARCH%
 
 :: Copy files
-mkdir %PKGDIR%
-call .\makeCopyCommon.cmd %~d0%~p0 %PKGDIR%
-call .\makeCopyCommon.cmd %~d0%~p0\..\.. %PKGDIR%
-if "%PLUGINARCH%" equ "src"    (
-    call .\makeCopySources.cmd %~d0%~p0 %PKGDIR%
-    mkdir %PKGDIR%\tinyXML
-    copy ..\..\libs\tinyXML %PKGDIR%\tinyXML > NUL
-) else (
-    copy ..\..\%FARVER%_%PLUGINARCH%\Plugins\%PLUGINNAME%\*.dll %PKGDIR% > NUL
-    copy ..\..\dlls\%PLUGINARCH%\*.dll %PKGDIR% > NUL
-    copy ..\..\libs\openssl\%PLUGINARCH%\*.dll %PKGDIR% > NUL
-    call .\makeCopyCommon.cmd %~d0%~p0 ..\..\%FARVER%_%PLUGINARCH%\Plugins\%PLUGINNAME% > NUL
-)
+if not exist %PKGDIR% ( mkdir %PKGDIR% > NUL )
+mkdir %PKGDIRARCH% > NUL
+call .\makeCopyCommon.cmd %~d0%~p0 %PKGDIRARCH%
+call .\makeCopyCommon.cmd %~d0%~p0\..\.. %PKGDIRARCH%
+copy ..\..\%FARVER%_%PLUGINARCH%\Plugins\%PLUGINNAME%\*.dll %PKGDIRARCH% > NUL
+copy ..\..\dlls\%PLUGINARCH%\*.dll %PKGDIRARCH% > NUL
+copy ..\..\libs\openssl\%PLUGINARCH%\*.dll %PKGDIRARCH% > NUL
+call .\makeCopyCommon.cmd %~d0%~p0 ..\..\%FARVER%_%PLUGINARCH%\Plugins\%PLUGINNAME% > NUL
+
 :: Make archive
-call "C:\Program Files\7-Zip\7z.exe" a -t7z -r %PKGNAME% %PKGDIR%/* > NUL
+if exist %PKGNAME% del %PKGNAME%
+call "C:\Program Files\7-Zip\7z.exe" a -mx9 -t7z -r %PKGNAME% %PKGDIR%/* > NUL
 if errorlevel 1 echo Error creating archive & exit 1 /b
 
-rmdir /S /Q %PKGDIR%
+@rem rmdir /S /Q %PKGDIRARCH%
 
 echo Package %PKGNAME% created
 exit 0 /b
