19eb28a98adaa9410000a720e006dd1ac46e94dd
Michael Lukashov <michael.lukashov@gmail.com>
2012-02-01 23:41:45 +0300

    ﻿ TSessionDialog: SCP: allow to change "UTF-8 encoding for filenames:" setting

--- src/NetBox/WinSCPDialogs.cpp
+++ src/NetBox/WinSCPDialogs.cpp
@@ -2953,7 +2953,7 @@ void TSessionDialog::UpdateControls()
 
     // Environment tab
     DSTModeUnixCheck->SetEnabled(!FtpProtocol);
-    UtfCombo->SetEnabled(FSProtocol != fsSCPonly);
+    // UtfCombo->SetEnabled(FSProtocol != fsSCPonly);
     TimeDifferenceEdit->SetEnabled((FtpProtocol || (FSProtocol == fsSCPonly)));
 
     // Recycle bin tab
