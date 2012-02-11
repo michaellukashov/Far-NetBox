08a31b9a7257db9a7a34c0b132c2079a9ddd6042
Michael Lukashov <michael.lukashov@gmail.com>
2012-02-11 23:18:47 +0300

    ﻿ version info: update

--- src/NetBox/resource.h
+++ src/NetBox/resource.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define PLUGIN_VERSION_NUM 2,0,9,60
-#define PLUGIN_VERSION_TXT "2.0.9"
-#define PLUGIN_VERSION_WTXT L"2.0.9"
+#define PLUGIN_VERSION_NUM 2,0,10,61
+#define PLUGIN_VERSION_TXT "2.0.10"
+#define PLUGIN_VERSION_WTXT L"2.0.10"
 
 #define PLUGIN_NAME         L"NetBox"
 #define PLUGIN_AUTHOR       L"Michael Lukashov"
@@ -13,7 +13,7 @@
 #define WINSCP_VERSION_WTXT         L"4.3.6"
 #define PUTTY_VERSION_WTXT          L"0.62"
 #define FILEZILLA_VERSION_WTXT      L"2.2.32"
-#define ZLIB_VERSION_WTXT           L"1.2.5"
+#define ZLIB_VERSION_WTXT           L"1.2.6"
 #define LIBSSH2_VERSION_WTXT        L"1.2.8"
 #define CURL_VERSION_WTXT           L"7.24.0"
 #define OPENSSL_VERSION_WTXT        L"1.0.0g"
