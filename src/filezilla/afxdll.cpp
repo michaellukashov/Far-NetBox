80b07c05f789a17bc8b57f40459378c15158ec6e
Michael Lukashov <michael.lukashov@gmail.com>
2012-02-19 09:36:14 +0300

    ﻿ InitExtensionModule: cleanup

--- src/filezilla/afxdll.cpp
+++ src/filezilla/afxdll.cpp
@@ -3,21 +3,13 @@
 
 HINSTANCE HInst;
 
-// static AFX_EXTENSION_MODULE MyExtDLL = { NULL, NULL } ;
-
 void InitExtensionModule(HINSTANCE HInst)
 {
-    // AfxInitExtensionModule(MyExtDLL, HInst);
-    // AFX_MANAGE_STATE(AfxGetModuleState());
-    // afxCurrentResourceHandle = HInst;
     ::HInst = HInst;
-    // Insert this DLL into the resource chain
-    // new CDynLinkLibrary(MyExtDLL);
     AFX_MANAGE_STATE(AfxGetModuleState());
     afxCurrentResourceHandle = ::HInst;
 }
 
 void TermExtensionModule()
 {
-    // AfxTermExtensionModule(MyExtDLL);
 }
