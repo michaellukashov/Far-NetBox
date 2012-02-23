b70737be95b9ed45f68a5debd2a23cbc9d59d994
Michael Lukashov <michael.lukashov@gmail.com>
2012-02-23 20:47:20 +0300

    ﻿ check correspondence with WinSCP 4.3.7 sources

--- src/core/Queue.cpp
+++ src/core/Queue.cpp
@@ -1008,13 +1008,6 @@ void TTerminalItem::ProcessEvent()
     }
     catch (const std::exception &E)
     {
-        // do not show error messages, if task was canceled anyway
-        // (for example if transfer is cancelled during reconnection attempts)
-        // if (!FCancel &&
-        // (FTerminal->QueryUserException(L"", &E, qaOK | qaCancel, NULL, qtError) == qaCancel))
-        // {
-        // FCancel = true;
-        // }
         std::wstring Message;
         if (ExceptionMessage(&E, Message))
         {
