ea38dc36c3953f0b97d5d97e4888fa1950ae353c
Michael Lukashov <michael.lukashov@gmail.com>
2012-03-11 19:51:11 +0300

    version info: update

--- src/NetBox/resource.h
+++ src/NetBox/resource.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define PLUGIN_VERSION_NUM 2,0,14,85
-#define PLUGIN_VERSION_TXT "2.0.14"
-#define PLUGIN_VERSION_WTXT L"2.0.14"
+#define PLUGIN_VERSION_NUM 2,0,15,86
+#define PLUGIN_VERSION_TXT "2.0.15"
+#define PLUGIN_VERSION_WTXT L"2.0.15"
 
 #define PLUGIN_NAME         L"NetBox"
 #define PLUGIN_AUTHOR       L"Michael Lukashov"
--- src/NetBox/version.h
+++ src/NetBox/version.h
@@ -8,9 +8,9 @@
 
 #define PLUGIN_VERSION_MAJOR         2
 #define PLUGIN_VERSION_MINOR         0
-#define PLUGIN_VERSION_PATCH         14
-#define PLUGIN_VERSION_BUILD         85
+#define PLUGIN_VERSION_PATCH         15
+#define PLUGIN_VERSION_BUILD         86
 
-static const std::wstring CONST_VERSION_STR(L"2.0.14");
-static const std::wstring CONST_COMPILATION_TIME(L"09.03.2012 11:20:23");
+static const std::wstring CONST_VERSION_STR(L"2.0.15");
+static const std::wstring CONST_COMPILATION_TIME(L"11.03.2012 19:50:59");
 
