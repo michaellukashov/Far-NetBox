fa943186af7217e4b2496fa101702adc4381d272
Michael Lukashov <michael.lukashov@gmail.com>
2012-04-26 13:02:16 +0400

    run_astyle.bat: new params

--- src/NetBox/scripts/run_astyle.bat
+++ src/NetBox/scripts/run_astyle.bat
@@ -1,1 +1,2 @@
-astyle --style=allman --indent=spaces=4 --add-brackets --convert-tabs --align-pointer=name --align-reference=name --mode=c --keep-one-line-statements --keep-one-line-blocks %*
+rem astyle --style=allman --indent=spaces=4 --add-brackets --convert-tabs --align-pointer=name --align-reference=name --mode=c --keep-one-line-statements --keep-one-line-blocks %*
+astyle --style=allman --indent=spaces=2 --convert-tabs --align-pointer=middle --align-reference=middle --mode=c --keep-one-line-statements --keep-one-line-blocks %*
