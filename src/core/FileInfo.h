3814995df43f013ce8a9cef8e14d6a9dc405c7b6
Michael Lukashov <michael.lukashov@gmail.com>
2012-05-05 02:08:21 +0400

    FileInfo.cpp: astyle

--- src/core/FileInfo.h
+++ src/core/FileInfo.h
@@ -3,35 +3,34 @@
 
 #include "stdafx.h"
 
-struct TTranslation
-{
-    unsigned int Language, CharSet;
+struct TTranslation {
+  Word Language, CharSet;
 };
 
 // Return pointer to file version info block
-void * __fastcall CreateFileInfo(const UnicodeString FileName);
+void * __fastcall CreateFileInfo(UnicodeString FileName);
 
 // Free file version info block memory
-void __fastcall FreeFileInfo(void *FileInfo);
+void __fastcall FreeFileInfo(void * FileInfo);
 
 // Return pointer to fixed file version info
-VS_FIXEDFILEINFO __fastcall GetFixedFileInfo(void *FileInfo);
+VS_FIXEDFILEINFO __fastcall GetFixedFileInfo(void * FileInfo);
 
 // Return number of available file version info translations
-unsigned __fastcall GetTranslationCount(void *FileInfo);
+unsigned __fastcall GetTranslationCount(void * FileInfo);
 
 // Return i-th translation in the file version info translation list
-TTranslation __fastcall GetTranslation(void *FileInfo, unsigned i);
+TTranslation __fastcall GetTranslation(void * FileInfo, unsigned i);
 
 // Return the name of the specified language
-UnicodeString __fastcall GetLanguage(unsigned int Language);
+UnicodeString __fastcall GetLanguage(Word Language);
 
 // Return the value of the specified file version info string using the
 // specified translation
-UnicodeString __fastcall GetFileInfoString(void *FileInfo,
-                               TTranslation Translation, UnicodeString StringName);
+UnicodeString __fastcall GetFileInfoString(void * FileInfo,
+  TTranslation Translation, UnicodeString StringName);
 
 int __fastcall CalculateCompoundVersion(int MajorVer,
-                             int MinorVer, int Release, int Build);
+  int MinorVer, int Release, int Build);
 
 #endif // FileInfoH
