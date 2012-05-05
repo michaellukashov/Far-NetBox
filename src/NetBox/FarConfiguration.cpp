8b8d0f3338261b03c0f31f2da6746f77f9d8c47d
Michael Lukashov <michael.lukashov@gmail.com>
2012-05-05 11:51:00 +0400

    FarConfiguration.cpp: astyle

--- src/NetBox/FarConfiguration.cpp
+++ src/NetBox/FarConfiguration.cpp
@@ -9,71 +9,71 @@
 #include "FarConfiguration.h"
 #include "FarPlugin.h"
 //---------------------------------------------------------------------------
-TFarConfiguration::TFarConfiguration(TCustomFarPlugin *APlugin) :
-    TGUIConfiguration()
+TFarConfiguration::TFarConfiguration(TCustomFarPlugin * APlugin) :
+  TGUIConfiguration()
 {
-    // DEBUG_PRINTF(L"begin");
-    Self = this;
-    FFarConfirmations = -1;
-    FFarPlugin = APlugin;
-    FBookmarks = new TBookmarks();
-    Default();
-    // DEBUG_PRINTF(L"end");
+  // DEBUG_PRINTF(L"begin");
+  Self = this;
+  FFarConfirmations = -1;
+  FFarPlugin = APlugin;
+  FBookmarks = new TBookmarks();
+  Default();
+  // DEBUG_PRINTF(L"end");
 }
 //---------------------------------------------------------------------------
 TFarConfiguration::~TFarConfiguration()
 {
-    delete FBookmarks;
+  delete FBookmarks;
 }
 //---------------------------------------------------------------------------
 void TFarConfiguration::Default()
 {
-    // DEBUG_PRINTF(L"begin");
-    TGUIConfiguration::Default();
+  // DEBUG_PRINTF(L"begin");
+  TGUIConfiguration::Default();
 
-    FForceInheritance = false;
-    FConfirmOverwritingOverride = false;
-    FConfirmSynchronizedBrowsing = true;
+  FForceInheritance = false;
+  FConfirmOverwritingOverride = false;
+  FConfirmSynchronizedBrowsing = true;
 
-    SetDisksMenu(true);
-    SetDisksMenuHotKey(0);
-    SetPluginsMenu(true);
-    SetPluginsMenuCommands(true);
-    SetCommandPrefixes(L"netbox,scp,sftp,ftps,http,https");
-    SetHostNameInTitle(true);
-    SetEditorDownloadDefaultMode(true);
-    SetEditorUploadSameOptions(true);
-    FEditorUploadOnSave = false;
-    FEditorMultiple = false;
-    FQueueBeep = true;
+  SetDisksMenu(true);
+  SetDisksMenuHotKey(0);
+  SetPluginsMenu(true);
+  SetPluginsMenuCommands(true);
+  SetCommandPrefixes(L"netbox,scp,sftp,ftps,http,https");
+  SetHostNameInTitle(true);
+  SetEditorDownloadDefaultMode(true);
+  SetEditorUploadSameOptions(true);
+  FEditorUploadOnSave = false;
+  FEditorMultiple = false;
+  FQueueBeep = true;
 
-    SetCustomPanelModeDetailed(true);
-    SetFullScreenDetailed(true);
-    SetColumnTypesDetailed(L"N,S,DM,O,G,R");
-    SetColumnWidthsDetailed(L"0,8,14,0,0,9");
-    SetStatusColumnTypesDetailed(L"NR");
-    SetStatusColumnWidthsDetailed(L"0");
+  SetCustomPanelModeDetailed(true);
+  SetFullScreenDetailed(true);
+  SetColumnTypesDetailed(L"N,S,DM,O,G,R");
+  SetColumnWidthsDetailed(L"0,8,14,0,0,9");
+  SetStatusColumnTypesDetailed(L"NR");
+  SetStatusColumnWidthsDetailed(L"0");
 
-    SetApplyCommandCommand(L"");
-    SetApplyCommandParams(0);
+  SetApplyCommandCommand(L"");
+  SetApplyCommandParams(0);
 
-    SetPuttygenPath(FormatCommand(ExtractFilePath(ModuleFileName()) + L"putty\\puttygen.exe", L""));
-    SetPageantPath(FormatCommand(ExtractFilePath(ModuleFileName()) + L"putty\\pageant.exe", L""));
+  SetPuttygenPath(FormatCommand(ExtractFilePath(ModuleFileName()) + L"putty\\puttygen.exe", L""));
+  SetPageantPath(FormatCommand(ExtractFilePath(ModuleFileName()) + L"putty\\pageant.exe", L""));
 
-    FBookmarks->Clear();
-    // DEBUG_PRINTF(L"end");
+  FBookmarks->Clear();
+  // DEBUG_PRINTF(L"end");
 }
 //---------------------------------------------------------------------------
-THierarchicalStorage *TFarConfiguration::CreateScpStorage(bool SessionList)
+THierarchicalStorage * TFarConfiguration::CreateScpStorage(bool SessionList)
 {
-    // DEBUG_PRINTF(L"GetStorage = %d", GetStorage());
-    return TGUIConfiguration::CreateScpStorage(SessionList);
+  // DEBUG_PRINTF(L"GetStorage = %d", GetStorage());
+  return TGUIConfiguration::CreateScpStorage(SessionList);
 }
 //---------------------------------------------------------------------------
 void TFarConfiguration::Saved()
 {
-    TGUIConfiguration::Saved();
-    FBookmarks->ModifyAll(false);
+  TGUIConfiguration::Saved();
+  FBookmarks->ModifyAll(false);
 }
 //---------------------------------------------------------------------------
 // duplicated from core\configuration.cpp
@@ -115,142 +115,142 @@ void TFarConfiguration::Saved()
     KEY(bool,     ConfirmSynchronizedBrowsing); \
   );
 //---------------------------------------------------------------------------
-void TFarConfiguration::SaveData(THierarchicalStorage *Storage,
+void TFarConfiguration::SaveData(THierarchicalStorage * Storage,
                                  bool All)
 {
-    // DEBUG_PRINTF(L"begin");
-    TGUIConfiguration::SaveData(Storage, All);
+  // DEBUG_PRINTF(L"begin");
+  TGUIConfiguration::SaveData(Storage, All);
 
-    // duplicated from core\configuration.cpp
+  // duplicated from core\configuration.cpp
 #define KEY(TYPE, VAR) Storage->Write ## TYPE(LASTELEM(MB2W(#VAR)), Get##VAR())
-    REGCONFIG(true);
+  REGCONFIG(true);
 #undef KEY
 
-    if (Storage->OpenSubKey(L"Bookmarks", true))
-    {
-        FBookmarks->Save(Storage, All);
+  if (Storage->OpenSubKey(L"Bookmarks", true))
+  {
+    FBookmarks->Save(Storage, All);
 
-        Storage->CloseSubKey();
-    }
-    // DEBUG_PRINTF(L"end");
+    Storage->CloseSubKey();
+  }
+  // DEBUG_PRINTF(L"end");
 }
 //---------------------------------------------------------------------------
-void TFarConfiguration::LoadData(THierarchicalStorage *Storage)
+void TFarConfiguration::LoadData(THierarchicalStorage * Storage)
 {
-    TGUIConfiguration::LoadData(Storage);
+  TGUIConfiguration::LoadData(Storage);
 
-    // duplicated from core\configuration.cpp
+  // duplicated from core\configuration.cpp
 #define KEY(TYPE, VAR) Set##VAR(Storage->Read ## TYPE(LASTELEM(MB2W(#VAR)), Get##VAR()))
-    REGCONFIG(false);
+  REGCONFIG(false);
 #undef KEY
 
-    if (Storage->OpenSubKey(L"Bookmarks", false))
-    {
-        FBookmarks->Load(Storage);
-        Storage->CloseSubKey();
-    }
+  if (Storage->OpenSubKey(L"Bookmarks", false))
+  {
+    FBookmarks->Load(Storage);
+    Storage->CloseSubKey();
+  }
 }
 //---------------------------------------------------------------------------
 void TFarConfiguration::Load()
 {
-    FForceInheritance = true;
+  FForceInheritance = true;
+  {
+    BOOST_SCOPE_EXIT ( (&Self) )
     {
-        BOOST_SCOPE_EXIT ( (&Self) )
-        {
-            Self->FForceInheritance = false;
-        } BOOST_SCOPE_EXIT_END
-        TGUIConfiguration::Load();
-    }
+      Self->FForceInheritance = false;
+    } BOOST_SCOPE_EXIT_END
+    TGUIConfiguration::Load();
+  }
 }
 //---------------------------------------------------------------------------
 void TFarConfiguration::Save(bool All, bool Explicit)
 {
-    FForceInheritance = true;
+  FForceInheritance = true;
+  {
+    BOOST_SCOPE_EXIT ( (&Self) )
     {
-        BOOST_SCOPE_EXIT ( (&Self) )
-        {
-            Self->FForceInheritance = false;
-        } BOOST_SCOPE_EXIT_END
-        TGUIConfiguration::Save(All, Explicit);
-    }
+      Self->FForceInheritance = false;
+    } BOOST_SCOPE_EXIT_END
+    TGUIConfiguration::Save(All, Explicit);
+  }
 }
 //---------------------------------------------------------------------------
-void TFarConfiguration::SetPlugin(TCustomFarPlugin *value)
+void TFarConfiguration::SetPlugin(TCustomFarPlugin * value)
 {
-    if (GetPlugin() != value)
-    {
-        assert(!GetPlugin() || !value);
-        FFarPlugin = value;
-    }
+  if (GetPlugin() != value)
+  {
+    assert(!GetPlugin() || !value);
+    FFarPlugin = value;
+  }
 }
 //---------------------------------------------------------------------------
 void TFarConfiguration::CacheFarSettings()
 {
-    FFarConfirmations = GetPlugin()->FarAdvControl(ACTL_GETCONFIRMATIONS);
+  FFarConfirmations = GetPlugin()->FarAdvControl(ACTL_GETCONFIRMATIONS);
 }
 //---------------------------------------------------------------------------
 int TFarConfiguration::FarConfirmations()
 {
-    if (GetCurrentThreadId() == GetPlugin()->GetFarThread())
-    {
-        return GetPlugin()->FarAdvControl(ACTL_GETCONFIRMATIONS);
-    }
-    else
-    {
-        assert(FFarConfirmations >= 0);
-        return FFarConfirmations;
-    }
+  if (GetCurrentThreadId() == GetPlugin()->GetFarThread())
+  {
+    return GetPlugin()->FarAdvControl(ACTL_GETCONFIRMATIONS);
+  }
+  else
+  {
+    assert(FFarConfirmations >= 0);
+    return FFarConfirmations;
+  }
 }
 //---------------------------------------------------------------------------
 bool TFarConfiguration::GetConfirmOverwriting()
 {
-    if (FForceInheritance || FConfirmOverwritingOverride)
-    {
-        return TGUIConfiguration::GetConfirmOverwriting();
-    }
-    else
-    {
-        assert(GetPlugin());
-        return (FarConfirmations() & FCS_COPYOVERWRITE) != 0;
-    }
+  if (FForceInheritance || FConfirmOverwritingOverride)
+  {
+    return TGUIConfiguration::GetConfirmOverwriting();
+  }
+  else
+  {
+    assert(GetPlugin());
+    return (FarConfirmations() & FCS_COPYOVERWRITE) != 0;
+  }
 }
 //---------------------------------------------------------------------------
 void TFarConfiguration::SetConfirmOverwriting(bool value)
 {
-    if (FForceInheritance)
+  if (FForceInheritance)
+  {
+    TGUIConfiguration::SetConfirmOverwriting(value);
+  }
+  else
+  {
+    if (GetConfirmOverwriting() != value)
     {
-        TGUIConfiguration::SetConfirmOverwriting(value);
+      FConfirmOverwritingOverride = true;
+      TGUIConfiguration::SetConfirmOverwriting(value);
     }
-    else
-    {
-        if (GetConfirmOverwriting() != value)
-        {
-            FConfirmOverwritingOverride = true;
-            TGUIConfiguration::SetConfirmOverwriting(value);
-        }
-    }
+  }
 }
 //---------------------------------------------------------------------------
 bool TFarConfiguration::GetConfirmDeleting()
 {
-    assert(GetPlugin());
-    return (FarConfirmations() & FCS_DELETE) != 0;
+  assert(GetPlugin());
+  return (FarConfirmations() & FCS_DELETE) != 0;
 }
 //---------------------------------------------------------------------------
 UnicodeString TFarConfiguration::ModuleFileName()
 {
-    assert(GetPlugin());
-    return GetPlugin()->GetModuleName();
+  assert(GetPlugin());
+  return GetPlugin()->GetModuleName();
 }
 //---------------------------------------------------------------------------
 void TFarConfiguration::SetBookmark(const UnicodeString Key,
-                                    TBookmarkList *value)
+                                    TBookmarkList * value)
 {
-    FBookmarks->SetBookmarks(Key, value);
-    Changed();
+  FBookmarks->SetBookmarks(Key, value);
+  Changed();
 }
 //---------------------------------------------------------------------------
-TBookmarkList *TFarConfiguration::GetBookmark(const UnicodeString Key)
+TBookmarkList * TFarConfiguration::GetBookmark(const UnicodeString Key)
 {
-    return FBookmarks->GetBookmarks(Key);
+  return FBookmarks->GetBookmarks(Key);
 }
--- src/NetBox/FarConfiguration.h
+++ src/NetBox/FarConfiguration.h
@@ -11,113 +11,113 @@ class TBookmarkList;
 class TFarConfiguration : public TGUIConfiguration
 {
 public:
-    explicit TFarConfiguration(TCustomFarPlugin *APlugin);
-    virtual ~TFarConfiguration();
+  explicit TFarConfiguration(TCustomFarPlugin * APlugin);
+  virtual ~TFarConfiguration();
 
-    TCustomFarPlugin *GetPlugin() { return FFarPlugin; }
-    void SetPlugin(TCustomFarPlugin *value);
-    bool GetConfirmOverwritingOverride() { return FConfirmOverwritingOverride; }
-    void SetConfirmOverwritingOverride(bool value) { FConfirmOverwritingOverride = value; }
-    bool GetConfirmDeleting();
-    bool GetConfirmSynchronizedBrowsing() { return FConfirmSynchronizedBrowsing; }
-    void SetConfirmSynchronizedBrowsing(bool value) { FConfirmSynchronizedBrowsing = value; }
-    bool GetDisksMenu() { return FDisksMenu; }
-    void SetDisksMenu(bool value) { FDisksMenu = value; }
-    int GetDisksMenuHotKey() { return FDisksMenuHotKey; }
-    void SetDisksMenuHotKey(int value) { FDisksMenuHotKey = value; }
-    bool GetPluginsMenu() { return FPluginsMenu; }
-    void SetPluginsMenu(bool value) { FPluginsMenu = value; }
-    bool GetPluginsMenuCommands() { return FPluginsMenuCommands; }
-    void SetPluginsMenuCommands(bool value) { FPluginsMenuCommands = value; }
-    UnicodeString GetCommandPrefixes() { return FCommandPrefixes; }
-    void SetCommandPrefixes(const UnicodeString value) { FCommandPrefixes = value; }
-    bool GetHostNameInTitle() { return FHostNameInTitle; }
-    void SetHostNameInTitle(bool value) { FHostNameInTitle = value; }
+  TCustomFarPlugin * GetPlugin() { return FFarPlugin; }
+  void SetPlugin(TCustomFarPlugin * value);
+  bool GetConfirmOverwritingOverride() { return FConfirmOverwritingOverride; }
+  void SetConfirmOverwritingOverride(bool value) { FConfirmOverwritingOverride = value; }
+  bool GetConfirmDeleting();
+  bool GetConfirmSynchronizedBrowsing() { return FConfirmSynchronizedBrowsing; }
+  void SetConfirmSynchronizedBrowsing(bool value) { FConfirmSynchronizedBrowsing = value; }
+  bool GetDisksMenu() { return FDisksMenu; }
+  void SetDisksMenu(bool value) { FDisksMenu = value; }
+  int GetDisksMenuHotKey() { return FDisksMenuHotKey; }
+  void SetDisksMenuHotKey(int value) { FDisksMenuHotKey = value; }
+  bool GetPluginsMenu() { return FPluginsMenu; }
+  void SetPluginsMenu(bool value) { FPluginsMenu = value; }
+  bool GetPluginsMenuCommands() { return FPluginsMenuCommands; }
+  void SetPluginsMenuCommands(bool value) { FPluginsMenuCommands = value; }
+  UnicodeString GetCommandPrefixes() { return FCommandPrefixes; }
+  void SetCommandPrefixes(const UnicodeString value) { FCommandPrefixes = value; }
+  bool GetHostNameInTitle() { return FHostNameInTitle; }
+  void SetHostNameInTitle(bool value) { FHostNameInTitle = value; }
 
-    bool GetCustomPanelModeDetailed() { return FCustomPanelModeDetailed; }
-    void SetCustomPanelModeDetailed(bool value) { FCustomPanelModeDetailed = value; }
-    bool GetFullScreenDetailed() { return FFullScreenDetailed; }
-    void SetFullScreenDetailed(bool value) { FFullScreenDetailed = value; }
-    UnicodeString GetColumnTypesDetailed() { return FColumnTypesDetailed; }
-    void SetColumnTypesDetailed(const UnicodeString value) { FColumnTypesDetailed = value; }
-    UnicodeString GetColumnWidthsDetailed() { return FColumnWidthsDetailed; }
-    void SetColumnWidthsDetailed(const UnicodeString value) { FColumnWidthsDetailed = value; }
-    UnicodeString GetStatusColumnTypesDetailed() { return FStatusColumnTypesDetailed; }
-    void SetStatusColumnTypesDetailed(const UnicodeString value) { FStatusColumnTypesDetailed = value; }
-    UnicodeString GetStatusColumnWidthsDetailed() { return FStatusColumnWidthsDetailed; }
-    void SetStatusColumnWidthsDetailed(const UnicodeString value) { FStatusColumnWidthsDetailed = value; }
-    bool GetEditorDownloadDefaultMode() { return FEditorDownloadDefaultMode; }
-    void SetEditorDownloadDefaultMode(bool value) { FEditorDownloadDefaultMode = value; }
-    bool GetEditorUploadSameOptions() { return FEditorUploadSameOptions; }
-    void SetEditorUploadSameOptions(bool value) { FEditorUploadSameOptions = value; }
-    bool GetEditorUploadOnSave() { return FEditorUploadOnSave; }
-    void SetEditorUploadOnSave(bool value) { FEditorUploadOnSave = value; }
-    bool GetEditorMultiple() { return FEditorMultiple; }
-    void SetEditorMultiple(bool value) { FEditorMultiple = value; }
-    bool GetQueueBeep() { return FQueueBeep; }
-    void SetQueueBeep(bool value) { FQueueBeep = value; }
+  bool GetCustomPanelModeDetailed() { return FCustomPanelModeDetailed; }
+  void SetCustomPanelModeDetailed(bool value) { FCustomPanelModeDetailed = value; }
+  bool GetFullScreenDetailed() { return FFullScreenDetailed; }
+  void SetFullScreenDetailed(bool value) { FFullScreenDetailed = value; }
+  UnicodeString GetColumnTypesDetailed() { return FColumnTypesDetailed; }
+  void SetColumnTypesDetailed(const UnicodeString value) { FColumnTypesDetailed = value; }
+  UnicodeString GetColumnWidthsDetailed() { return FColumnWidthsDetailed; }
+  void SetColumnWidthsDetailed(const UnicodeString value) { FColumnWidthsDetailed = value; }
+  UnicodeString GetStatusColumnTypesDetailed() { return FStatusColumnTypesDetailed; }
+  void SetStatusColumnTypesDetailed(const UnicodeString value) { FStatusColumnTypesDetailed = value; }
+  UnicodeString GetStatusColumnWidthsDetailed() { return FStatusColumnWidthsDetailed; }
+  void SetStatusColumnWidthsDetailed(const UnicodeString value) { FStatusColumnWidthsDetailed = value; }
+  bool GetEditorDownloadDefaultMode() { return FEditorDownloadDefaultMode; }
+  void SetEditorDownloadDefaultMode(bool value) { FEditorDownloadDefaultMode = value; }
+  bool GetEditorUploadSameOptions() { return FEditorUploadSameOptions; }
+  void SetEditorUploadSameOptions(bool value) { FEditorUploadSameOptions = value; }
+  bool GetEditorUploadOnSave() { return FEditorUploadOnSave; }
+  void SetEditorUploadOnSave(bool value) { FEditorUploadOnSave = value; }
+  bool GetEditorMultiple() { return FEditorMultiple; }
+  void SetEditorMultiple(bool value) { FEditorMultiple = value; }
+  bool GetQueueBeep() { return FQueueBeep; }
+  void SetQueueBeep(bool value) { FQueueBeep = value; }
 
-    UnicodeString GetApplyCommandCommand() { return FApplyCommandCommand; }
-    void SetApplyCommandCommand(const UnicodeString value) { FApplyCommandCommand = value; }
-    int GetApplyCommandParams() { return FApplyCommandParams; }
-    void SetApplyCommandParams(int value) { FApplyCommandParams = value; }
+  UnicodeString GetApplyCommandCommand() { return FApplyCommandCommand; }
+  void SetApplyCommandCommand(const UnicodeString value) { FApplyCommandCommand = value; }
+  int GetApplyCommandParams() { return FApplyCommandParams; }
+  void SetApplyCommandParams(int value) { FApplyCommandParams = value; }
 
-    UnicodeString GetPageantPath() const { return FPageantPath; }
-    void SetPageantPath(const UnicodeString value) { FPageantPath = value; }
-    UnicodeString GetPuttygenPath() const { return FPuttygenPath; }
-    void SetPuttygenPath(const UnicodeString value) { FPuttygenPath = value; }
-    TBookmarkList *GetBookmark(const UnicodeString Key);
-    void SetBookmark(const UnicodeString Key, TBookmarkList *value);
+  UnicodeString GetPageantPath() const { return FPageantPath; }
+  void SetPageantPath(const UnicodeString value) { FPageantPath = value; }
+  UnicodeString GetPuttygenPath() const { return FPuttygenPath; }
+  void SetPuttygenPath(const UnicodeString value) { FPuttygenPath = value; }
+  TBookmarkList * GetBookmark(const UnicodeString Key);
+  void SetBookmark(const UnicodeString Key, TBookmarkList * value);
 
-    virtual void __fastcall Load();
-    virtual void __fastcall Save(bool All, bool Explicit);
-    virtual void __fastcall Default();
-    virtual THierarchicalStorage * __fastcall CreateScpStorage(bool SessionList);
-    void CacheFarSettings();
+  virtual void __fastcall Load();
+  virtual void __fastcall Save(bool All, bool Explicit);
+  virtual void __fastcall Default();
+  virtual THierarchicalStorage * __fastcall CreateScpStorage(bool SessionList);
+  void CacheFarSettings();
 
 protected:
-    virtual bool __fastcall GetConfirmOverwriting();
-    virtual void __fastcall SetConfirmOverwriting(bool value);
+  virtual bool __fastcall GetConfirmOverwriting();
+  virtual void __fastcall SetConfirmOverwriting(bool value);
 
-    virtual void __fastcall SaveData(THierarchicalStorage *Storage, bool All);
-    virtual void __fastcall LoadData(THierarchicalStorage *Storage);
+  virtual void __fastcall SaveData(THierarchicalStorage * Storage, bool All);
+  virtual void __fastcall LoadData(THierarchicalStorage * Storage);
 
-    virtual UnicodeString __fastcall ModuleFileName();
-    virtual void __fastcall Saved();
+  virtual UnicodeString __fastcall ModuleFileName();
+  virtual void __fastcall Saved();
 
 private:
-    TCustomFarPlugin *FFarPlugin;
-    int FFarConfirmations;
-    bool FConfirmOverwritingOverride;
-    bool FConfirmSynchronizedBrowsing;
-    bool FForceInheritance;
-    bool FDisksMenu;
-    int FDisksMenuHotKey;
-    bool FPluginsMenu;
-    bool FPluginsMenuCommands;
-    UnicodeString FCommandPrefixes;
-    bool FHostNameInTitle;
-    bool FEditorDownloadDefaultMode;
-    bool FEditorUploadSameOptions;
-    bool FEditorUploadOnSave;
-    bool FEditorMultiple;
-    bool FQueueBeep;
-    UnicodeString FPageantPath;
-    UnicodeString FPuttygenPath;
-    UnicodeString FApplyCommandCommand;
-    int FApplyCommandParams;
+  TCustomFarPlugin * FFarPlugin;
+  int FFarConfirmations;
+  bool FConfirmOverwritingOverride;
+  bool FConfirmSynchronizedBrowsing;
+  bool FForceInheritance;
+  bool FDisksMenu;
+  int FDisksMenuHotKey;
+  bool FPluginsMenu;
+  bool FPluginsMenuCommands;
+  UnicodeString FCommandPrefixes;
+  bool FHostNameInTitle;
+  bool FEditorDownloadDefaultMode;
+  bool FEditorUploadSameOptions;
+  bool FEditorUploadOnSave;
+  bool FEditorMultiple;
+  bool FQueueBeep;
+  UnicodeString FPageantPath;
+  UnicodeString FPuttygenPath;
+  UnicodeString FApplyCommandCommand;
+  int FApplyCommandParams;
 
-    bool FCustomPanelModeDetailed;
-    bool FFullScreenDetailed;
-    UnicodeString FColumnTypesDetailed;
-    UnicodeString FColumnWidthsDetailed;
-    UnicodeString FStatusColumnTypesDetailed;
-    UnicodeString FStatusColumnWidthsDetailed;
+  bool FCustomPanelModeDetailed;
+  bool FFullScreenDetailed;
+  UnicodeString FColumnTypesDetailed;
+  UnicodeString FColumnWidthsDetailed;
+  UnicodeString FStatusColumnTypesDetailed;
+  UnicodeString FStatusColumnWidthsDetailed;
 
-    TBookmarks *FBookmarks;
-    TFarConfiguration *Self;
+  TBookmarks * FBookmarks;
+  TFarConfiguration * Self;
 
-    int FarConfirmations();
+  int FarConfirmations();
 };
 //---------------------------------------------------------------------------
 #define FarConfiguration ((TFarConfiguration *) Configuration)
