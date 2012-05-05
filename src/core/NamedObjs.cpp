a4cff7e1801ffdd44f6a540df6ef838fb57e9819
Michael Lukashov <michael.lukashov@gmail.com>
2012-05-05 12:14:41 +0400

    NamedObjs.cpp: astyle

--- src/core/NamedObjs.cpp
+++ src/core/NamedObjs.cpp
@@ -4,120 +4,120 @@
 #include "NamedObjs.h"
 #include "Common.h"
 //---------------------------------------------------------------------------
-int NamedObjectSortProc(void *Item1, void *Item2)
+int NamedObjectSortProc(void * Item1, void * Item2)
 {
-    bool HasPrefix1 = (static_cast<TNamedObject *>(Item1))->GetHidden();
-    bool HasPrefix2 = (static_cast<TNamedObject *>(Item2))->GetHidden();
-    if (HasPrefix1 && !HasPrefix2) { return -1; }
-    else if (!HasPrefix1 && HasPrefix2) { return 1; }
-    else
-    {
-        return ::AnsiCompareStr((static_cast<TNamedObject *>(Item1))->GetName(), (static_cast<TNamedObject *>(Item2))->GetName());
-    }
+  bool HasPrefix1 = (static_cast<TNamedObject *>(Item1))->GetHidden();
+  bool HasPrefix2 = (static_cast<TNamedObject *>(Item2))->GetHidden();
+  if (HasPrefix1 && !HasPrefix2) { return -1; }
+  else if (!HasPrefix1 && HasPrefix2) { return 1; }
+  else
+  {
+    return ::AnsiCompareStr((static_cast<TNamedObject *>(Item1))->GetName(), (static_cast<TNamedObject *>(Item2))->GetName());
+  }
 }
 //--- TNamedObject ----------------------------------------------------------
 TNamedObject::TNamedObject(const UnicodeString AName) :
-    TPersistent()
+  TPersistent()
 {
-    SetName(AName);
+  SetName(AName);
 }
 //---------------------------------------------------------------------------
 void TNamedObject::SetName(const UnicodeString value)
 {
-    FHidden = (value.SubString(0, TNamedObjectList::HiddenPrefix.Length()) == TNamedObjectList::HiddenPrefix);
-    FName = value;
+  FHidden = (value.SubString(0, TNamedObjectList::HiddenPrefix.Length()) == TNamedObjectList::HiddenPrefix);
+  FName = value;
 }
 
 int TNamedObject::CompareName(const UnicodeString aName,
                               bool CaseSensitive)
 {
-    // DEBUG_PRINTF(L"CaseSensitive = %d, Name = %s, aName = %s", CaseSensitive, Name.c_str(), aName.c_str());
-    if (CaseSensitive)
-    {
-        return ::AnsiCompare(GetName(), aName);
-    }
-    else
-    {
-        return ::AnsiCompareIC(GetName(), aName);
-    }
+  // DEBUG_PRINTF(L"CaseSensitive = %d, Name = %s, aName = %s", CaseSensitive, Name.c_str(), aName.c_str());
+  if (CaseSensitive)
+  {
+    return ::AnsiCompare(GetName(), aName);
+  }
+  else
+  {
+    return ::AnsiCompareIC(GetName(), aName);
+  }
 }
 //---------------------------------------------------------------------------
-void TNamedObject::MakeUniqueIn(TNamedObjectList *List)
+void TNamedObject::MakeUniqueIn(TNamedObjectList * List)
 {
-    // This object can't be item of list, it would create infinite loop
-    if (List && (List->IndexOf(this) < 0))
-        while (List->FindByName(GetName()))
+  // This object can't be item of list, it would create infinite loop
+  if (List && (List->IndexOf(this) < 0))
+    while (List->FindByName(GetName()))
+    {
+      size_t N = 0, P = 0;
+      // If name already contains number parenthesis remove it (and remember it)
+      UnicodeString Name = GetName();
+      if ((Name[Name.Length() - 1] == L')') && ((P = ::LastDelimiter(Name, L"(")) >= 0))
+        try
         {
-            size_t N = 0, P = 0;
-            // If name already contains number parenthesis remove it (and remember it)
-            UnicodeString Name = GetName();
-            if ((Name[Name.Length() - 1] == L')') && ((P = ::LastDelimiter(Name, L"(")) >= 0))
-                try
-                {
-                    N = StrToInt(Name.SubString(P + 1, Name.Length() - P - 1));
-                    Name.Delete(P, Name.Length() - P + 1);
-                    SetName(::TrimRight(Name));
-                }
-                catch (const std::exception &E)
-                {
-                    N = 0;
-                };
-            Name += L" (" + IntToStr(static_cast<int>(N+1)) + L")";
+          N = StrToInt(Name.SubString(P + 1, Name.Length() - P - 1));
+          Name.Delete(P, Name.Length() - P + 1);
+          SetName(::TrimRight(Name));
         }
+        catch (const std::exception & E)
+        {
+          N = 0;
+        };
+      Name += L" (" + IntToStr(static_cast<int>(N+1)) + L")";
+    }
 }
 //--- TNamedObjectList ------------------------------------------------------
 const UnicodeString TNamedObjectList::HiddenPrefix = L"_!_";
 //---------------------------------------------------------------------------
 TNamedObjectList::TNamedObjectList() :
-    TObjectList(),
-    FHiddenCount(0),
-    AutoSort(true)
+  TObjectList(),
+  FHiddenCount(0),
+  AutoSort(true)
 {
-    AutoSort = true;
+  AutoSort = true;
 }
 //---------------------------------------------------------------------------
-TNamedObject *TNamedObjectList::AtObject(size_t Index)
+TNamedObject * TNamedObjectList::AtObject(size_t Index)
 {
-    // DEBUG_PRINTF(L"Index = %d, Count = %d, GetHiddenCount = %d", Index, GetCount(), GetHiddenCount());
-    return static_cast<TNamedObject *>(GetItem(Index + GetHiddenCount()));
+  // DEBUG_PRINTF(L"Index = %d, Count = %d, GetHiddenCount = %d", Index, GetCount(), GetHiddenCount());
+  return static_cast<TNamedObject *>(GetItem(Index + GetHiddenCount()));
 }
 //---------------------------------------------------------------------------
 void TNamedObjectList::Recount()
 {
-    size_t i = 0;
-    while ((i < TObjectList::GetCount()) && (static_cast<TNamedObject *>(GetItem(i)))->GetHidden()) { i++; }
-    FHiddenCount = i;
+  size_t i = 0;
+  while ((i < TObjectList::GetCount()) && (static_cast<TNamedObject *>(GetItem(i)))->GetHidden()) { i++; }
+  FHiddenCount = i;
 }
 //---------------------------------------------------------------------------
 void TNamedObjectList::AlphaSort()
 {
-    Sort(NamedObjectSortProc);
+  Sort(NamedObjectSortProc);
 }
 //---------------------------------------------------------------------------
-void TNamedObjectList::Notify(void *Ptr, TListNotification Action)
+void TNamedObjectList::Notify(void * Ptr, TListNotification Action)
 {
-    TObjectList::Notify(Ptr, Action);
-    if (AutoSort && (Action == lnAdded)) { AlphaSort(); }
-    Recount();
+  TObjectList::Notify(Ptr, Action);
+  if (AutoSort && (Action == lnAdded)) { AlphaSort(); }
+  Recount();
 }
 //---------------------------------------------------------------------------
-TNamedObject *TNamedObjectList::FindByName(const UnicodeString Name,
-        bool CaseSensitive)
+TNamedObject * TNamedObjectList::FindByName(const UnicodeString Name,
+    bool CaseSensitive)
 {
-    for (size_t Index = 0; Index < TObjectList::GetCount(); Index++)
-        if (!(static_cast<TNamedObject *>(GetItem(Index)))->CompareName(Name, CaseSensitive))
-        {
-            return static_cast<TNamedObject *>(GetItem(Index));
-        }
-    return NULL;
+  for (size_t Index = 0; Index < TObjectList::GetCount(); Index++)
+    if (!(static_cast<TNamedObject *>(GetItem(Index)))->CompareName(Name, CaseSensitive))
+    {
+      return static_cast<TNamedObject *>(GetItem(Index));
+    }
+  return NULL;
 }
 //---------------------------------------------------------------------------
 void TNamedObjectList::SetCount(size_t value)
 {
-    TObjectList::SetCount(value/*+HiddenCount*/);
+  TObjectList::SetCount(value/*+HiddenCount*/);
 }
 //---------------------------------------------------------------------------
 size_t TNamedObjectList::GetCount()
 {
-    return TObjectList::GetCount() - GetHiddenCount();
+  return TObjectList::GetCount() - GetHiddenCount();
 }
--- src/core/NamedObjs.h
+++ src/core/NamedObjs.h
@@ -9,43 +9,43 @@ class TNamedObjectList;
 class TNamedObject : public TPersistent
 {
 public:
-    TNamedObject() : TPersistent() {};
-    explicit TNamedObject(const UnicodeString aName);
-    bool __fastcall GetHidden() { return FHidden; }
-    UnicodeString __fastcall GetName() { return FName; }
-    void __fastcall SetName(const UnicodeString value);
-    int __fastcall CompareName(const UnicodeString aName, bool CaseSensitive = false);
-    void __fastcall MakeUniqueIn(TNamedObjectList *List);
+  TNamedObject() : TPersistent() {};
+  explicit TNamedObject(const UnicodeString aName);
+  bool __fastcall GetHidden() { return FHidden; }
+  UnicodeString __fastcall GetName() { return FName; }
+  void __fastcall SetName(const UnicodeString value);
+  int __fastcall CompareName(const UnicodeString aName, bool CaseSensitive = false);
+  void __fastcall MakeUniqueIn(TNamedObjectList * List);
 private:
-    UnicodeString FName;
-    bool FHidden;
+  UnicodeString FName;
+  bool FHidden;
 };
 
 //---------------------------------------------------------------------------
 class TNamedObjectList : public TObjectList
 {
 private:
-    size_t FHiddenCount;
-    virtual void Notify(void *Ptr, TListNotification Action);
+  size_t FHiddenCount;
+  virtual void Notify(void * Ptr, TListNotification Action);
 protected:
-    void Recount();
+  void Recount();
 public:
-    TNamedObjectList();
+  TNamedObjectList();
 
-    static const UnicodeString HiddenPrefix;
-    static bool IsHidden(TNamedObject *Object);
+  static const UnicodeString HiddenPrefix;
+  static bool IsHidden(TNamedObject * Object);
 
-    bool AutoSort;
+  bool AutoSort;
 
-    void AlphaSort();
-    virtual TNamedObject *AtObject(size_t Index);
-    TNamedObject *FindByName(const UnicodeString Name, bool CaseSensitive = false);
-    size_t GetCount();
-    void SetCount(size_t value);
-    size_t GetHiddenCount() { return FHiddenCount; }
-    void SetHiddenCount(size_t value) { FHiddenCount = value; }
+  void AlphaSort();
+  virtual TNamedObject * AtObject(size_t Index);
+  TNamedObject * FindByName(const UnicodeString Name, bool CaseSensitive = false);
+  size_t GetCount();
+  void SetCount(size_t value);
+  size_t GetHiddenCount() { return FHiddenCount; }
+  void SetHiddenCount(size_t value) { FHiddenCount = value; }
 };
 //---------------------------------------------------------------------------
-int NamedObjectSortProc(void *Item1, void *Item2);
+int NamedObjectSortProc(void * Item1, void * Item2);
 //---------------------------------------------------------------------------
 #endif
