13b34a480a7a43d8dc4d920799c333a000e7e9f0
Michael Lukashov <michael.lukashov@gmail.com>
2012-09-01 00:23:50 +0400

    version info: update

--- src/NetBox/version.h
+++ src/NetBox/version.h
@@ -9,8 +9,8 @@
 #define PLUGIN_VERSION_MAJOR         2
 #define PLUGIN_VERSION_MINOR         1
 #define PLUGIN_VERSION_PATCH         11
-#define PLUGIN_VERSION_BUILD         149
+#define PLUGIN_VERSION_BUILD         150
 
 static const std::wstring NETBOX_VERSION_NUMBER(L"2.1.11");
-static const std::wstring NETBOX_COMPILATION_TIME(L"31.08.2012 23:57:52");
+static const std::wstring NETBOX_COMPILATION_TIME(L"01.09.2012 00:23:42");
 
