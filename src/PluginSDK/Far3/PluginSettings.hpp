b87c54bdd6853667171a9178ce3d05087b1c1944
Michael Lukashov <michael.lukashov@gmail.com>
2012-09-30 12:50:57 +0400

    PluginSettings.hpp: Replace Tabs by spaces

--- src/PluginSDK/Far3/PluginSettings.hpp
+++ src/PluginSDK/Far3/PluginSettings.hpp
@@ -5,139 +5,139 @@
 
 class PluginSettings
 {
-	HANDLE handle;
-	FARAPISETTINGSCONTROL SettingsControl;
+  HANDLE handle;
+  FARAPISETTINGSCONTROL SettingsControl;
 
 public:
 
-	PluginSettings(const GUID &guid, FARAPISETTINGSCONTROL SettingsControl)
-	{
-		this->SettingsControl = SettingsControl;
-		handle = INVALID_HANDLE_VALUE;
+  PluginSettings(const GUID &guid, FARAPISETTINGSCONTROL SettingsControl)
+  {
+    this->SettingsControl = SettingsControl;
+    handle = INVALID_HANDLE_VALUE;
 
-		FarSettingsCreate settings={sizeof(FarSettingsCreate),guid,handle};
-		if (SettingsControl(INVALID_HANDLE_VALUE,SCTL_CREATE,0,&settings))
-			handle = settings.Handle;
-	}
+    FarSettingsCreate settings={sizeof(FarSettingsCreate),guid,handle};
+    if (SettingsControl(INVALID_HANDLE_VALUE,SCTL_CREATE,0,&settings))
+      handle = settings.Handle;
+  }
 
-	~PluginSettings()
-	{
-	    SettingsControl(handle,SCTL_FREE,0,0);
-	}
+  ~PluginSettings()
+  {
+      SettingsControl(handle,SCTL_FREE,0,0);
+  }
 
-	int CreateSubKey(int Root, const wchar_t *Name)
-	{
-		FarSettingsValue value={Root,Name};
-		return (int)SettingsControl(handle,SCTL_CREATESUBKEY,0,&value);
-	}
+  int CreateSubKey(int Root, const wchar_t *Name)
+  {
+    FarSettingsValue value={sizeof(FarSettingsValue),Root,Name};
+    return (int)SettingsControl(handle,SCTL_CREATESUBKEY,0,&value);
+  }
 
-	int OpenSubKey(int Root, const wchar_t *Name)
-	{
-		FarSettingsValue value={Root,Name};
-		return (int)SettingsControl(handle,SCTL_OPENSUBKEY,0,&value);
-	}
+  int OpenSubKey(int Root, const wchar_t *Name)
+  {
+    FarSettingsValue value={sizeof(FarSettingsValue),Root,Name};
+    return (int)SettingsControl(handle,SCTL_OPENSUBKEY,0,&value);
+  }
 
-	bool DeleteSubKey(int Root)
-	{
-		FarSettingsValue value={Root,nullptr};
-		return (int)SettingsControl(handle,SCTL_DELETE,0,&value) ? true : false;
-	}
+  bool DeleteSubKey(int Root)
+  {
+    FarSettingsValue value={sizeof(FarSettingsValue),Root,nullptr};
+    return (int)SettingsControl(handle,SCTL_DELETE,0,&value) ? true : false;
+  }
 
-	bool DeleteValue(int Root, const wchar_t *Name)
-	{
-		FarSettingsValue value={Root,Name};
-		return (int)SettingsControl(handle,SCTL_DELETE,0,&value) ? true : false;
-	}
+  bool DeleteValue(int Root, const wchar_t *Name)
+  {
+    FarSettingsValue value={sizeof(FarSettingsValue),Root,Name};
+    return (int)SettingsControl(handle,SCTL_DELETE,0,&value) ? true : false;
+  }
 
-	const wchar_t *Get(int Root, const wchar_t *Name, const wchar_t *Default)
-	{
-		FarSettingsItem item={Root,Name,FST_STRING};
-		if (SettingsControl(handle,SCTL_GET,0,&item))
-		{
-			return item.String;
-		}
-		return Default;
-	}
-
-	void Get(int Root, const wchar_t *Name, wchar_t *Value, size_t Size, const wchar_t *Default)
-	{
-		lstrcpyn(Value, Get(Root,Name,Default), (int)Size);
-	}
-
-	unsigned __int64 Get(int Root, const wchar_t *Name, unsigned __int64 Default)
-	{
-		FarSettingsItem item={Root,Name,FST_QWORD};
-		if (SettingsControl(handle,SCTL_GET,0,&item))
-		{
-			return item.Number;
-		}
-		return Default;
-	}
-
-	__int64      Get(int Root, const wchar_t *Name, __int64 Default) { return (__int64)Get(Root,Name,(unsigned __int64)Default); }
-	int          Get(int Root, const wchar_t *Name, int Default)  { return (int)Get(Root,Name,(unsigned __int64)Default); }
-	unsigned int Get(int Root, const wchar_t *Name, unsigned int Default) { return (unsigned int)Get(Root,Name,(unsigned __int64)Default); }
-	DWORD        Get(int Root, const wchar_t *Name, DWORD Default) { return (DWORD)Get(Root,Name,(unsigned __int64)Default); }
-	bool         Get(int Root, const wchar_t *Name, bool Default) { return Get(Root,Name,Default?1ull:0ull)?true:false; }
-
-	size_t Get(int Root, const wchar_t *Name, void *Value, size_t Size)
-	{
-		FarSettingsItem item={Root,Name,FST_DATA};
-		if (SettingsControl(handle,SCTL_GET,0,&item))
-		{
-			Size = (item.Data.Size>Size)?Size:item.Data.Size;
-			memcpy(Value,item.Data.Data,Size);
-			return Size;
-		}
-		return 0;
-	}
-
-	bool Set(int Root, const wchar_t *Name, const wchar_t *Value)
-	{
-		FarSettingsItem item={Root,Name,FST_STRING};
-		item.String=Value;
-		return SettingsControl(handle,SCTL_SET,0,&item)!=FALSE;
-	}
-
-	bool Set(int Root, const wchar_t *Name, unsigned __int64 Value)
-	{
-		FarSettingsItem item={Root,Name,FST_QWORD};
-		item.Number=Value;
-		return SettingsControl(handle,SCTL_SET,0,&item)!=FALSE;
-	}
-
-	bool Set(int Root, const wchar_t *Name, __int64 Value) { return Set(Root,Name,(unsigned __int64)Value); }
-	bool Set(int Root, const wchar_t *Name, int Value) { return Set(Root,Name,(unsigned __int64)Value); }
-	bool Set(int Root, const wchar_t *Name, unsigned int Value) { return Set(Root,Name,(unsigned __int64)Value); }
-	bool Set(int Root, const wchar_t *Name, DWORD Value) { return Set(Root,Name,(unsigned __int64)Value); }
-	bool Set(int Root, const wchar_t *Name, bool Value) { return Set(Root,Name,Value?1ull:0ull); }
-
-	bool Set(int Root, const wchar_t *Name, const void *Value, size_t Size)
-	{
-		FarSettingsItem item={Root,Name,FST_DATA};
-		item.Data.Size=Size;
-		item.Data.Data=Value;
-		return SettingsControl(handle,SCTL_SET,0,&item)!=FALSE;
-	}
-
-    bool GetSubKeyNames(FarSettingsEnum &settings)
+  const wchar_t *Get(int Root, const wchar_t *Name, const wchar_t *Default)
+  {
+    FarSettingsItem item={sizeof(FarSettingsItem),Root,Name,FST_STRING};
+    if (SettingsControl(handle,SCTL_GET,0,&item))
     {
-          return SettingsControl(handle, SCTL_ENUM, 0, &settings) != FALSE;
+      return item.String;
     }
-    bool ValueExists(int Root, const wchar_t *Name)
+    return Default;
+  }
+
+  void Get(int Root, const wchar_t *Name, wchar_t *Value, size_t Size, const wchar_t *Default)
+  {
+    lstrcpyn(Value, Get(Root,Name,Default), (int)Size);
+  }
+
+  unsigned __int64 Get(int Root, const wchar_t *Name, unsigned __int64 Default)
+  {
+    FarSettingsItem item={sizeof(FarSettingsItem),Root,Name,FST_QWORD};
+    if (SettingsControl(handle,SCTL_GET,0,&item))
     {
-      FarSettingsItem item = {Root, Name, FST_DATA};
-      return SettingsControl(handle, SCTL_GET, 0, &item) != FALSE;
+      return item.Number;
     }
-    int BinaryDataSize(int Root, const wchar_t *Name)
+    return Default;
+  }
+
+  __int64      Get(int Root, const wchar_t *Name, __int64 Default) { return (__int64)Get(Root,Name,(unsigned __int64)Default); }
+  int          Get(int Root, const wchar_t *Name, int Default)  { return (int)Get(Root,Name,(unsigned __int64)Default); }
+  unsigned int Get(int Root, const wchar_t *Name, unsigned int Default) { return (unsigned int)Get(Root,Name,(unsigned __int64)Default); }
+  DWORD        Get(int Root, const wchar_t *Name, DWORD Default) { return (DWORD)Get(Root,Name,(unsigned __int64)Default); }
+  bool         Get(int Root, const wchar_t *Name, bool Default) { return Get(Root,Name,Default?1ull:0ull)?true:false; }
+
+  size_t Get(int Root, const wchar_t *Name, void *Value, size_t Size)
+  {
+    FarSettingsItem item={sizeof(FarSettingsItem),Root,Name,FST_DATA};
+    if (SettingsControl(handle,SCTL_GET,0,&item))
     {
-      FarSettingsItem item = {Root, Name, FST_DATA};
-      if (SettingsControl(handle, SCTL_GET, 0, &item) != FALSE)
-      {
-         return item.Data.Size;
-      }
-      return 0;
+      Size = (item.Data.Size>Size)?Size:item.Data.Size;
+      memcpy(Value,item.Data.Data,Size);
+      return Size;
     }
+    return 0;
+  }
+
+  bool Set(int Root, const wchar_t *Name, const wchar_t *Value)
+  {
+    FarSettingsItem item={sizeof(FarSettingsItem),Root,Name,FST_STRING};
+    item.String=Value;
+    return SettingsControl(handle,SCTL_SET,0,&item)!=FALSE;
+  }
+
+  bool Set(int Root, const wchar_t *Name, unsigned __int64 Value)
+  {
+    FarSettingsItem item={sizeof(FarSettingsItem),Root,Name,FST_QWORD};
+    item.Number=Value;
+    return SettingsControl(handle,SCTL_SET,0,&item)!=FALSE;
+  }
+
+  bool Set(int Root, const wchar_t *Name, __int64 Value) { return Set(Root,Name,(unsigned __int64)Value); }
+  bool Set(int Root, const wchar_t *Name, int Value) { return Set(Root,Name,(unsigned __int64)Value); }
+  bool Set(int Root, const wchar_t *Name, unsigned int Value) { return Set(Root,Name,(unsigned __int64)Value); }
+  bool Set(int Root, const wchar_t *Name, DWORD Value) { return Set(Root,Name,(unsigned __int64)Value); }
+  bool Set(int Root, const wchar_t *Name, bool Value) { return Set(Root,Name,Value?1ull:0ull); }
+
+  bool Set(int Root, const wchar_t *Name, const void *Value, size_t Size)
+  {
+    FarSettingsItem item={sizeof(FarSettingsItem),Root,Name,FST_DATA};
+    item.Data.Size=Size;
+    item.Data.Data=Value;
+    return SettingsControl(handle,SCTL_SET,0,&item)!=FALSE;
+  }
+
+  bool GetSubKeyNames(FarSettingsEnum &settings)
+  {
+        return SettingsControl(handle, SCTL_ENUM, 0, &settings) != FALSE;
+  }
+  bool ValueExists(int Root, const wchar_t *Name)
+  {
+    FarSettingsItem item = {Root, Name, FST_DATA};
+    return SettingsControl(handle, SCTL_GET, 0, &item) != FALSE;
+  }
+  int BinaryDataSize(int Root, const wchar_t *Name)
+  {
+    FarSettingsItem item = {Root, Name, FST_DATA};
+    if (SettingsControl(handle, SCTL_GET, 0, &item) != FALSE)
+    {
+       return item.Data.Size;
+    }
+    return 0;
+  }
 };
 
 #endif
