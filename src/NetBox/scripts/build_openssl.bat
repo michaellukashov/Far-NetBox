521df584dc7a46dab6acc96ff0e4c52af6fa032c
Michael Lukashov <michael.lukashov@gmail.com>
2012-11-10 12:52:09 +0300

    build_openssl.bat: Remove unused ciphers

--- src/NetBox/scripts/build_openssl.bat
+++ src/NetBox/scripts/build_openssl.bat
@@ -10,23 +10,22 @@ if "%1" == "x64" goto x64
 goto x86
 
 :x64
-rm -rf x64
+rm -rf x64 inc32 out32
 call %VS100COMNTOOLS%\..\..\VC\vcvarsall.bat x86_amd64
-perl Configure VC-WIN64A no-asm enable-static-engine 2>&1 | tee res.txt
-ms\do_win64a 2>&1 | tee -a res.txt
-nmake -f ms\nt.mak 2>&1 | tee -a res.txt
+perl Configure VC-WIN64A enable-static-engine no-shared no-hw no-camellia no-seed no-mdc2 no-rc4 no-rc5 no-cms no-krb5 no-ec no-whirlpool no-srp no-gost no-idea no-ripemd no-des no-asn1 -O1 -Ob2 -Oi -Os -Oy -GL -GF -GS- -Gy  -DNDEBUG;BN_ASM;OPENSSL_NO_CAPIENG;NO_CHMOD;OPENSSL_NO_DGRAM;OPENSSL_NO_RIJNDAEL;DSO_WIN32
+call ms\do_win64a
+nmake -f ms\nt.mak
 mkdir x64
 cp out32/ssleay32.lib out32/libeay32.lib x64
 cp -R inc32 x64
 goto end
 
 :x86
-rm -rf x86
+rm -rf x86 inc32 out32
 call %VS100COMNTOOLS%\..\..\VC\vcvarsall.bat x86
-
-perl Configure VC-WIN32 enable-static-engine 2>&1 | tee res.txt
-ms\do_nasm 2>&1 | tee -a res.txt
-nmake -f ms\nt.mak 2>&1 | tee -a res.txt
+perl Configure VC-WIN32 enable-static-engine no-shared no-hw no-camellia no-seed no-mdc2 no-rc4 no-rc5 no-cms no-krb5 no-ec no-whirlpool no-srp no-gost no-idea no-ripemd no-des no-asn1 -Ob2 -Oi -Os -Oy -GL -GF -GS- -Gy -arch:SSE2 -DNDEBUG;BN_ASM;OPENSSL_NO_CAPIENG;NO_CHMOD;OPENSSL_NO_DGRAM;OPENSSL_NO_RIJNDAEL;DSO_WIN32
+call ms\do_nasm
+nmake -f ms\nt.mak
 mkdir x86
 cp out32/ssleay32.lib out32/libeay32.lib x86
 cp -R inc32 x86
