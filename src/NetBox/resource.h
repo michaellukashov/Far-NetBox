73e95fab4e0a8eed87125c764c428cfc93d8d198
Michael Lukashov <michael.lukashov@gmail.com>
2012-12-07 19:58:02 +0300

    Update WinSCP version info

--- src/NetBox/resource.h
+++ src/NetBox/resource.h
@@ -9,9 +9,8 @@
 #define PLUGIN_FILENAME     L"NetBox.dll"
 
 #define WINSCP_PLUGIN_VERSION_WTXT  L"1.6.2.151"
-#define WINSCP_VERSION_WTXT         L"5.1.1"
+#define WINSCP_VERSION_WTXT         L"5.1.2"
 #define PUTTY_VERSION_WTXT          L"0.62"
 #define FILEZILLA_VERSION_WTXT      L"2.2.32"
 #define ZLIB_VERSION_WTXT           L"1.2.7"
 #define OPENSSL_VERSION_WTXT        L"1.0.1c"
-#define BOOST_VERSION_WTXT          L"1.48.0"
