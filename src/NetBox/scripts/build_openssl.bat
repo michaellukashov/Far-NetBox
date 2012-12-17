fa266b76ced71a1062e55425d82a823886476144
Michael Lukashov <michael.lukashov@gmail.com>
2012-12-17 23:23:30 +0300

    build_openssl.bat: Remove -arch:SSE2

--- src/NetBox/scripts/build_openssl.bat
+++ src/NetBox/scripts/build_openssl.bat
@@ -23,7 +23,7 @@ goto end
 :x86
 rm -rf x86 inc32 out32
 call %VS100COMNTOOLS%\..\..\VC\vcvarsall.bat x86
-perl Configure VC-WIN32 enable-static-engine no-shared no-hw no-camellia no-seed no-mdc2 no-rc4 no-rc5 no-cms no-krb5 no-ec no-whirlpool no-srp no-gost no-idea no-ripemd no-des no-asn1 -Ob2 -Oi -Os -Oy -GL -GF -GS- -Gy -arch:SSE2 -DNDEBUG;BN_ASM;OPENSSL_NO_CAPIENG;NO_CHMOD;OPENSSL_NO_DGRAM;OPENSSL_NO_RIJNDAEL;DSO_WIN32
+perl Configure VC-WIN32 enable-static-engine no-shared no-hw no-camellia no-seed no-mdc2 no-rc4 no-rc5 no-cms no-krb5 no-ec no-whirlpool no-srp no-gost no-idea no-ripemd no-des no-asn1 -Ob2 -Oi -Os -Oy -GL -GF -GS- -Gy -DNDEBUG;BN_ASM;OPENSSL_NO_CAPIENG;NO_CHMOD;OPENSSL_NO_DGRAM;OPENSSL_NO_RIJNDAEL;DSO_WIN32
 call ms\do_nasm
 nmake -f ms\nt.mak
 mkdir x86
