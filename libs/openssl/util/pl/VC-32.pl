bdf80070312d020e236bc31fc6735869fecfd026
Michael Lukashov <michael.lukashov@gmail.com>
2012-12-24 12:10:43 +0400

    openssl: Improvements

--- libs/openssl/util/pl/VC-32.pl
+++ libs/openssl/util/pl/VC-32.pl
@@ -47,7 +47,7 @@ if ($FLAVOR =~ /WIN64/)
     $lib_cflag='/Zl' if (!$shlib);	# remove /DEFAULTLIBs from static lib
     $opt_cflags=$f.' /Ox';
     $dbg_cflags=$f.'d /Od -DDEBUG -D_DEBUG';
-    $lflags="/nologo /subsystem:console /opt:ref /opt:icf /LTCG";
+    $lflags="/nologo /subsystem:console /opt:ref /opt:icf";
 
     *::perlasm_compile_target = sub {
 	my ($target,$source,$bname)=@_;
@@ -126,9 +126,9 @@ else	# Win32
     $base_cflags= " $mf_cflag";
     my $f = $shlib || $fips ?' /MD':' /MT';
     $lib_cflag='/Zl' if (!$shlib);	# remove /DEFAULTLIBs from static lib
-    $opt_cflags=$f.' /Ox /O2 /Ob2';
+    $opt_cflags=$f.' /Ox /O2 /Ob1';
     $dbg_cflags=$f.'d /Od -DDEBUG -D_DEBUG';
-    $lflags="/nologo /subsystem:console /opt:ref /opt:icf /LTCG";
+    $lflags="/nologo /subsystem:console /opt:ref /opt:icf";
     }
 $mlflags='';
 
--- src/NetBox/scripts/build_openssl.bat
+++ src/NetBox/scripts/build_openssl.bat
@@ -12,7 +12,7 @@ goto x86
 :x64
 rm -rf x64 inc32 out32
 call %VS100COMNTOOLS%\..\..\VC\vcvarsall.bat x86_amd64
-perl Configure VC-WIN64A no-asm enable-static-engine no-shared no-hw no-camellia no-seed no-mdc2 no-rc4 no-rc5 no-cms no-krb5 no-ec no-whirlpool no-srp no-gost no-idea no-ripemd no-des -O2 -Ob2 -Oi -Os -Oy -GL -GF -GS- -Gy  -DNDEBUG;OPENSSL_NO_CAPIENG;NO_CHMOD;OPENSSL_NO_DGRAM;OPENSSL_NO_RIJNDAEL;DSO_WIN32
+perl Configure VC-WIN64A no-asm enable-static-engine no-shared no-hw no-camellia no-seed no-mdc2 no-rc4 no-rc5 no-cms no-krb5 no-ec no-whirlpool no-srp no-gost no-idea no-ripemd no-des -Ox -Ob1 -Oi -Os -Oy -GF -GS- -Gy  -DNDEBUG;OPENSSL_NO_CAPIENG;NO_CHMOD;OPENSSL_NO_DGRAM;OPENSSL_NO_RIJNDAEL;DSO_WIN32
 call ms\do_win64a
 nmake -f ms\nt.mak
 mkdir x64
@@ -23,8 +23,9 @@ goto end
 :x86
 rm -rf x86 inc32 out32
 call %VS100COMNTOOLS%\..\..\VC\vcvarsall.bat x86
-perl Configure VC-WIN32 enable-static-engine no-shared no-hw no-camellia no-seed no-mdc2 no-rc4 no-rc5 no-cms no-krb5 no-ec no-whirlpool no-srp no-gost no-idea no-ripemd no-des -Ob2 -Oi -Os -Oy -GL -GF -GS- -Gy -DNDEBUG;BN_ASM;OPENSSL_NO_CAPIENG;NO_CHMOD;OPENSSL_NO_DGRAM;OPENSSL_NO_RIJNDAEL;DSO_WIN32
-call ms\do_nasm
+perl Configure VC-WIN32 no-asm enable-static-engine no-shared no-hw no-camellia no-seed no-mdc2 no-rc4 no-rc5 no-cms no-krb5 no-ec no-whirlpool no-srp no-gost no-idea no-ripemd no-des -Ox -Ob1 -Oi -Os -Oy -GF -GS- -Gy -DNDEBUG;BN_ASM;OPENSSL_NO_CAPIENG;NO_CHMOD;OPENSSL_NO_DGRAM;OPENSSL_NO_RIJNDAEL;DSO_WIN32
+rem call ms\do_nasm
+call ms\do_ms
 nmake -f ms\nt.mak
 mkdir x86
 cp out32/ssleay32.lib out32/libeay32.lib x86
