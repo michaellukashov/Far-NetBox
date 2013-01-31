c11d96cbba34eed9f81591ffcde1b0be34eb9ab3
Michael Lukashov <michael.lukashov@gmail.com>
2013-01-31 20:06:52 +0300

    version info: update

--- src/NetBox/resource.h
+++ src/NetBox/resource.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define PLUGIN_VERSION_NUM 2,1,23,209
-#define PLUGIN_VERSION_TXT "2.1.23"
-#define PLUGIN_VERSION_WTXT L"2.1.23"
+#define PLUGIN_VERSION_NUM 2,1,24,210
+#define PLUGIN_VERSION_TXT "2.1.24"
+#define PLUGIN_VERSION_WTXT L"2.1.24"
 
 #define PLUGIN_AUTHOR       L"Michael Lukashov"
 #define PLUGIN_DESCR        L"SFTP/FTP(S)/SCP/WebDAV client for FAR 2.0/3.0 x86/x64"
