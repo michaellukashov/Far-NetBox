2eeddb6f46c46fad59ebcad983ac37c2507d2926
Michael Lukashov <michael.lukashov@gmail.com>
2013-02-09 16:49:17 +0300

    compile_setup.bat: Cleanup

--- release/compile_setup.bat
+++ release/compile_setup.bat
@@ -1,7 +1,5 @@
-rem Run from project root NetBox
 SET ISCC="C:\Program Files\Inno Setup 5\ISCC.exe"
 if exist %ISCC% (
-  %ISCC% /v3 release/netboxsetup.iss
-  REM "/dFAR_VERSION=Far2" "/dROOT_DIR=.." "/dSOURCE_DIR=../src/NetBox" "/dPUTTY_SOURCE_DIR=C:/Program Files/Putty"
+  %ISCC% /v3 netboxsetup.iss
   if errorlevel 1 echo Error creating distributive & exit 1 /b
 )
