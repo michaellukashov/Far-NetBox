6c48ae10c9a1ef34fe37334f6fddfd96b198431b
Michael Lukashov <michael.lukashov@gmail.com>
2013-04-03 18:31:46 +0400

    SftpFileSystem.cpp: Fix minor warnings

--- src/core/SftpFileSystem.h
+++ src/core/SftpFileSystem.h
@@ -116,7 +116,7 @@ protected:
     int AllowStatus = -1);
   virtual UnicodeString GetCurrentDirectory();
   UnicodeString GetHomeDirectory();
-  unsigned long GotStatusPacket(TSFTPPacket * Packet, int AllowStatus);
+  rde::uint32 GotStatusPacket(TSFTPPacket * Packet, int AllowStatus);
   bool IsAbsolutePath(const UnicodeString & Path);
   bool RemoteFileExists(const UnicodeString & FullPath, TRemoteFile ** File = NULL);
   TRemoteFile * LoadFile(TSFTPPacket * Packet,
