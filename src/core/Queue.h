0af31a5cde19a18fbbb7823f74cb75f22b1a04a9
Michael Lukashov <michael.lukashov@gmail.com>
2013-04-15 13:33:59 +0400

    Sync with WinSCP 5.2.0

--- src/core/Queue.h
+++ src/core/Queue.h
@@ -246,8 +246,6 @@ public:
   bool GetProcessingUserAction() const { return FProcessingUserAction; }
   void * GetUserData() { return FUserData; }
   void SetUserData(void * Value) { FUserData = Value; }
-  TFileOperationProgressType * GetProgressData();
-  __int64 GetTotalTransferred();
 
 private:
   TFileOperationProgressType * FProgressData;
@@ -278,9 +276,6 @@ public:
 
   TQueueItemProxy * FindByQueueItem(TQueueItem * QueueItem);
 
-  intptr_t GetDoneCount() const { return FDoneCount; }
-  intptr_t GetDoneAndActiveCount() const;
-  void SetDoneCount(intptr_t Value);
 protected:
   TTerminalQueueStatus();
 
