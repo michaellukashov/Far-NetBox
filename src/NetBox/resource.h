3548e59a56914b428337e9e5d8d43527bda2fe27
Michael Lukashov <michael.lukashov@gmail.com>
2013-04-29 15:42:49 +0400

    Update zlib info

--- src/NetBox/resource.h
+++ src/NetBox/resource.h
@@ -12,5 +12,5 @@
 #define WINSCP_VERSION_WTXT         L"5.2.0"
 #define PUTTY_VERSION_WTXT          L"0.62"
 #define FILEZILLA_VERSION_WTXT      L"2.2.32"
-#define ZLIB_VERSION_WTXT           L"1.2.7"
+#define ZLIB_VERSION_WTXT           L"1.2.8"
 #define OPENSSL_VERSION_WTXT        L"1.0.1e"
