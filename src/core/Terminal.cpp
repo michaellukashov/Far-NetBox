55a85e6fc8ed271d36648230cb4b831a3bd9a57a
Michael Lukashov <michael.lukashov@gmail.com>
2013-05-20 04:25:42 +0400

    TTerminal::DoPromptUser: Bugfix.

--- src/core/Terminal.cpp
+++ src/core/Terminal.cpp
@@ -1137,7 +1137,7 @@ bool TTerminal::DoPromptUser(TSessionData * /*Data*/, TPromptKind Kind,
   if (Result && GetConfiguration()->GetRememberPassword() &&
     (Prompts->GetCount() == 1) && (Prompts->GetObject(0) == NULL) &&
     ((Kind == pkPassword) || (Kind == pkPassphrase) || (Kind == pkKeybInteractive) ||
-     (Kind == pkTIS) || (Kind == pkCryptoCard)));
+     (Kind == pkTIS) || (Kind == pkCryptoCard)))
   {
     RawByteString EncryptedPassword = EncryptPassword(Results->GetString(0));
     if (FTunnelOpening)
