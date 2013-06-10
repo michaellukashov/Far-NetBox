49c0eb5977ef3bba3d0efbd68109912523dd5aa5
Michael Lukashov <michael.lukashov@gmail.com>
2013-06-10 14:17:35 +0400

    Fix minor code style.

--- src/NetBox/WinSCPPlugin.cpp
+++ src/NetBox/WinSCPPlugin.cpp
@@ -220,7 +220,7 @@ intptr_t TWinSCPPlugin::ProcessEditorInputEx(const INPUT_RECORD * Rec)
       (Rec->Event.KeyEvent.uChar.AsciiChar == 'W') &&
       (FLAGSET(Rec->Event.KeyEvent.dwControlKeyState, LEFT_ALT_PRESSED) ||
        FLAGSET(Rec->Event.KeyEvent.dwControlKeyState, RIGHT_ALT_PRESSED)) &&
-      FLAGSET(Rec->Event.KeyEvent.dwControlKeyState, SHIFT_PRESSED))
+       FLAGSET(Rec->Event.KeyEvent.dwControlKeyState, SHIFT_PRESSED))
   {
     CommandsMenu(false);
     Result = 1;
