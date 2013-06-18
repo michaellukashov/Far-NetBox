57a0f298205d5e3f28d2ddc6f1f43d82094c62e3
Michael Lukashov <michael.lukashov@gmail.com>
2013-06-18 21:58:23 +0400

    version info: update

--- src/NetBox/plugin_version.hpp
+++ src/NetBox/plugin_version.hpp
@@ -9,8 +9,8 @@
 #define NETBOX_VERSION_MAJOR         2
 #define NETBOX_VERSION_MINOR         1
 #define NETBOX_VERSION_PATCH         31
-#define NETBOX_VERSION_BUILD         266
+#define NETBOX_VERSION_BUILD         267
 
 static const std::wstring NETBOX_VERSION_NUMBER(L"2.1.31");
-static const std::wstring NETBOX_COMPILATION_TIME(L"18.06.2013 21:27:04");
+static const std::wstring NETBOX_COMPILATION_TIME(L"18.06.2013 21:58:15");
 
