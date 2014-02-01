6f5ce809fcf478d315f931a424d7fd0bdeb93d3e
Michael Lukashov <michael.lukashov@gmail.com>
2014-02-01 21:43:20 +0400

    update openssl to 1.0.1f

--- libs/openssl/util/pl/VC-32.pl
+++ libs/openssl/util/pl/VC-32.pl
@@ -47,7 +47,7 @@ if ($FLAVOR =~ /WIN64/)
     $lib_cflag='/Zl' if (!$shlib);	# remove /DEFAULTLIBs from static lib
     $opt_cflags=$f.' /Ox';
     $dbg_cflags=$f.'d /Od -DDEBUG -D_DEBUG';
-    $lflags="/nologo /subsystem:console /opt:ref /opt:icf";
+    $lflags="/nologo /subsystem:console /opt:ref";
 
     *::perlasm_compile_target = sub {
 	my ($target,$source,$bname)=@_;
@@ -126,9 +126,9 @@ else	# Win32
     $base_cflags= " $mf_cflag";
     my $f = $shlib || $fips ?' /MD':' /MT';
     $lib_cflag='/Zl' if (!$shlib);	# remove /DEFAULTLIBs from static lib
-    $opt_cflags=$f.' /Ox /O2 /Ob1';
+    $opt_cflags=$f.' /Ox /O2 /Ob2';
     $dbg_cflags=$f.'d /Od -DDEBUG -D_DEBUG';
-    $lflags="/nologo /subsystem:console /opt:ref /opt:icf";
+    $lflags="/nologo /subsystem:console /opt:ref";
     }
 $mlflags='';
 
