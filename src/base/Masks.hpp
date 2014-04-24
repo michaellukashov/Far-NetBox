9a324eda9859957c421d790066759601c91bae35
Michael Lukashov <michael.lukashov@gmail.com>
2014-04-24 18:35:32 +0400

    bugfix: "Transfer settings..." -> File mask: file mask wasn't handled
    (http://forum.farmanager.com/viewtopic.php?f=5&t=6317&p=118264#p118264)

--- src/base/Masks.hpp
+++ src/base/Masks.hpp
@@ -7,19 +7,14 @@ namespace Masks {
 class TMask : public TObject
 {
 public:
-  explicit TMask(const UnicodeString & Mask)
-  {
-    FMask = ::ExtractFileExtension(Mask, L'\\');
-  }
-  bool Matches(const UnicodeString & Str)
-  {
-    UnicodeString Ext = ::ExtractFileExtension(Str, L'\\');
-    return Sysutils::AnsiCompareIC(FMask, Ext) == 0;
-  }
+  explicit TMask(const UnicodeString & Mask);
+  bool Matches(const UnicodeString & Str);
 
 private:
   UnicodeString FMask;
 };
+
+int CmpName(const wchar_t *pattern, const wchar_t *str, bool CmpNameSearchMode=false);
 
 } // namespace Masks
 
