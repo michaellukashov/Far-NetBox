91b0c068a038b94dae62a0c2c7c06a8dea218434
Michael Lukashov <michael.lukashov@gmail.com>
2014-07-06 17:26:21 +0400

    fix AV
    (http://forum.farmanager.com/viewtopic.php?f=5&t=6317&start=1632#p120025)

--- src/NetBox/WinSCPPlugin.cpp
+++ src/NetBox/WinSCPPlugin.cpp
@@ -340,6 +340,8 @@ void TWinSCPPlugin::ParseCommandLine(UnicodeString & CommandLine,
   {
     while ((Index < CmdLine.Length()) && (CmdLine[Index] == L' '))
      ++Index;
+    if (Index >= CmdLine.Length())
+      return;
     if (CmdLine[Index] == L'"')
     {
       ++Index;
