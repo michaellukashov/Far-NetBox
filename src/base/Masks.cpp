c902ff1c6d98b10788d262eb7495ab18769910c1
Michael Lukashov <michael.lukashov@gmail.com>
2014-07-20 16:05:48 +0400

    Masks.cpp: cleanup

--- src/base/Masks.cpp
+++ src/base/Masks.cpp
@@ -135,8 +135,6 @@ TMask::TMask(const UnicodeString & Mask)
 
 bool TMask::Matches(const UnicodeString & Str)
 {
-  // UnicodeString Ext = ::ExtractFileExtension(Str, L'\\');
-  //return Sysutils::AnsiCompareIC(FMask, Ext) == 0;
   return CmpName(FMask.c_str(), Str.c_str()) == TRUE;
 }
 
