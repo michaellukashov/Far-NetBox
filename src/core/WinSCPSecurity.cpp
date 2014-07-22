fb99e5b21ffa407fe978109eb2e80db09abc3328
Michael Lukashov <michael.lukashov@gmail.com>
2014-07-22 10:07:29 +0400

    fix warnings: conversion from 'size_t' to 'DWORD', possible loss of data

--- src/core/WinSCPSecurity.cpp
+++ src/core/WinSCPSecurity.cpp
@@ -118,7 +118,7 @@ bool WindowsValidateCertificate(const uint8_t * Certificate, size_t Len)
   // Parse the certificate into a context.
   const CERT_CONTEXT * CertContext =
     CertCreateCertificateContext(
-      X509_ASN_ENCODING | PKCS_7_ASN_ENCODING, Certificate, Len);
+      X509_ASN_ENCODING | PKCS_7_ASN_ENCODING, Certificate, static_cast<DWORD>(Len));
 
   if (CertContext != NULL)
   {
