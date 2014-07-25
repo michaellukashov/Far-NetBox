280c9dd3bee08e170f7daa5af93f23c5b9c842a1
Michael Lukashov <michael.lukashov@gmail.com>
2014-07-25 16:15:31 +0400

    base: update to WinSCP 5.6.1

--- src/base/StrUtils.cpp
+++ src/base/StrUtils.cpp
@@ -15,3 +15,10 @@ bool StartsStr(const UnicodeString & SubStr, const UnicodeString & Str)
 {
   return Str.Pos(SubStr) == 1;
 }
+
+bool EndsStr(const UnicodeString & SubStr, const UnicodeString & Str)
+{
+  if (SubStr.Length() > Str.Length())
+    return false;
+  return Str.SubStr(Str.Length() - SubStr.Length() + 1, SubStr.Length()) == SubStr;
+}
