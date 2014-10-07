e2243002c80243db267cc67560217e38afba24d9
Michael Lukashov <michael.lukashov@gmail.com>
2014-10-07 21:27:38 +0400

    local.cpp: remove unused code

--- src/base/local.hpp
+++ src/base/local.hpp
@@ -52,49 +52,13 @@ extern int Win32BuildNumber;
 
 inline intptr_t __cdecl StrLength(const wchar_t *str) { return wcslen(str ? str : L""); }
 
-inline int IsSpace(wchar_t x) { return x==L' ' || x==L'\t';  }
-
-inline int IsEol(wchar_t x) { return x==L'\r' || x==L'\n'; }
-
-inline int IsSpaceOrEos(wchar_t x) { return !x || x==L' ' || x==L'\t'; }
-
 inline wchar_t __cdecl Upper(wchar_t Ch) { CharUpperBuff(&Ch, 1); return Ch; }
 
 inline wchar_t __cdecl Lower(wchar_t Ch) { CharLowerBuff(&Ch, 1); return Ch; }
 
 inline int __cdecl StrCmpNNI(const wchar_t *s1, int n1, const wchar_t *s2, int n2) { return CompareString(0,NORM_IGNORECASE|NORM_STOP_ON_NULL|SORT_STRINGSORT,s1,n1,s2,n2)-2; }
-inline int __cdecl FarStrCmpNI(const wchar_t *s1, const wchar_t *s2, int n) { return StrCmpNNI(s1,n,s2,n); }
 
 inline int __cdecl FarStrCmpI(const wchar_t *s1, const wchar_t *s2) { return CompareString(0,NORM_IGNORECASE|SORT_STRINGSORT,s1,-1,s2,-1)-2; }
 
 inline int __cdecl StrCmpNN(const wchar_t *s1, int n1, const wchar_t *s2, int n2) { return CompareString(0,NORM_STOP_ON_NULL|SORT_STRINGSORT,s1,n1,s2,n2)-2; }
-inline int __cdecl FarStrCmpN(const wchar_t *s1, const wchar_t *s2, int n) { return StrCmpNN(s1,n,s2,n); }
 
-inline int __cdecl FarStrCmp(const wchar_t *s1, const wchar_t *s2) { return CompareString(0,SORT_STRINGSORT,s1,-1,s2,-1)-2; }
-
-inline int __cdecl IsUpper(wchar_t Ch) { return IsCharUpper(Ch); }
-
-inline int __cdecl IsLower(wchar_t Ch) { return IsCharLower(Ch); }
-
-inline int __cdecl IsAlpha(wchar_t Ch) { return IsCharAlpha(Ch); }
-
-inline int __cdecl IsAlphaNum(wchar_t Ch) { return IsCharAlphaNumeric(Ch); }
-
-inline void __cdecl UpperBuf(wchar_t *Buf, intptr_t Length) { CharUpperBuff(Buf, (DWORD)Length); }
-
-inline void __cdecl LowerBuf(wchar_t *Buf, intptr_t Length) { CharLowerBuff(Buf, (DWORD)Length); }
-
-inline void __cdecl StrUpper(wchar_t *s1) { UpperBuf(s1, StrLength(s1)); }
-
-inline void __cdecl StrLower(wchar_t *s1) { LowerBuf(s1, StrLength(s1)); }
-
-const wchar_t * __cdecl FarStrStr(const wchar_t *str1, const wchar_t *str2);
-const wchar_t * __cdecl FarStrStrI(const wchar_t *str1, const wchar_t *str2);
-const wchar_t * __cdecl RevStrStr(const wchar_t *str1, const wchar_t *str2);
-const wchar_t * __cdecl RevStrStrI(const wchar_t *str1, const wchar_t *str2);
-
-int NumStrCmp(const wchar_t *s1, size_t n1, const wchar_t *s2, size_t n2, bool IgnoreCase);
-inline int NumStrCmpN(const wchar_t *s1, int n1, const wchar_t *s2, int n2) { return NumStrCmp(s1, n1, s2, n2, false); }
-inline int NumStrCmpNI(const wchar_t *s1, int n1, const wchar_t *s2, int n2) { return NumStrCmp(s1, n1, s2, n2, true); }
-inline int NumStrCmp(const wchar_t *s1, const wchar_t *s2) { return NumStrCmp(s1, (size_t)-1, s2, (size_t)-1, false); }
-inline int NumStrCmpI(const wchar_t *s1, const wchar_t *s2) { return NumStrCmp(s1, (size_t)-1, s2, (size_t)-1, true); }
