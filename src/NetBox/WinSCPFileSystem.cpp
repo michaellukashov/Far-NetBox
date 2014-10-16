2ab30ce9112cd8eb11d4b719fde1471dfededb97
Michael Lukashov <michael.lukashov@gmail.com>
2014-10-16 23:57:00 +0400

    TWinSCPFileSystem::CreateFileList: allow duplicated filenames

--- src/NetBox/WinSCPFileSystem.cpp
+++ src/NetBox/WinSCPFileSystem.cpp
@@ -2838,6 +2838,8 @@ TStrings * TWinSCPFileSystem::CreateFileList(TObjectList * PanelItems,
   TStrings * AFileList)
 {
   std::unique_ptr<TStrings> FileList(AFileList == nullptr ? new TStringList() : AFileList);
+  FileList->SetDuplicates(dupAccept);
+
   UnicodeString FileName;
   TFarPanelItem * PanelItem;
   TObject * Data = nullptr;
