647d7a07e2559e424023b07ccecbf83941336606
Michael Lukashov <michael.lukashov@gmail.com>
2014-10-25 22:02:23 +0400

    update SessionData.cpp

--- src/core/SessionData.h
+++ src/core/SessionData.h
@@ -1008,7 +1008,6 @@ public:
 private:
   TSessionData * FDefaultSettings;
   bool FReadOnly;
-  void SetDefaultSettings(TSessionData * Value);
   void DoSave(THierarchicalStorage * Storage, bool All,
     bool RecryptPasswordOnly, TStrings * RecryptPasswordErrors);
   void DoSave(bool All, bool Explicit, bool RecryptPasswordOnly,
