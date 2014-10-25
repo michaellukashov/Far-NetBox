28a1b49d885f6f1c8957267f85fc5d7ce2aa84ae
Michael Lukashov <michael.lukashov@gmail.com>
2014-10-25 12:59:54 +0400

    update SessionInfo.cpp

--- src/core/SessionInfo.h
+++ src/core/SessionInfo.h
@@ -92,11 +92,28 @@ enum TLogLineType
   llInput,
   llStdError,
   llMessage,
-  llException
+  llException,
 };
+
 enum TLogAction
 {
-  laUpload, laDownload, laTouch, laChmod, laMkdir, laRm, laMv, laCall, laLs, laStat
+  laUpload,
+  laDownload,
+  laTouch,
+  laChmod,
+  laMkdir,
+  laRm,
+  laMv,
+  laCall,
+  laLs,
+  laStat,
+};
+
+enum TCaptureOutputType
+{
+  cotOutput,
+  cotError,
+  cotExitCode,
 };
 
 DEFINE_CALLBACK_TYPE2(TCaptureOutputEvent, void,
