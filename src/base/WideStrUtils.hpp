97b0696a3cbafb5b8eef7d40dc65e720b958bf2f
Michael Lukashov <michael.lukashov@gmail.com>
2014-10-25 22:01:19 +0400

    add WideStrUtils.cpp

--- src/base/WideStrUtils.hpp
+++ src/base/WideStrUtils.hpp
@@ -1,4 +1,13 @@
 #pragma once
 
 #include <Classes.hpp>
+#include <UnicodeString.hpp>
 
+enum TEncodeType
+{ 
+  etUSASCII,
+  etUTF8,
+  etANSI
+};
+
+extern TEncodeType DetectUTF8Encoding(const RawByteString & S);
