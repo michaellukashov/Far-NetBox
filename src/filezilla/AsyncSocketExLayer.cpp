6fd2aaadcdc6903ef1c3a18fac74531cffebc328
Michael Lukashov <michael.lukashov@gmail.com>
2015-01-18 12:57:56 +0300

    sync with WinSCP 5.6.5 (filezilla)

--- src/filezilla/AsyncSocketExLayer.cpp
+++ src/filezilla/AsyncSocketExLayer.cpp
@@ -500,7 +500,9 @@ BOOL CAsyncSocketExLayer::GetPeerName( CString& rPeerAddress, UINT& rPeerPort )
 BOOL CAsyncSocketExLayer::GetPeerNameNext( CString& rPeerAddress, UINT& rPeerPort )
 {
 	if (m_pNextLayer)
+	{
 		return m_pNextLayer->GetPeerName(rPeerAddress, rPeerPort);
+	}
 	else
 	{
 		SOCKADDR* sockAddr = NULL;
@@ -557,14 +559,20 @@ BOOL CAsyncSocketExLayer::GetPeerName( SOCKADDR* lpSockAddr, int* lpSockAddrLen 
 BOOL CAsyncSocketExLayer::GetPeerNameNext( SOCKADDR* lpSockAddr, int* lpSockAddrLen )
 {
 	if (m_pNextLayer)
+	{
 		return m_pNextLayer->GetPeerName(lpSockAddr, lpSockAddrLen);
+	}
 	else
 	{
 		ASSERT(m_pOwnerSocket);
 		if ( !getpeername(m_pOwnerSocket->GetSocketHandle(), lpSockAddr, lpSockAddrLen) )
+		{
 			return TRUE;
+		}
 		else
+		{
 			return FALSE;
+		}
 	}
 }
 
