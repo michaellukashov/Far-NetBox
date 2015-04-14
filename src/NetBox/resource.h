120f8b95be1405e908cfc24ec8e2ed7745b43fd2
Michael Lukashov <michael.lukashov@gmail.com>
2015-04-14 21:29:01 +0300

    update WinSCP version info

--- src/NetBox/resource.h
+++ src/NetBox/resource.h
@@ -13,4 +13,4 @@
 #define FILEZILLA_VERSION_WTXT      L"2.2.32"
 #define ZLIB_VERSION_WTXT           L"1.2.8"
 #define OPENSSL_VERSION_WTXT        L"1.0.1m"
-#define WINSCP_VERSION_WTXT         L"5.7.1"
+#define WINSCP_VERSION_WTXT         L"5.7.2"
