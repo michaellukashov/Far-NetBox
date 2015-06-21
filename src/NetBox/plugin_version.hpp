e73e1b3da6832037b58899852c3f961420cd1929
Michael Lukashov <michael.lukashov@gmail.com>
2015-06-21 20:49:15 +0300

    bump version to 2.1.43

--- src/NetBox/plugin_version.hpp
+++ src/NetBox/plugin_version.hpp
@@ -8,9 +8,9 @@
 
 #define NETBOX_VERSION_MAJOR         2
 #define NETBOX_VERSION_MINOR         1
-#define NETBOX_VERSION_SUBMINOR      42
-#define NETBOX_VERSION_BUILD         385
+#define NETBOX_VERSION_SUBMINOR      43
+#define NETBOX_VERSION_BUILD         386
 
-static const std::wstring NETBOX_VERSION_NUMBER(L"2.1.42");
-static const std::wstring NETBOX_COMPILATION_TIME(L"17.06.2015 14:42:46");
+static const std::wstring NETBOX_VERSION_NUMBER(L"2.1.43");
+static const std::wstring NETBOX_COMPILATION_TIME(L"21.06.2015 20:48:57");
 
