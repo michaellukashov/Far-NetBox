fc7be01502a29c9948faadc6867550d021302041
Michael Lukashov <michael.lukashov@gmail.com>
2015-08-29 15:47:22 +0300

    update DefaultKexList

--- src/core/SessionData.cpp
+++ src/core/SessionData.cpp
@@ -32,7 +32,7 @@ const wchar_t ProxyMethodList[][10] = {L"none", L"SOCKS4", L"SOCKS5", L"HTTP", L
 const TCipher DefaultCipherList[CIPHER_COUNT] =
   { cipAES, cipBlowfish, cip3DES, cipWarn, cipArcfour, cipDES };
 const TKex DefaultKexList[KEX_COUNT] =
-  { kexDHGEx, kexDHGroup14, kexDHGroup1, kexRSA, kexWarn };
+  { kexDHGEx, kexDHGroup14, kexDHGroup1, kexRSA, kexECDH, kexWarn };
 const wchar_t FSProtocolNames[FSPROTOCOL_COUNT][11] = { L"SCP", L"SFTP (SCP)", L"SFTP", L"", L"", L"FTP", L"WebDAV" };
 const intptr_t SshPortNumber = 22;
 const intptr_t FtpPortNumber = 21;
