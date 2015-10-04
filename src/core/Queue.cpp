819a0de785fbd18567492521eea88e28dd1a30cf
Michael Lukashov <michael.lukashov@gmail.com>
2015-10-04 11:32:49 +0300

    fix warning: Possible null pointer dereference

--- src/core/Queue.cpp
+++ src/core/Queue.cpp
@@ -1976,7 +1976,8 @@ UnicodeString TLocatedQueueItem::GetStartupDirectory() const
 void TLocatedQueueItem::DoExecute(TTerminal * Terminal)
 {
   assert(Terminal != nullptr);
-  Terminal->TerminalSetCurrentDirectory(FCurrentDir);
+  if (Terminal)
+    Terminal->TerminalSetCurrentDirectory(FCurrentDir);
 }
 
 // TTransferQueueItem
