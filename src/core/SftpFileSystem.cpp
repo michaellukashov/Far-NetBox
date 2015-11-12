d2e9c91b357275c4c74566dd025dc784f4488acd
Michael Lukashov <michael.lukashov@gmail.com>
2015-11-12 18:54:42 +0300

    fix warnings

--- src/core/SftpFileSystem.cpp
+++ src/core/SftpFileSystem.cpp
@@ -2536,7 +2536,7 @@ bool TSFTPFileSystem::PeekPacket()
   bool Result = FSecureShell->Peek(Buf, 4);
   if (Result)
   {
-    intptr_t Length = PacketLength(Buf, (SSH_FX_TYPES)-1);
+    intptr_t Length = PacketLength(Buf, (SSH_FXP_TYPES)-1);
     Result = FSecureShell->Peek(Buf, 4 + Length);
   }
   return Result;
