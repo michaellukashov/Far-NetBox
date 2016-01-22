a8f00826aabf4aad990470cb6af1d054194dcf16
Michael Lukashov <michael.lukashov@gmail.com>
2016-01-22 14:52:33 +0300

    UTF8String

--- src/base/UnicodeString.hpp
+++ src/base/UnicodeString.hpp
@@ -18,7 +18,7 @@ public:
   explicit UTF8String(const UnicodeString & Str);
   explicit UTF8String(const wchar_t * Str);
   explicit UTF8String(const wchar_t * Str, intptr_t Size);
-  explicit UTF8String(const char * Str, intptr_t Size);
+  explicit UTF8String(const char * Str, intptr_t Size) { Init(Str, Size); }
   explicit UTF8String(const char * Str) { Init(Str, Str ? strlen(Str) : 0); }
 
   ~UTF8String() {}
