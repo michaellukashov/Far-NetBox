761789b4254dcf265bd3d00a5da7da32b284d41b
Michael Lukashov <michael.lukashov@gmail.com>
2016-01-22 14:58:24 +0300

    Global

--- src/base/Global.h
+++ src/base/Global.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <tchar.h>
 #include <assert.h>
 #include <Classes.hpp>
 #include <Sysutils.hpp>
@@ -35,17 +36,68 @@ private:
   TCriticalSection & FCriticalSection;
 };
 
+#ifdef _DEBUG
+
+void SetTraceFile(HANDLE TraceFile);
+void CleanupTracing();
+#define TRACEENV "WINSCPTRACE"
+extern BOOL IsTracing;
+const unsigned int CallstackTlsOff = (unsigned int)-1;
+extern unsigned int CallstackTls;
+extern "C" void Trace(const wchar_t * SourceFile, const wchar_t * Func,
+  int Line, const wchar_t * Message);
+//void TraceFmt(const wchar_t * SourceFile, const wchar_t * Func,
+//  int Line, const wchar_t * Format, TVarRec * Args, const int Args_Size);
+
+#ifdef TRACE_IN_MEMORY
+
+void TraceDumpToFile();
+void TraceInMemoryCallback(const wchar_t * Msg);
+
+#endif // TRACE_IN_MEMORY
+
 #define ACCESS_VIOLATION_TEST { (*((int*)NULL)) = 0; }
-#if !defined(_DEBUG) || defined(DESIGN_ONLY)
-#define DebugAssert(p)   ((void)0)
+
+#if !defined(_DEBUG)
+
+#define DebugAssert(p) ((void)0)
 #define DebugCheck(p) (p)
 #define DebugFail()
-#else // if !defined(_DEBUG) || defined(DESIGN_ONLY)
+
+#else // if !defined(_DEBUG)
+
+#undef DebugAssert
+void DoAssert(wchar_t * Message, wchar_t * Filename, int LineNumber);
+inline bool DoAlwaysTrue(bool Value, wchar_t * Message, wchar_t * Filename, int LineNumber)
+{
+  if (!Value)
+  {
+    DoAssert(Message, Filename, LineNumber);
+  }
+  return Value;
+}
+inline bool DoAlwaysFalse(bool Value, wchar_t * Message, wchar_t * Filename, int LineNumber)
+{
+  if (Value)
+  {
+    DoAssert(Message, Filename, LineNumber);
+  }
+  return Value;
+}
+template<typename T>
+inline typename T * DoCheckNotNull(T* p, wchar_t * Message, wchar_t * Filename, int LineNumber)
+{
+  if (p == NULL)
+  {
+    DoAssert(Message, Filename, LineNumber);
+  }
+  return p;
+}
 void DoAssert(const wchar_t * Message, const wchar_t * Filename, int LineNumber);
 #define DebugAssert(p) ((p) ? (void)0 : DoAssert(TEXT(#p), TEXT(__FILE__), __LINE__))
 #define DebugCheck(p) { bool __CHECK_RESULT__ = (p); DebugAssert(__CHECK_RESULT__); }
 #define DebugFail() DebugAssert(false)
-#endif // if !defined(_DEBUG) || defined(DESIGN_ONLY)
+#endif // if !defined(_DEBUG)
 
 #define DebugAlwaysTrue(p) (p)
 #define DebugAlwaysFalse(p) (p)
@@ -69,3 +121,4 @@ typedef struct _TIME_DYNAMIC_ZONE_INFORMATION
 } DYNAMIC_TIME_ZONE_INFORMATION, *PDYNAMIC_TIME_ZONE_INFORMATION;
 #endif
 
+#endif
