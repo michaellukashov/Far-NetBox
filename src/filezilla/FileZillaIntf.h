77525396778ed276c456e0fac60fc2c6796a81b4
Michael Lukashov <michael.lukashov@gmail.com>
2016-02-06 22:26:13 +0300

    filezilla: dynamically load getaddrinfo etc

--- src/filezilla/FileZillaIntf.h
+++ src/filezilla/FileZillaIntf.h
@@ -8,6 +8,16 @@
 #include <FileZillaOpt.h>
 #include <FileZillaTools.h>
 //---------------------------------------------------------------------------
+#define DECL_WINDOWS_FUNCTION(linkage, rettype, name, params) \
+  typedef rettype (WINAPI *t_##name) params; \
+  linkage t_##name p_##name
+#define STR1(x) #x
+#define STR(x) STR1(x)
+#define GET_WINDOWS_FUNCTION_PP(module, name) \
+  (p_##name = module ? (t_##name) GetProcAddress(module, STR(name)) : NULL)
+#define GET_WINDOWS_FUNCTION(module, name) \
+  (p_##name = module ? (t_##name) GetProcAddress(module, #name) : NULL)
+//---------------------------------------------------------------------------
 class CFileZillaApi;
 class TFileZillaIntern;
 //---------------------------------------------------------------------------
