18131d9335d524bd5250cef922ab47c9226a9ce9
Michael Lukashov <michael.lukashov@gmail.com>
2016-02-14 00:14:27 +0300

    NETBOX_VERSION_SUBMINOR --> NETBOX_VERSION_PATCH

--- src/NetBox/plugin_version.hpp
+++ src/NetBox/plugin_version.hpp
@@ -8,7 +8,7 @@
 
 #define NETBOX_VERSION_MAJOR         2
 #define NETBOX_VERSION_MINOR         1
-#define NETBOX_VERSION_SUBMINOR      45
+#define NETBOX_VERSION_PATCH         45
 #define NETBOX_VERSION_BUILD         426
 
 static const std::wstring NETBOX_VERSION_NUMBER(L"2.1.45");
