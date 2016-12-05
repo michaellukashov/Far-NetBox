bf4aed53e9416356aaf3b9a9a76d541133c3cf47
Michael Lukashov <michael.lukashov@gmail.com>
2016-12-05 17:25:40 +0300

    fix Coverity warning: Explicit null dereferenced

--- src/filezilla/AsyncSocketEx.cpp
+++ src/filezilla/AsyncSocketEx.cpp
@@ -1241,9 +1241,13 @@ BOOL CAsyncSocketEx::GetPeerName( CString& rPeerAddress, UINT& rPeerPort )
     nSockAddrLen = sizeof(SOCKADDR_IN);
   }
 
-  memset(sockAddr, 0, nSockAddrLen);
+  BOOL bResult = FALSE;
+  if (sockAddr)
+  {
+    memset(sockAddr, 0, nSockAddrLen);
 
-  BOOL bResult = GetPeerName(sockAddr, &nSockAddrLen);
+    bResult = GetPeerName(sockAddr, &nSockAddrLen);
+  }
 
   if (bResult)
   {
