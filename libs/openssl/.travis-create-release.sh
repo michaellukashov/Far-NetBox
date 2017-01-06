766f620df088f105dd96047d79ad4886b4f50a5f
Michael Lukashov <michael.lukashov@gmail.com>
2017-01-06 17:36:45 +0300

    openssl-1.1.0c

--- libs/openssl/.travis-create-release.sh
+++ libs/openssl/.travis-create-release.sh
@@ -2,9 +2,10 @@
 
 # $1 is expected to be $TRAVIS_OS_NAME
 
+./Configure dist
 if [ "$1" == osx ]; then
-    make -f Makefile.org \
-	 DISTTARVARS="NAME=_srcdist TAR_COMMAND='\$\$(TAR) \$\$(TARFLAGS) -s \"|^|\$\$(NAME)/|\" -T \$\$(TARFILE).list -cvf -' TARFLAGS='-n' TARFILE=_srcdist.tar" SHELL='sh -vx' dist
+    make NAME='_srcdist' TARFILE='_srcdist.tar' \
+         TAR_COMMAND='$(TAR) $(TARFLAGS) -cvf -' tar
 else
-    make -f Makefile.org DISTTARVARS='TARFILE=_srcdist.tar NAME=_srcdist' SHELL='sh -v' dist
+    make TARFILE='_srcdist.tar' NAME='_srcdist' dist
 fi
