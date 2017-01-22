ef2f025ec7bf44cbce8feae06fc95b2f58e88ad2
Michael Lukashov <michael.lukashov@gmail.com>
2017-01-22 12:33:44 +0300

    build_netbox.cmd: add vs2015-x86 vs2015-x64

--- src/NetBox/scripts/build_netbox_release.cmd
+++ src/NetBox/scripts/build_netbox_release.cmd
@@ -9,7 +9,12 @@ set PROJECT_BUILD=Build
 
 if "%1" == "" goto x86
 if "%1" == "x64" goto x64
+if "%1" == "vs2015-x86" goto vs2015-x86
+if "%1" == "vs2015-x64" goto vs2015-x64
 goto x86
+
+:vs2015-x64
+set PROJECT_COMNTOOLS=%VS140COMNTOOLS%
 
 :x64
 set PROJECT_PLATFORM=x64
@@ -20,6 +25,9 @@ call %~dp0\build_netbox.cmd
 
 
 goto end
+
+:vs2015-x86
+set PROJECT_COMNTOOLS=%VS140COMNTOOLS%
 
 :x86
 set PROJECT_PLATFORM=x86
