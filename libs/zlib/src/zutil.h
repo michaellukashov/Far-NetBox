a0b59c8bd951d4350eff05e0d0d27b97786c6de5
Michael Lukashov <michael.lukashov@gmail.com>
2017-01-24 06:24:24 +0300

    zlib: add zmemcpy zmemcmp zmemzero

--- libs/zlib/src/zutil.h
+++ libs/zlib/src/zutil.h
@@ -108,6 +108,32 @@ extern const char * const z_errmsg[10]; /* indexed by 2-zlib_error */
 
          /* functions */
 
+#if defined(SMALL_MEDIUM) && !defined(_MSC_VER) && !defined(__SC__)
+ /* Use our own functions for small and medium model with MSC <= 5.0.
+  * You may have to use the same strategy for Borland C (untested).
+  * The __SC__ check is for Symantec.
+  */
+#  define NO_MEMCPY
+#endif
+#if defined(STDC) && !defined(HAVE_MEMCPY) && !defined(NO_MEMCPY)
+#  define HAVE_MEMCPY
+#endif
+#ifdef HAVE_MEMCPY
+#  ifdef SMALL_MEDIUM /* MSDOS small or medium model */
+#    define zmemcpy _fmemcpy
+#    define zmemcmp _fmemcmp
+#    define zmemzero(dest, len) _fmemset(dest, 0, len)
+#  else
+#    define zmemcpy memcpy
+#    define zmemcmp memcmp
+#    define zmemzero(dest, len) memset(dest, 0, len)
+#  endif
+#else
+   void ZLIB_INTERNAL zmemcpy(unsigned char *dest, const unsigned char* source, unsigned int len);
+   int ZLIB_INTERNAL zmemcmp(const unsigned char* s1, const unsigned char* s2, unsigned int len);
+   void ZLIB_INTERNAL zmemzero(unsigned char* dest, unsigned int len);
+#endif
+
 /* Diagnostic functions */
 #ifdef ZLIB_DEBUG
 #   include <stdio.h>
