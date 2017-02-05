8ab5d6eed63f518a02d36dc79a3cd044eb8224eb
Michael Lukashov <michael.lukashov@gmail.com>
2017-02-05 11:07:38 +0300

    TryStrToInt

--- src/base/Sysutils.cpp
+++ src/base/Sysutils.cpp
@@ -219,7 +219,7 @@ int64_t StrToInt64Def(const UnicodeString & Value, int64_t DefVal)
 
 bool TryStrToInt(const UnicodeString & StrValue, int64_t & Value)
 {
-  bool Result = !StrValue.IsEmpty() && (StrValue.FindFirstNotOf(L"+-0123456789") == -1);
+  bool Result = !StrValue.IsEmpty(); // && (StrValue.FindFirstNotOf(L"+-0123456789") == -1);
   if (Result)
   {
     errno = 0;
