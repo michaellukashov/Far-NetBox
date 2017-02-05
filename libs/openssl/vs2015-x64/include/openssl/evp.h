bce5ed6da70b3e08bc5ce0af7026a6711559c1f1
Michael Lukashov <michael.lukashov@gmail.com>
2017-02-05 06:46:23 +0300

    includes

--- libs/openssl/vs2015-x64/include/openssl/evp.h
+++ libs/openssl/vs2015-x64/include/openssl/evp.h
@@ -57,6 +57,8 @@
 extern "C" {
 #endif
 
+#if 0
+
 /*
  * Type needs to be a bit field Sub-type needs to be for variations on the
  * method, as in, can it do arbitrary encryption....
@@ -90,6 +92,8 @@ struct evp_pkey_st {
 # define EVP_PKEY_MO_VERIFY      0x0002
 # define EVP_PKEY_MO_ENCRYPT     0x0004
 # define EVP_PKEY_MO_DECRYPT     0x0008
+
+#endif
 
 # define EVP_PKEY_MO_SIGN        0x0001
 # define EVP_PKEY_MO_VERIFY      0x0002
