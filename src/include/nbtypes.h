bce29417d5664a379d6b172c3119d9150405db7f
Michael Lukashov <michael.lukashov@gmail.com>
2017-02-05 10:11:10 +0300

    add nbstring.h from sp

---
+++ src/include/nbtypes.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <wtypes.h>
+
+//#define FALSE 0
+//#define TRUE  1
+
+//typedef void*          HANDLE, HICON, HBITMAP;
+//typedef int            BOOL;
+//typedef unsigned int   DWORD;
+//typedef intptr_t       WPARAM, LPARAM;
+//typedef char*          LPSTR;
+//typedef const char*    LPCSTR;
+//typedef wchar_t*       LPWSTR, LPTSTR;
+//typedef const wchar_t* LPCWSTR, LPCTSTR;
+
