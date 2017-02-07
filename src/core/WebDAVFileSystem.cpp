df5f02e8f9100fa7632cdb5550c4f64758b4bf71
Michael Lukashov <michael.lukashov@gmail.com>
2017-02-07 07:40:36 +0300

    struct TReadFileData

--- src/core/WebDAVFileSystem.cpp
+++ src/core/WebDAVFileSystem.cpp
@@ -801,6 +801,7 @@ void TWebDAVFileSystem::CachedChangeDirectory(const UnicodeString & Directory)
 
 struct TReadFileData
 {
+CUSTOM_MEM_ALLOCATION_IMPL
   TWebDAVFileSystem * FileSystem;
   TRemoteFile * File;
   TRemoteFileList * FileList;
