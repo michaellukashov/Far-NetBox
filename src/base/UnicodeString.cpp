021193a8bc4ba1438d24e02eb854e3d80ecdd9ed
Michael Lukashov <michael.lukashov@gmail.com>
2017-02-08 07:21:47 +0300

    UnicodeString: cleanup

--- src/base/UnicodeString.cpp
+++ src/base/UnicodeString.cpp
@@ -383,14 +383,6 @@ void UTF8String::Init(const wchar_t * Str, intptr_t Length)
 
 void UTF8String::Init(const char * Str, intptr_t Length)
 {
-//  intptr_t Size = ::MultiByteToWideChar(CP_UTF8, 0, Str, static_cast<int>(Length > 0 ? Length : -1), nullptr, 0);
-//  Data.resize(Size + 1);
-//  if (Size > 0)
-//  {
-//    ::MultiByteToWideChar(CP_UTF8, 0, Str, -1, const_cast<wchar_t *>(Data.c_str()), static_cast<int>(Size));
-//    Data[Size] = 0;
-//  }
-//  Data = Data.c_str();
   Data.resize(Length);
   if (Length > 0)
   {
