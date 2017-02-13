e97fbe1f6bebe505f69f49951ae5cc6cf81e411e
Michael Lukashov <michael.lukashov@gmail.com>
2017-02-13 12:53:21 +0300

    include "GlobalUtils.hpp"

--- src/filezilla/FtpListResult.cpp
+++ src/filezilla/FtpListResult.cpp
@@ -2,6 +2,7 @@
 #include "stdafx.h"
 #include "FtpListResult.h"
 #include "FileZillaApi.h"
+#include <GlobalUtils.hpp>
 
 //////////////////////////////////////////////////////////////////////
 // Konstruktion/Destruktion
