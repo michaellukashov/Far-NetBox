2841e29a2e9f0232ba77491b481bf4de91e51952
Michael Lukashov <michael.lukashov@gmail.com>
2017-02-13 10:27:40 +0300

    nullptr

--- src/NetBox/FarDialog.cpp
+++ src/NetBox/FarDialog.cpp
@@ -257,16 +257,12 @@ intptr_t TFarDialog::GetItem(TFarDialogItem * Item) const
 
 TFarDialogItem * TFarDialog::GetItem(intptr_t Index) const
 {
-  TFarDialogItem * DialogItem;
+  TFarDialogItem * DialogItem = nullptr;
   if (GetItemCount())
   {
     DebugAssert(Index >= 0 && Index < FItems->GetCount());
     DialogItem = dyn_cast<TFarDialogItem>(FItems->GetObj(Index));
     DebugAssert(DialogItem);
-  }
-  else
-  {
-    DialogItem = nullptr;
   }
   return DialogItem;
 }
