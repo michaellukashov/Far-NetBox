4b3ae52fab54fe3dd0bc1d19e8cf3518e4c2b33e
Michael Lukashov <michael.lukashov@gmail.com>
2017-02-17 07:49:52 +0300

    update headers

--- libs/openssl/crypto/buildinf.h
+++ libs/openssl/crypto/buildinf.h
@@ -2,11 +2,11 @@
   /* auto-generated/updated by util/mk1mf.pl for crypto/cversion.c */
   #define CFLAGS "compiler: cl  /MD /Ox /O2 /Ob1  -DOPENSSL_USE_APPLINK -I. -DOPENSSL_NO_STATIC_ENGINE  "
   #define PLATFORM "VC-WIN32"
-  #define DATE "Sun Jan 29 01:50:30 2017"
+  #define DATE "Fri Feb 17 04:37:39 2017"
 #endif
 #ifdef MK1MF_PLATFORM_VC_WIN64A
   /* auto-generated/updated by util/mk1mf.pl for crypto/cversion.c */
   #define CFLAGS "compiler: cl  /MD /Ox  -DOPENSSL_USE_APPLINK -I. -DOPENSSL_NO_STATIC_ENGINE  "
   #define PLATFORM "VC-WIN64A"
-  #define DATE "Sun Jan 29 01:54:54 2017"
+  #define DATE "Fri Feb 17 04:41:24 2017"
 #endif
--- libs/openssl/x64/include/openssl/evp.h
+++ libs/openssl/x64/include/openssl/evp.h
@@ -57,39 +57,6 @@
 extern "C" {
 #endif
 
-#ifndef NB_COMPILE_OPENSSL
-
-/*
- * Type needs to be a bit field Sub-type needs to be for variations on the
- * method, as in, can it do arbitrary encryption....
- */
-struct evp_pkey_st {
-    int type;
-    int save_type;
-    int references;
-    const EVP_PKEY_ASN1_METHOD *ameth;
-    ENGINE *engine;
-    union {
-        char *ptr;
-# ifndef OPENSSL_NO_RSA
-        struct rsa_st *rsa;     /* RSA */
-# endif
-# ifndef OPENSSL_NO_DSA
-        struct dsa_st *dsa;     /* DSA */
-# endif
-# ifndef OPENSSL_NO_DH
-        struct dh_st *dh;       /* DH */
-# endif
-# ifndef OPENSSL_NO_EC
-        struct ec_key_st *ec;   /* ECC */
-# endif
-    } pkey;
-    int save_parameters;
-    STACK_OF(X509_ATTRIBUTE) *attributes; /* [ 0 ] */
-} /* EVP_PKEY */ ;
-
-#endif // #ifndef NB_COMPILE_OPENSSL
-
 # define EVP_PKEY_MO_SIGN        0x0001
 # define EVP_PKEY_MO_VERIFY      0x0002
 # define EVP_PKEY_MO_ENCRYPT     0x0004
