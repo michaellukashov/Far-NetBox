8bc74b083a5d6acd049cb63c94523da24812f9f4
Michael Lukashov <michael.lukashov@gmail.com>
2017-02-19 20:17:21 +0300

    cast_or_null

--- src/include/rtti.hpp
+++ src/include/rtti.hpp
@@ -343,3 +343,9 @@ inline bool isa(const Y * Val)
 {
   return Val && rtti::isa<X>(Val);
 }
+
+template <class X, class Y>
+inline X * cast_or_null(void * Val)
+{
+  return rtti::cast_or_null<X>(static_cast<Y *>(Val));
+}
