7c04879b83d4c8a8d0656adaba8bb301e8ea3e9e
Michael Lukashov <michael.lukashov@gmail.com>
2017-03-05 09:43:01 +0300

    Building VS2015-x86

--- appveyor.bat
+++ appveyor.bat
@@ -7,7 +7,9 @@ goto :error
 
 :build_cpp
 echo Building VS2015-x64
-call src\NetBox\scripts\build_netbox_release.cmd vs2015-x64 || goto error
+call src/NetBox/scripts/build_netbox_release.cmd vs2015-x64 || goto error
+echo Building VS2015-x86
+call src/NetBox/scripts/build_netbox_release.cmd vs2015-x86 || goto error
 goto :EOF
 
 goto :EOF
