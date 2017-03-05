2e072de49807723cb0882df5e1ba1fd80f9139a5
Michael Lukashov <michael.lukashov@gmail.com>
2017-03-05 09:29:39 +0300

    add appveyor.yml appveyor.bat

---
+++ appveyor.bat
@@ -0,0 +1,17 @@
+setlocal
+
+IF %language%==cpp GOTO build_cpp
+
+echo Unsupported language %language%. Exiting.
+goto :error
+
+:build_cpp
+echo Building VS2015-x64
+call src\NetBox\scripts\build_netbox_release.cmd vs2015-x64 || goto error
+goto :EOF
+
+goto :EOF
+
+:error
+echo Failed!
+EXIT /b %ERRORLEVEL%
