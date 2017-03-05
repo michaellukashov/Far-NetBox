43c83b0ab7ff2be753467779f371e34a9fd9b12d
Michael Lukashov <michael.lukashov@gmail.com>
2017-03-05 09:51:45 +0300

    platforms: Win64 Win32

--- appveyor.bat
+++ appveyor.bat
@@ -6,8 +6,16 @@ echo Unsupported language %language%. Exiting.
 goto :error
 
 :build_cpp
+if %platform%==Win64 GOTO :build_vs2015_x64
+if %platform%==Win32 GOTO :build_vs2015_x86
+GOTO :build_vs2015_x64
+
+:build_vs2015_x64
 echo Building VS2015-x64
 call src/NetBox/scripts/build_netbox_release.cmd vs2015-x64 || goto error
+goto :EOF
+
+:build_vs2015_x86
 echo Building VS2015-x86
 call src/NetBox/scripts/build_netbox_release.cmd vs2015-x86 || goto error
 goto :EOF
