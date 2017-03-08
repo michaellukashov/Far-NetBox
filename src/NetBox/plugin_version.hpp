c41d700711d587a84944058e7f82c356ba1611d7
Michael Lukashov <michael.lukashov@gmail.com>
2017-03-08 11:49:33 +0300

    update version info

--- src/NetBox/plugin_version.hpp
+++ src/NetBox/plugin_version.hpp
@@ -9,7 +9,7 @@
 #define NETBOX_VERSION_MAJOR         2
 #define NETBOX_VERSION_MINOR         4
 #define NETBOX_VERSION_PATCH         2
-#define NETBOX_VERSION_BUILD         464
+#define NETBOX_VERSION_BUILD         465
 
 static const std::wstring NETBOX_VERSION_NUMBER(L"2.4.2");
 
