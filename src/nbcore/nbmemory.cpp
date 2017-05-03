593323bd7b38a5367ef9f47453b95c3c11f743c2
Michael Lukashov <michael.lukashov@gmail.com>
2017-05-03 13:14:19 +0300

    nbcore: code reformat

--- src/nbcore/nbmemory.cpp
+++ src/nbcore/nbmemory.cpp
@@ -6,14 +6,14 @@
 static int CheckBlock(void* blk)
 {
   int result = FALSE;
-  char* p = (char*)blk - sizeof(uint32_t)*2;
+  char* p = (char*)blk - sizeof(uint32_t) * 2;
   uint32_t size, *b, *e;
 
   __try
   {
     size = *(uint32_t*)p;
-    b = (uint32_t*)&p[ sizeof(uint32_t) ];
-    e = (uint32_t*)&p[ sizeof(uint32_t)*2 + size ];
+    b = (uint32_t*)&p[sizeof(uint32_t)];
+    e = (uint32_t*)&p[sizeof(uint32_t) * 2 + size];
 
     if (*b != BLOCK_ALLOCATED || *e != BLOCK_ALLOCATED)
     {
@@ -21,18 +21,18 @@ static int CheckBlock(void* blk)
         OutputDebugStringA("memory block is already deleted\n");
       else
         OutputDebugStringA("memory block is corrupted\n");
-      #if defined(_DEBUG)
+#if defined(_DEBUG)
         DebugBreak();
-      #endif
+#endif
     }
-     else result = TRUE;
+    else result = TRUE;
   }
-  __except(EXCEPTION_EXECUTE_HANDLER)
+  __except (EXCEPTION_EXECUTE_HANDLER)
   {
     OutputDebugStringA("access violation during checking memory block\n");
-    #if defined(_DEBUG)
+#if defined(_DEBUG)
       DebugBreak();
-    #endif
+#endif
   }
 
   return result;
@@ -45,19 +45,20 @@ NB_C_CORE_DLL(void*) nbcore_alloc(size_t size)
   if (size == 0)
     return nullptr;
 
-  char *p = (char*)nb_malloc(size + sizeof(uint32_t)* 3);
-  if (p == nullptr) {
+  char* p = (char*)nb_malloc(size + sizeof(uint32_t)* 3);
+  if (p == nullptr)
+  {
     OutputDebugStringA("memory overflow\n");
-    #if defined(_DEBUG)
+#if defined(_DEBUG)
       DebugBreak();
-    #endif
+#endif
     return nullptr;
   }
 
   *(uint32_t*)p = (uint32_t)size;
   *(uint32_t*)&p[sizeof(uint32_t)] = BLOCK_ALLOCATED;
-  *(uint32_t*)&p[size + sizeof(uint32_t)*2] = BLOCK_ALLOCATED;
-  return p + sizeof(uint32_t)* 2;
+  *(uint32_t*)&p[size + sizeof(uint32_t) * 2] = BLOCK_ALLOCATED;
+  return p + sizeof(uint32_t) * 2;
 }
 
 /******************************************************************************/
@@ -74,28 +75,30 @@ NB_C_CORE_DLL(void*) nbcore_calloc(size_t size)
 
 NB_C_CORE_DLL(void*) nbcore_realloc(void* ptr, size_t size)
 {
-  char *p;
+  char* p;
 
-  if (ptr != nullptr) {
+  if (ptr != nullptr)
+  {
     if (!CheckBlock(ptr))
       return nullptr;
-    p = (char*)ptr - sizeof(uint32_t)*2;
+    p = (char*)ptr - sizeof(uint32_t) * 2;
   }
   else p = nullptr;
 
   p = (char*)nb_realloc(p, size + sizeof(uint32_t)*3);
-  if (p == nullptr) {
+  if (p == nullptr)
+  {
     OutputDebugStringA("memory overflow\n");
-    #if defined(_DEBUG)
+#if defined(_DEBUG)
       DebugBreak();
-    #endif
+#endif
     return nullptr;
   }
 
   *(uint32_t*)p = (uint32_t)size;
   *(uint32_t*)&p[sizeof(uint32_t)] = BLOCK_ALLOCATED;
-  *(uint32_t*)&p[size + sizeof(uint32_t)*2] = BLOCK_ALLOCATED;
-  return p + sizeof(uint32_t)*2;
+  *(uint32_t*)&p[size + sizeof(uint32_t) * 2] = BLOCK_ALLOCATED;
+  return p + sizeof(uint32_t) * 2;
 }
 
 /******************************************************************************/
@@ -110,33 +113,33 @@ NB_C_CORE_DLL(void) nbcore_free(void* ptr)
   if (!CheckBlock(ptr))
     return;
 
-  p = (char*)ptr - sizeof(uint32_t)*2;
+  p = (char*)ptr - sizeof(uint32_t) * 2;
   size = *(uint32_t*)p;
 
   *(uint32_t*)&p[sizeof(uint32_t)] = BLOCK_FREED;
-  *(uint32_t*)&p[size + sizeof(uint32_t)*2] = BLOCK_FREED;
+  *(uint32_t*)&p[size + sizeof(uint32_t) * 2] = BLOCK_FREED;
   nb_free(p);
 }
 
 /******************************************************************************/
 
-NB_CORE_DLL(char*) nbcore_strdup(const char *str)
+NB_CORE_DLL(char*) nbcore_strdup(const char* str)
 {
   if (str == nullptr)
     return nullptr;
 
-  char *p = (char*)nbcore_alloc(strlen(str)+1);
+  char* p = (char*)nbcore_alloc(strlen(str) + 1);
   if (p)
     strcpy(p, str);
   return p;
 }
 
-NB_CORE_DLL(wchar_t*) nbcore_wstrdup(const wchar_t *str)
+NB_CORE_DLL(wchar_t*) nbcore_wstrdup(const wchar_t* str)
 {
   if (str == nullptr)
     return nullptr;
 
-  wchar_t *p = (wchar_t*)nbcore_alloc(sizeof(wchar_t)*(wcslen(str)+1));
+  wchar_t* p = (wchar_t*)nbcore_alloc(sizeof(wchar_t) * (wcslen(str) + 1));
   if (p)
     wcscpy(p, str);
   return p;
@@ -144,27 +147,29 @@ NB_CORE_DLL(wchar_t*) nbcore_wstrdup(const wchar_t *str)
 
 /******************************************************************************/
 
-NB_CORE_DLL(char*) nbcore_strndup(const char *str, size_t len)
+NB_CORE_DLL(char*) nbcore_strndup(const char* str, size_t len)
 {
   if (str == nullptr || len == 0)
     return nullptr;
 
-  char *p = (char*)nbcore_alloc(len+1);
-  if (p) {
+  char* p = (char*)nbcore_alloc(len + 1);
+  if (p)
+  {
     memcpy(p, str, len);
     p[len] = 0;
   }
   return p;
 }
 
-NB_CORE_DLL(wchar_t*) nbcore_wstrndup(const wchar_t *str, size_t len)
+NB_CORE_DLL(wchar_t*) nbcore_wstrndup(const wchar_t* str, size_t len)
 {
   if (str == nullptr || len == 0)
     return nullptr;
 
-  wchar_t *p = (wchar_t*)nbcore_alloc(sizeof(wchar_t)*(len+1));
-  if (p) {
-    memcpy(p, str, sizeof(wchar_t)*len);
+  wchar_t* p = (wchar_t*)nbcore_alloc(sizeof(wchar_t) * (len + 1));
+  if (p)
+  {
+    memcpy(p, str, sizeof(wchar_t) * len);
     p[len] = 0;
   }
   return p;
@@ -172,43 +177,43 @@ NB_CORE_DLL(wchar_t*) nbcore_wstrndup(const wchar_t *str, size_t len)
 
 /******************************************************************************/
 
-NB_CORE_DLL(int) nbcore_snprintf(char *buffer, size_t count, const char* fmt, ...)
+NB_CORE_DLL(int) nbcore_snprintf(char* buffer, size_t count, const char* fmt, ...)
 {
   va_list va;
   va_start(va, fmt);
-  int len = _vsnprintf(buffer, count-1, fmt, va);
+  int len = _vsnprintf(buffer, count - 1, fmt, va);
   va_end(va);
-  buffer[count-1] = 0;
+  buffer[count - 1] = 0;
   return len;
 }
 
 /******************************************************************************/
 
-NB_CORE_DLL(int) nbcore_snwprintf(wchar_t *buffer, size_t count, const wchar_t* fmt, ...)
+NB_CORE_DLL(int) nbcore_snwprintf(wchar_t* buffer, size_t count, const wchar_t* fmt, ...)
 {
   va_list va;
   va_start(va, fmt);
-  int len = _vsntprintf(buffer, count-1, fmt, va);
+  int len = _vsntprintf(buffer, count - 1, fmt, va);
   va_end(va);
-  buffer[count-1] = 0;
+  buffer[count - 1] = 0;
   return len;
 }
 
 /******************************************************************************/
 
-NB_CORE_DLL(int) nbcore_vsnprintf(char *buffer, size_t count, const char* fmt, va_list va)
+NB_CORE_DLL(int) nbcore_vsnprintf(char* buffer, size_t count, const char* fmt, va_list va)
 {
-  int len = _vsnprintf(buffer, count-1, fmt, va);
-  buffer[count-1] = 0;
+  int len = _vsnprintf(buffer, count - 1, fmt, va);
+  buffer[count - 1] = 0;
   return len;
 }
 
 /******************************************************************************/
 
-NB_CORE_DLL(int) nbcore_vsnwprintf(wchar_t *buffer, size_t count, const wchar_t* fmt, va_list va)
+NB_CORE_DLL(int) nbcore_vsnwprintf(wchar_t* buffer, size_t count, const wchar_t* fmt, va_list va)
 {
-  int len = _vsntprintf(buffer, count-1, fmt, va);
-  buffer[count-1] = 0;
+  int len = _vsntprintf(buffer, count - 1, fmt, va);
+  buffer[count - 1] = 0;
   return len;
 }
 
@@ -220,7 +225,7 @@ NB_CORE_DLL(wchar_t*) nbcore_a2u_cp(const char* src, int codepage)
     return nullptr;
 
   int cbLen = ::MultiByteToWideChar(codepage, 0, src, -1, nullptr, 0);
-  wchar_t* result = (wchar_t*)nbcore_alloc(sizeof(wchar_t)*(cbLen+1));
+  wchar_t* result = (wchar_t*)nbcore_alloc(sizeof(wchar_t) * (cbLen + 1));
   if (result == nullptr)
     return nullptr;
 
@@ -244,7 +249,7 @@ NB_CORE_DLL(char*) nbcore_u2a_cp(const wchar_t* src, int codepage)
     return nullptr;
 
   int cbLen = WideCharToMultiByte(codepage, 0, src, -1, nullptr, 0, nullptr, nullptr);
-  char* result = (char*)nbcore_alloc(cbLen+1);
+  char* result = (char*)nbcore_alloc(cbLen + 1);
   if (result == nullptr)
     return nullptr;
 
