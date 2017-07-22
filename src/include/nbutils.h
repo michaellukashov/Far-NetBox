8efa245627cff9b1f948e66a660f04bca3f79f60
Michael Lukashov <michael.lukashov@gmail.com>
2017-07-22 18:29:51 +0300

    nbutils.h

--- src/include/nbutils.h
+++ src/include/nbutils.h
@@ -2,13 +2,13 @@
 
 namespace nb {
 
-intptr_t __cdecl StrLength(const wchar_t * str);
-wchar_t __cdecl Upper(wchar_t Ch);
-wchar_t __cdecl Lower(wchar_t Ch);
-int __cdecl StrCmpNNI(const wchar_t * s1, int n1, const wchar_t * s2, int n2);
-int __cdecl StrLIComp(const wchar_t * s1, const wchar_t * s2, int n);
-int __cdecl FarStrCmpI(const wchar_t * s1, const wchar_t * s2);
-int __cdecl StrCmpNN(const wchar_t * s1, int n1, const wchar_t * s2, int n2);
+NB_CORE_EXPORT intptr_t __cdecl StrLength(const wchar_t * str);
+NB_CORE_EXPORT wchar_t __cdecl Upper(wchar_t Ch);
+NB_CORE_EXPORT wchar_t __cdecl Lower(wchar_t Ch);
+NB_CORE_EXPORT int __cdecl StrCmpNNI(const wchar_t * s1, int n1, const wchar_t * s2, int n2);
+NB_CORE_EXPORT int __cdecl StrLIComp(const wchar_t * s1, const wchar_t * s2, int n);
+NB_CORE_EXPORT int __cdecl FarStrCmpI(const wchar_t * s1, const wchar_t * s2);
+NB_CORE_EXPORT int __cdecl StrCmpNN(const wchar_t * s1, int n1, const wchar_t * s2, int n2);
 
 enum TEncodeType
 {
