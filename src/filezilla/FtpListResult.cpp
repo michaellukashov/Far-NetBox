badc3290304e9404fe9b0b3f850b00a142edea1b
Michael Lukashov <michael.lukashov@gmail.com>
2017-07-22 13:40:38 +0300

    fix warning: filezilla: '&&' within '||' [-Wlogical-op-parentheses]

--- src/filezilla/FtpListResult.cpp
+++ src/filezilla/FtpListResult.cpp
@@ -2392,7 +2392,7 @@ BOOL CFtpListResult::parseAsOther(const char *line, const int linelen, t_directo
         {
           direntry.dir = TRUE;
           tokenlen -= 5;
-          while (tokenlen && str[tokenlen - 1] == ' ' || str[tokenlen - 1] == '\t')
+          while (tokenlen && (str[tokenlen - 1] == ' ' || str[tokenlen - 1] == '\t'))
             tokenlen--;
         }
         if (!tokenlen)
