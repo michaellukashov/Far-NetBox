9a0f90f26ec2b32f8c3f34680566ab2dc4aa8207
Michael Lukashov <michael.lukashov@gmail.com>
2017-07-23 21:57:15 +0300

    FormatUtils.cpp

--- src/base/FormatUtils.cpp
+++ src/base/FormatUtils.cpp
@@ -4,22 +4,46 @@
 
 #include "FormatUtils.h"
 
-UnicodeString nb_format(fmt::WCStringRef format_str, fmt::ArgList args)
+namespace nb {
+
+UnicodeString fmtformat(fmt::WCStringRef format_str, fmt::ArgList args)
 {
   fmt::WMemoryWriter w;
   w.write(format_str, args);
   return UnicodeString(w.c_str(), w.size());
 }
 
-UnicodeString nb_sprintf(fmt::WCStringRef format, fmt::ArgList args)
+//UnicodeString fmtsprintf(fmt::WCStringRef format, fmt::ArgList args)
+//{
+//  fmt::WMemoryWriter w;
+//  fmt::printf(w, format, args);
+//  return UnicodeString(w.c_str(), w.size());
+//}
+
+UnicodeString fmtsprintf(UnicodeString format, fmt::ArgList args)
 {
   fmt::WMemoryWriter w;
-  fmt::printf(w, format, args);
+  fmt::printf(w, format.c_str(), args);
   return UnicodeString(w.c_str(), w.size());
 }
+
+UnicodeString fmtloadstr(intptr_t Id, fmt::ArgList args)
+{
+  UnicodeString Fmt = GetGlobals()->GetMsg(Id);
+  if (!Fmt.IsEmpty())
+  {
+    UnicodeString Result = fmtsprintf(Fmt, args);
+    return Result;
+  }
+  DEBUG_PRINTF("Unknown resource string id: %d\n", Id);
+  return UnicodeString();
+}
+
+} // namespace nb
 
 std::basic_ostream<wchar_t> &operator<<(std::basic_ostream<wchar_t> &os, const UnicodeString &value)
 {
   os << value.c_str();
   return os;
 }
+
--- src/base/FormatUtils.h
+++ src/base/FormatUtils.h
@@ -13,11 +13,19 @@
 
 #include "UnicodeString.hpp"
 
+namespace nb {
 
-NB_CORE_EXPORT UnicodeString nb_format(fmt::WCStringRef format_str, fmt::ArgList args);
-FMT_VARIADIC_W(UnicodeString, nb_format, fmt::WCStringRef)
+NB_CORE_EXPORT UnicodeString fmtformat(fmt::WCStringRef format_str, fmt::ArgList args);
+FMT_VARIADIC_W(UnicodeString, fmtformat, fmt::WCStringRef)
 
-NB_CORE_EXPORT UnicodeString nb_sprintf(fmt::WCStringRef format, fmt::ArgList args);
-FMT_VARIADIC_W(UnicodeString, nb_sprintf, fmt::WCStringRef)
+//NB_CORE_EXPORT UnicodeString fmtsprintf(fmt::WCStringRef format, fmt::ArgList args);
+//FMT_VARIADIC_W(UnicodeString, fmtsprintf, fmt::WCStringRef)
+NB_CORE_EXPORT UnicodeString fmtsprintf(UnicodeString format, fmt::ArgList args);
+FMT_VARIADIC_W(UnicodeString, fmtsprintf, UnicodeString)
+
+NB_CORE_EXPORT UnicodeString fmtloadstr(intptr_t Id, fmt::ArgList args);
+FMT_VARIADIC_W(UnicodeString, fmtloadstr, intptr_t)
+
+} // namespace nb
 
 NB_CORE_EXPORT std::basic_ostream<wchar_t>& operator<<(std::basic_ostream<wchar_t>& os, const UnicodeString& value);
