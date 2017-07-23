548b5e8311e10a830bde372a151c8882cf398537
Michael Lukashov <michael.lukashov@gmail.com>
2017-07-23 17:30:06 +0300

    UnicodeString.hpp: add operator <<

--- src/base/UnicodeString.hpp
+++ src/base/UnicodeString.hpp
@@ -377,3 +377,11 @@ inline bool operator>(const S & lhs, const S & rhs)
 
 }  // namespace rde
 
+template <class T>
+inline UnicodeString ToUnicodeString(const T &a) { return UnicodeString(a.c_str(), (intptr_t)a.size()); }
+
+inline std::basic_ostream<wchar_t>& operator << (std::basic_ostream<wchar_t>& os, const UnicodeString& value)
+{
+  os << std::wstring(value.c_str());
+  return os;
+}
