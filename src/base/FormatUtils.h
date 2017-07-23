36d557230191a68f67d77d0fe520c35d61c94acc
Michael Lukashov <michael.lukashov@gmail.com>
2017-07-23 18:54:07 +0300

    add FormatUtils.h

--- src/base/FormatUtils.h
+++ src/base/FormatUtils.h
@@ -8,16 +8,37 @@
 #undef min
 #endif
 
-//#include "fmt/format.h"
+#include "fmt/format.h"
 #include "fmt/printf.h"
 
 #include "UnicodeString.hpp"
 
-template <class T>
-inline UnicodeString ToUnicodeString(const T &a) { return UnicodeString(a.c_str(), (intptr_t)a.size()); }
+
+//namespace nb {
+
+inline UnicodeString nbformat(fmt::WCStringRef format_str, fmt::ArgList args)
+{
+  fmt::WMemoryWriter w;
+  w.write(format_str, args);
+  return UnicodeString(w.c_str(), w.size());
+}
+FMT_VARIADIC_W(UnicodeString, nbformat, fmt::WCStringRef)
+
+inline UnicodeString nbsprintf(fmt::WCStringRef format, fmt::ArgList args)
+{
+  fmt::WMemoryWriter w;
+  fmt::printf(w, format, args);
+  return UnicodeString(w.c_str(), w.size());
+}
+FMT_VARIADIC_W(UnicodeString, nbsprintf, fmt::WCStringRef)
+
+//} // namespace nb
+
+//template <class T>
+//inline UnicodeString ToUnicodeString(const T &a) { return UnicodeString(a.c_str(), (intptr_t)a.size()); }
 
 inline std::basic_ostream<wchar_t>& operator << (std::basic_ostream<wchar_t>& os, const UnicodeString& value)
 {
-  os << std::wstring(value.c_str());
+  os << value.c_str();
   return os;
 }
