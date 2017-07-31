eb727f0caed355e125f742918a5915c96f20dfc1
Michael Lukashov <michael.lukashov@gmail.com>
2017-07-31 21:12:11 +0300

    LocalOpenLogFile: use _wfsopen

--- src/core/SessionInfo.cpp
+++ src/core/SessionInfo.cpp
@@ -629,8 +629,8 @@ TFileSystemInfo::TFileSystemInfo()
 static FILE * LocalOpenLogFile(UnicodeString LogFileName, TDateTime Started, TSessionData * SessionData, bool Append, UnicodeString & ANewFileName)
 {
   UnicodeString NewFileName = StripPathQuotes(GetExpandedLogFileName(LogFileName, Started, SessionData));
-  FILE * Result = _fsopen(::W2MB(ApiPath(NewFileName).c_str()).c_str(),
-    Append ? "ab" : "wb", SH_DENYWR);
+  FILE * Result = _wfsopen(ApiPath(NewFileName).c_str(),
+    Append ? L"ab" : L"wb", SH_DENYWR);
   if (Result != nullptr)
   {
     setvbuf(Result, nullptr, _IONBF, BUFSIZ);
