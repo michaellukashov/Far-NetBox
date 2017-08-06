503cf29325905424c53234ff5f9cae6818fa156e
Michael Lukashov <michael.lukashov@gmail.com>
2017-08-06 23:46:10 +0300

    Revert "TAnonFunction"
    This reverts commit 0d68f54ad6c5f9ab8ed234a0d892c57f357bb3cc.

--- src/core/Terminal.h
+++ src/core/Terminal.h
@@ -807,8 +807,6 @@ private:
   void InternalTryOpen();
   void InternalDoTryOpen();
   void InitFileSystem();
-
-  void AnonFunction() {}
 };
 
 class NB_CORE_EXPORT TSecondaryTerminal : public TTerminal
