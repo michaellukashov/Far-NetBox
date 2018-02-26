23b936ce33ea3fedc9cfd75d52c06ff85e7e5c3b
Michael Lukashov <michael.lukashov@gmail.com>
2018-02-26 17:37:31 +0300

    sync

--- src/base/Common.cpp
+++ src/base/Common.cpp
@@ -1428,6 +1428,7 @@ void ProcessLocalDirectory(const UnicodeString ADirName,
 //---------------------------------------------------------------------------
 DWORD FileGetAttrFix(const UnicodeString AFileName)
 {
+  UnicodeString FileName = ApiPath(AFileName);
   // The default for FileGetAttr is to follow links
   bool FollowLink = true;
   // But the FileGetAttr whe called for link with FollowLink set will always fail
@@ -1436,7 +1437,7 @@ DWORD FileGetAttrFix(const UnicodeString AFileName)
   {
     FollowLink = false;
   }
-  return ::SysUtulsFileGetAttr(AFileName, FollowLink);
+  return ::SysUtulsFileGetAttr(FileName, FollowLink);
 }
 //---------------------------------------------------------------------------
 TDateTime EncodeDateVerbose(Word Year, Word Month, Word Day)
