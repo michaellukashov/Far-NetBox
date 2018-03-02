fbd4d52fa5d95955bbc57714a746954721a23765
Michael Lukashov <michael.lukashov@gmail.com>
2018-03-02 15:55:35 +0300

    add nb::counter_id()

--- src/include/rtti.hpp
+++ src/include/rtti.hpp
@@ -3,7 +3,7 @@
 #include <string>
 #include "type_traits.h"
 
-//===-- from llvm/Support/Casting.h - Allow flexible, checked, casts
+//===-- from llvm/Support/Casting.h - Allow flexible, checked casts
 //
 // This file defines the isa<X>(), cast<X>(), dyn_cast<X>(), cast_or_null<X>(),
 // and dyn_cast_or_null<X>() templates.
@@ -378,3 +378,65 @@ inline bool isa(const Y *Val)
 {
   return Val && rtti::isa<X>(Val);
 }
+
+namespace nb {
+// generate unique IDs at compile-time
+
+// Number of Bits our counter is using. Lower number faster compile time,
+// but less distinct values. With 16 we have 2^16 distinct values.
+static const int MAX_DEPTH = 16;
+
+// Used for counting.
+template<int N>
+struct flag {
+  friend constexpr int adl_flag(flag<N>);
+};
+
+// Used for noting how far down in the binary tree we are.
+// depth<0> equales leaf nodes. depth<MAX_DEPTH> equals root node.
+template<int N> struct depth {};
+
+// Creating an instance of this struct marks the flag<N> as used.
+template<int N>
+struct mark {
+  friend constexpr int adl_flag (flag<N>) {
+    return N;
+  }
+
+  static constexpr int value = N;
+};
+
+// Heart of the expression. The first two functions are for inner nodes and
+// the next two for termination at leaf nodes.
+
+// char[noexcept( adl_flag(flag<N>()) ) ? +1 : -1] is valid if flag<N> exists.
+template <int D, int N, class = char[noexcept( adl_flag(flag<N>()) ) ? +1 : -1]>
+int constexpr binary_search_flag(int,  depth<D>, flag<N>,
+                                 int next_flag = binary_search_flag(0, depth<D-1>(), flag<N + (1 << (D - 1))>())) {
+  return next_flag;
+}
+
+template <int D, int N>
+int constexpr binary_search_flag(float, depth<D>, flag<N>,
+                                 int next_flag = binary_search_flag(0, depth<D-1>(), flag<N - (1 << (D - 1))>())) {
+  return next_flag;
+}
+
+template <int N, class = char[noexcept( adl_flag(flag<N>()) ) ? +1 : -1]>
+int constexpr binary_search_flag(int,   depth<0>, flag<N>) {
+  return N + 1;
+}
+
+template <int N>
+int constexpr binary_search_flag(float, depth<0>, flag<N>) {
+  return N;
+}
+
+// The actual expression to call for increasing the count.
+template<int next_flag = binary_search_flag(0, depth<MAX_DEPTH-1>(),
+         flag<(1 << (MAX_DEPTH-1))>())>
+int constexpr counter_id(int value = mark<next_flag>::value) {
+  return value;
+}
+
+} // namespace nb
