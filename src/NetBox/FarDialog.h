71271a2ae5d02c2eb11560bc93e13096e0da9ca8
Michael Lukashov <michael.lukashov@gmail.com>
2018-03-02 14:46:20 +0300

    override

--- src/NetBox/FarDialog.h
+++ src/NetBox/FarDialog.h
@@ -459,9 +459,9 @@ public:
 
 protected:
   TFarAllowChangeEvent FOnAllowChange;
-  virtual LONG_PTR ItemProc(int Msg, LONG_PTR Param);
-  virtual bool GetIsEmpty() const;
-  virtual void SetData(const UnicodeString Value);
+  virtual LONG_PTR ItemProc(int Msg, LONG_PTR Param) override;
+  virtual bool GetIsEmpty() const override;
+  virtual void SetData(const UnicodeString Value) override;
 };
 
 class TFarEdit : public TFarDialogItem
@@ -664,8 +664,8 @@ public:
   void SetItemIndex(intptr_t Index) { FList->SetSelected(Index); }
 
 protected:
-  virtual LONG_PTR ItemProc(int Msg, LONG_PTR Param);
-  virtual void Init();
+  virtual LONG_PTR ItemProc(int Msg, LONG_PTR Param) override;
+  virtual void Init() override;
 
 private:
   TFarList *FList;
@@ -685,8 +685,8 @@ public:
   bool GetScrollBar() const;
 
 protected:
-  virtual LONG_PTR ItemProc(int Msg, LONG_PTR Param);
-  virtual void DoFocus();
+  virtual LONG_PTR ItemProc(int Msg, LONG_PTR Param) override;
+  virtual void DoFocus() override;
 
 private:
   void ItemsChange(TObject *Sender);
