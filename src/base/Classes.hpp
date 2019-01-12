9342bb88bc33902b275f74f55954861b558f0ade
Michael Lukashov <michael.lukashov@gmail.com>
2019-01-12 12:09:22 +0300

    base

--- src/base/Classes.hpp
+++ src/base/Classes.hpp
@@ -740,7 +740,7 @@ public:
     const UnicodeString APrompt, UnicodeString &Value, const UnicodeString HelpKeyword,
     TStrings *History, bool PathInput,
     TInputDialogInitializeEvent OnInitialize, bool Echo) = 0;
-  virtual uintptr_t MoreMessageDialog(const UnicodeString AMessage,
+  virtual uint32_t MoreMessageDialog(const UnicodeString AMessage,
     TStrings *MoreMessages, TQueryType Type, uintptr_t Answers,
     const TMessageParams *Params) = 0;
 };
