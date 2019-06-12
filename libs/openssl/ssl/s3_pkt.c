df89779b7a800b555c0a8d0fed2ec16a7a05f7a6
Michael Lukashov <michael.lukashov@gmail.com>
2019-06-12 15:56:17 +0300

    openssl-1.0.2s
    # Conflicts:
    #	libs/openssl/apps/CA.pl
    #	libs/openssl/doc/apps/ca.pod
    #	libs/openssl/doc/crypto/X509_NAME_ENTRY_get_object.pod

--- libs/openssl/ssl/s3_pkt.c
+++ libs/openssl/ssl/s3_pkt.c
@@ -56,7 +56,7 @@
  * [including the GNU Public Licence.]
  */
 /* ====================================================================
- * Copyright (c) 1998-2018 The OpenSSL Project.  All rights reserved.
+ * Copyright (c) 1998-2019 The OpenSSL Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -973,7 +973,7 @@ static int do_ssl3_write(SSL *s, int type, const unsigned char *buf,
 
     *(p++) = (s->version >> 8);
     /*
-     * Some servers hang if initial client hello is larger than 256 bytes
+     * Some servers hang if iniatial client hello is larger than 256 bytes
      * and record version number > TLS 1.0
      */
     if (s->state == SSL3_ST_CW_CLNT_HELLO_B
@@ -1500,6 +1500,7 @@ int ssl3_read_bytes(SSL *s, int type, unsigned char *buf, int len, int peek)
             ERR_add_error_data(2, "SSL alert number ", tmp);
             s->shutdown |= SSL_RECEIVED_SHUTDOWN;
             SSL_CTX_remove_session(s->session_ctx, s->session);
+            s->state = SSL_ST_ERR;
             return (0);
         } else {
             al = SSL_AD_ILLEGAL_PARAMETER;
@@ -1719,9 +1720,12 @@ int ssl3_send_alert(SSL *s, int level, int desc)
                                           * protocol_version alerts */
     if (desc < 0)
         return -1;
-    /* If a fatal one, remove from cache */
-    if ((level == 2) && (s->session != NULL))
-        SSL_CTX_remove_session(s->session_ctx, s->session);
+    /* If a fatal one, remove from cache and go into the error state */
+    if (level == SSL3_AL_FATAL) {
+        if (s->session != NULL)
+            SSL_CTX_remove_session(s->session_ctx, s->session);
+        s->state = SSL_ST_ERR;
+    }
 
     s->s3->alert_dispatch = 1;
     s->s3->send_alert[0] = level;
