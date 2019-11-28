bc06485b44f2c44349341147f46785f97620711e
Michael Lukashov <michael.lukashov@gmail.com>
2019-11-28 14:38:33 +0300

    apply custom patches

--- libs/openssl/ms/do_win64a.bat
+++ libs/openssl/ms/do_win64a.bat
@@ -12,7 +12,7 @@ perl ms\uplink-x86_64.pl masm > ms\uptable.asm
 ml64 -c -Foms\uptable.obj ms\uptable.asm
 
 :proceed
-perl util\mk1mf.pl VC-WIN64A >ms\nt.mak
+perl util\mk1mf.pl no-cast no-err no-bf no-sctp no-rsax VC-WIN64A >ms\nt.mak
 perl util\mk1mf.pl dll VC-WIN64A >ms\ntdll.mak
 
 perl util\mkdef.pl 32 libeay > ms\libeay32.def
