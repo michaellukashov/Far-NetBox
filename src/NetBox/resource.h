fcaa7199b885cd58fe9f9980e44daf4cf3199484
Iouri Kharon <bc-info@styx.cabel.net>
2021-09-17 17:49:06 +0300

    1. bugfix: FTP hang when panel close immediate after download 2. normalise plugin resource build (change only resource.h needed)

--- src/NetBox/resource.h
+++ src/NetBox/resource.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#define PLUGIN_VERSION_NUM 2,4,5,536
+#define PLUGIN_VERSION_NUM 2,4,5,537
+#define PLUGIN_VERSION_STR "2.4.5.537"
 #define PLUGIN_VERSION_TXT "2.4.5"
 #define PLUGIN_VERSION_WTXT L"2.4.5"
 
