2587cf74828a95c07ee3b4f0f8782aae5c6f7ecc
Iouri Kharon <bc-info@styx.cabel.net>
2021-09-18 11:25:06 +0300

    tune previous commit (normalise plugin resource build -- change only plugin_version.hpp) (Thanks to Виктор)

--- src/NetBox/resource.h
+++ src/NetBox/resource.h
@@ -1,9 +1,23 @@
 #pragma once
 
-#define PLUGIN_VERSION_NUM 2,4,5,537
-#define PLUGIN_VERSION_STR "2.4.5.537"
-#define PLUGIN_VERSION_TXT "2.4.5"
-#define PLUGIN_VERSION_WTXT L"2.4.5"
+#include "plugin_version.hpp"
+
+#define _MAJOR NETBOX_VERSION_MAJOR
+#define _MINOR NETBOX_VERSION_MINOR
+#define _PATCH NETBOX_VERSION_PATCH
+#define _BUILD NETBOX_VERSION_BUILD
+
+#define PLUGIN_VERSION_NUM   _MAJOR,_MINOR,_PATCH,_BUILD
+
+#define _NTOA(n) #n
+#define _NTOA1(v)         _NTOA(v)
+#define _NTOA2(v,r)       _NTOA1(v)   "." _NTOA1(r)
+#define _NTOA3(v,r,b)     _NTOA2(v,r) "." _NTOA1(b)
+#define _NTOA4(v,s,r,b)   _NTOA2(v,s) "." _NTOA2(r,b)
+
+#define PLUGIN_VERSION_STR _NTOA4(_MAJOR,_MINOR,_PATCH,_BUILD)
+#define PLUGIN_VERSION_TXT _NTOA3(_MAJOR,_MINOR,_PATCH)
+//#define PLUGIN_VERSION_WTXT L"2.4.5"
 
 #define PLUGIN_AUTHOR       L"Michael Lukashov"
 #define PLUGIN_DESCR        L"SFTP/FTP(S)/SCP/WebDAV client for FAR 3.0 x86/x64"
