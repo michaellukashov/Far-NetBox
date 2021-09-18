2f606e71e6006003120bf6aef87452a85b13769b
Iouri Kharon <bc-info@styx.cabel.net>
2021-09-18 11:19:26 +0300

    tune previous commit (normalise plugin resource build -- change only plugin_version.hpp)

--- src/NetBox/plugin_version.hpp
+++ src/NetBox/plugin_version.hpp
@@ -7,5 +7,5 @@
 #define NETBOX_VERSION_MAJOR         2
 #define NETBOX_VERSION_MINOR         4
 #define NETBOX_VERSION_PATCH         5
-#define NETBOX_VERSION_BUILD         536
+#define NETBOX_VERSION_BUILD         537
 
