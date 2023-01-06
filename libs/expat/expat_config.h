b1193e6c1b3d40ae62ad52ae0726e02b3d257a56
Mikhail Lukashov <michael.lukashov@gmail.com>
2023-01-07 01:40:50 +0300

    fix expat_config.h

--- libs/expat/expat_config.h
+++ libs/expat/expat_config.h
@@ -15,7 +15,7 @@
 /* #undef HAVE_ARC4RANDOM */
 
 /* Define to 1 if you have the `arc4random_buf' function. */
-#define HAVE_ARC4RANDOM_BUF 1
+// #define HAVE_ARC4RANDOM_BUF 1
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
 #define HAVE_DLFCN_H 1
