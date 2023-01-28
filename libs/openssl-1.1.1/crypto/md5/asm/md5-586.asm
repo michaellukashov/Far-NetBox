58f69f272e4fd79c5830a2e51ba2f479aede3a25
Mikhail Lukashov <michael.lukashov@gmail.com>
2023-01-28 11:41:36 +0300

    openssl: add x86 .asm files

--- libs/openssl-1.1.1/crypto/md5/asm/md5-586.asm
+++ libs/openssl-1.1.1/crypto/md5/asm/md5-586.asm
@@ -1,8 +1,8 @@
 %ifidn __OUTPUT_FORMAT__,obj
-section	code	use32 class=code align=256
+section	code	use32 class=code align=64
 %elifidn __OUTPUT_FORMAT__,win32
 $@feat.00 equ 1
-section	.text	code align=256
+section	.text	code align=64
 %else
 section	.text	code
 %endif
@@ -35,319 +35,319 @@ L$000start:
 	and	edi,ebx
 	lea	eax,[3614090360+ebp*1+eax]
 	xor	edi,edx
-	add	eax,edi
-	mov	edi,ebx
-	rol	eax,7
 	mov	ebp,DWORD [4+esi]
+	add	eax,edi
+	rol	eax,7
+	mov	edi,ebx
 	add	eax,ebx
 	; R0 1
 	xor	edi,ecx
 	and	edi,eax
 	lea	edx,[3905402710+ebp*1+edx]
 	xor	edi,ecx
-	add	edx,edi
-	mov	edi,eax
-	rol	edx,12
 	mov	ebp,DWORD [8+esi]
+	add	edx,edi
+	rol	edx,12
+	mov	edi,eax
 	add	edx,eax
 	; R0 2
 	xor	edi,ebx
 	and	edi,edx
 	lea	ecx,[606105819+ebp*1+ecx]
 	xor	edi,ebx
-	add	ecx,edi
-	mov	edi,edx
-	rol	ecx,17
 	mov	ebp,DWORD [12+esi]
+	add	ecx,edi
+	rol	ecx,17
+	mov	edi,edx
 	add	ecx,edx
 	; R0 3
 	xor	edi,eax
 	and	edi,ecx
 	lea	ebx,[3250441966+ebp*1+ebx]
 	xor	edi,eax
-	add	ebx,edi
-	mov	edi,ecx
-	rol	ebx,22
 	mov	ebp,DWORD [16+esi]
+	add	ebx,edi
+	rol	ebx,22
+	mov	edi,ecx
 	add	ebx,ecx
 	; R0 4
 	xor	edi,edx
 	and	edi,ebx
 	lea	eax,[4118548399+ebp*1+eax]
 	xor	edi,edx
-	add	eax,edi
-	mov	edi,ebx
-	rol	eax,7
 	mov	ebp,DWORD [20+esi]
+	add	eax,edi
+	rol	eax,7
+	mov	edi,ebx
 	add	eax,ebx
 	; R0 5
 	xor	edi,ecx
 	and	edi,eax
 	lea	edx,[1200080426+ebp*1+edx]
 	xor	edi,ecx
-	add	edx,edi
-	mov	edi,eax
-	rol	edx,12
 	mov	ebp,DWORD [24+esi]
+	add	edx,edi
+	rol	edx,12
+	mov	edi,eax
 	add	edx,eax
 	; R0 6
 	xor	edi,ebx
 	and	edi,edx
 	lea	ecx,[2821735955+ebp*1+ecx]
 	xor	edi,ebx
-	add	ecx,edi
-	mov	edi,edx
-	rol	ecx,17
 	mov	ebp,DWORD [28+esi]
+	add	ecx,edi
+	rol	ecx,17
+	mov	edi,edx
 	add	ecx,edx
 	; R0 7
 	xor	edi,eax
 	and	edi,ecx
 	lea	ebx,[4249261313+ebp*1+ebx]
 	xor	edi,eax
-	add	ebx,edi
-	mov	edi,ecx
-	rol	ebx,22
 	mov	ebp,DWORD [32+esi]
+	add	ebx,edi
+	rol	ebx,22
+	mov	edi,ecx
 	add	ebx,ecx
 	; R0 8
 	xor	edi,edx
 	and	edi,ebx
 	lea	eax,[1770035416+ebp*1+eax]
 	xor	edi,edx
-	add	eax,edi
-	mov	edi,ebx
-	rol	eax,7
 	mov	ebp,DWORD [36+esi]
+	add	eax,edi
+	rol	eax,7
+	mov	edi,ebx
 	add	eax,ebx
 	; R0 9
 	xor	edi,ecx
 	and	edi,eax
 	lea	edx,[2336552879+ebp*1+edx]
 	xor	edi,ecx
-	add	edx,edi
-	mov	edi,eax
-	rol	edx,12
 	mov	ebp,DWORD [40+esi]
+	add	edx,edi
+	rol	edx,12
+	mov	edi,eax
 	add	edx,eax
 	; R0 10
 	xor	edi,ebx
 	and	edi,edx
 	lea	ecx,[4294925233+ebp*1+ecx]
 	xor	edi,ebx
-	add	ecx,edi
-	mov	edi,edx
-	rol	ecx,17
 	mov	ebp,DWORD [44+esi]
+	add	ecx,edi
+	rol	ecx,17
+	mov	edi,edx
 	add	ecx,edx
 	; R0 11
 	xor	edi,eax
 	and	edi,ecx
 	lea	ebx,[2304563134+ebp*1+ebx]
 	xor	edi,eax
-	add	ebx,edi
-	mov	edi,ecx
-	rol	ebx,22
 	mov	ebp,DWORD [48+esi]
+	add	ebx,edi
+	rol	ebx,22
+	mov	edi,ecx
 	add	ebx,ecx
 	; R0 12
 	xor	edi,edx
 	and	edi,ebx
 	lea	eax,[1804603682+ebp*1+eax]
 	xor	edi,edx
-	add	eax,edi
-	mov	edi,ebx
-	rol	eax,7
 	mov	ebp,DWORD [52+esi]
+	add	eax,edi
+	rol	eax,7
+	mov	edi,ebx
 	add	eax,ebx
 	; R0 13
 	xor	edi,ecx
 	and	edi,eax
 	lea	edx,[4254626195+ebp*1+edx]
 	xor	edi,ecx
-	add	edx,edi
-	mov	edi,eax
-	rol	edx,12
 	mov	ebp,DWORD [56+esi]
+	add	edx,edi
+	rol	edx,12
+	mov	edi,eax
 	add	edx,eax
 	; R0 14
 	xor	edi,ebx
 	and	edi,edx
 	lea	ecx,[2792965006+ebp*1+ecx]
 	xor	edi,ebx
-	add	ecx,edi
-	mov	edi,edx
-	rol	ecx,17
 	mov	ebp,DWORD [60+esi]
+	add	ecx,edi
+	rol	ecx,17
+	mov	edi,edx
 	add	ecx,edx
 	; R0 15
 	xor	edi,eax
 	and	edi,ecx
 	lea	ebx,[1236535329+ebp*1+ebx]
 	xor	edi,eax
-	add	ebx,edi
-	mov	edi,ecx
-	rol	ebx,22
 	mov	ebp,DWORD [4+esi]
+	add	ebx,edi
+	rol	ebx,22
+	mov	edi,ecx
 	add	ebx,ecx
 	; 
 	; R1 section
 	; R1 16
-	lea	eax,[4129170786+ebp*1+eax]
 	xor	edi,ebx
 	and	edi,edx
-	mov	ebp,DWORD [24+esi]
+	lea	eax,[4129170786+ebp*1+eax]
 	xor	edi,ecx
+	mov	ebp,DWORD [24+esi]
 	add	eax,edi
 	mov	edi,ebx
 	rol	eax,5
 	add	eax,ebx
 	; R1 17
-	lea	edx,[3225465664+ebp*1+edx]
 	xor	edi,eax
 	and	edi,ecx
-	mov	ebp,DWORD [44+esi]
+	lea	edx,[3225465664+ebp*1+edx]
 	xor	edi,ebx
+	mov	ebp,DWORD [44+esi]
 	add	edx,edi
 	mov	edi,eax
 	rol	edx,9
 	add	edx,eax
 	; R1 18
-	lea	ecx,[643717713+ebp*1+ecx]
 	xor	edi,edx
 	and	edi,ebx
-	mov	ebp,DWORD [esi]
+	lea	ecx,[643717713+ebp*1+ecx]
 	xor	edi,eax
+	mov	ebp,DWORD [esi]
 	add	ecx,edi
 	mov	edi,edx
 	rol	ecx,14
 	add	ecx,edx
 	; R1 19
-	lea	ebx,[3921069994+ebp*1+ebx]
 	xor	edi,ecx
 	and	edi,eax
-	mov	ebp,DWORD [20+esi]
+	lea	ebx,[3921069994+ebp*1+ebx]
 	xor	edi,edx
+	mov	ebp,DWORD [20+esi]
 	add	ebx,edi
 	mov	edi,ecx
 	rol	ebx,20
 	add	ebx,ecx
 	; R1 20
-	lea	eax,[3593408605+ebp*1+eax]
 	xor	edi,ebx
 	and	edi,edx
-	mov	ebp,DWORD [40+esi]
+	lea	eax,[3593408605+ebp*1+eax]
 	xor	edi,ecx
+	mov	ebp,DWORD [40+esi]
 	add	eax,edi
 	mov	edi,ebx
 	rol	eax,5
 	add	eax,ebx
 	; R1 21
-	lea	edx,[38016083+ebp*1+edx]
 	xor	edi,eax
 	and	edi,ecx
-	mov	ebp,DWORD [60+esi]
+	lea	edx,[38016083+ebp*1+edx]
 	xor	edi,ebx
+	mov	ebp,DWORD [60+esi]
 	add	edx,edi
 	mov	edi,eax
 	rol	edx,9
 	add	edx,eax
 	; R1 22
-	lea	ecx,[3634488961+ebp*1+ecx]
 	xor	edi,edx
 	and	edi,ebx
-	mov	ebp,DWORD [16+esi]
+	lea	ecx,[3634488961+ebp*1+ecx]
 	xor	edi,eax
+	mov	ebp,DWORD [16+esi]
 	add	ecx,edi
 	mov	edi,edx
 	rol	ecx,14
 	add	ecx,edx
 	; R1 23
-	lea	ebx,[3889429448+ebp*1+ebx]
 	xor	edi,ecx
 	and	edi,eax
-	mov	ebp,DWORD [36+esi]
+	lea	ebx,[3889429448+ebp*1+ebx]
 	xor	edi,edx
+	mov	ebp,DWORD [36+esi]
 	add	ebx,edi
 	mov	edi,ecx
 	rol	ebx,20
 	add	ebx,ecx
 	; R1 24
-	lea	eax,[568446438+ebp*1+eax]
 	xor	edi,ebx
 	and	edi,edx
-	mov	ebp,DWORD [56+esi]
+	lea	eax,[568446438+ebp*1+eax]
 	xor	edi,ecx
+	mov	ebp,DWORD [56+esi]
 	add	eax,edi
 	mov	edi,ebx
 	rol	eax,5
 	add	eax,ebx
 	; R1 25
-	lea	edx,[3275163606+ebp*1+edx]
 	xor	edi,eax
 	and	edi,ecx
-	mov	ebp,DWORD [12+esi]
+	lea	edx,[3275163606+ebp*1+edx]
 	xor	edi,ebx
+	mov	ebp,DWORD [12+esi]
 	add	edx,edi
 	mov	edi,eax
 	rol	edx,9
 	add	edx,eax
 	; R1 26
-	lea	ecx,[4107603335+ebp*1+ecx]
 	xor	edi,edx
 	and	edi,ebx
-	mov	ebp,DWORD [32+esi]
+	lea	ecx,[4107603335+ebp*1+ecx]
 	xor	edi,eax
+	mov	ebp,DWORD [32+esi]
 	add	ecx,edi
 	mov	edi,edx
 	rol	ecx,14
 	add	ecx,edx
 	; R1 27
-	lea	ebx,[1163531501+ebp*1+ebx]
 	xor	edi,ecx
 	and	edi,eax
-	mov	ebp,DWORD [52+esi]
+	lea	ebx,[1163531501+ebp*1+ebx]
 	xor	edi,edx
+	mov	ebp,DWORD [52+esi]
 	add	ebx,edi
 	mov	edi,ecx
 	rol	ebx,20
 	add	ebx,ecx
 	; R1 28
-	lea	eax,[2850285829+ebp*1+eax]
 	xor	edi,ebx
 	and	edi,edx
-	mov	ebp,DWORD [8+esi]
+	lea	eax,[2850285829+ebp*1+eax]
 	xor	edi,ecx
+	mov	ebp,DWORD [8+esi]
 	add	eax,edi
 	mov	edi,ebx
 	rol	eax,5
 	add	eax,ebx
 	; R1 29
-	lea	edx,[4243563512+ebp*1+edx]
 	xor	edi,eax
 	and	edi,ecx
-	mov	ebp,DWORD [28+esi]
+	lea	edx,[4243563512+ebp*1+edx]
 	xor	edi,ebx
+	mov	ebp,DWORD [28+esi]
 	add	edx,edi
 	mov	edi,eax
 	rol	edx,9
 	add	edx,eax
 	; R1 30
-	lea	ecx,[1735328473+ebp*1+ecx]
 	xor	edi,edx
 	and	edi,ebx
-	mov	ebp,DWORD [48+esi]
+	lea	ecx,[1735328473+ebp*1+ecx]
 	xor	edi,eax
+	mov	ebp,DWORD [48+esi]
 	add	ecx,edi
 	mov	edi,edx
 	rol	ecx,14
 	add	ecx,edx
 	; R1 31
-	lea	ebx,[2368359562+ebp*1+ebx]
 	xor	edi,ecx
 	and	edi,eax
-	mov	ebp,DWORD [20+esi]
+	lea	ebx,[2368359562+ebp*1+ebx]
 	xor	edi,edx
+	mov	ebp,DWORD [20+esi]
 	add	ebx,edi
 	mov	edi,ecx
 	rol	ebx,20
@@ -359,13 +359,13 @@ L$000start:
 	xor	edi,ebx
 	lea	eax,[4294588738+ebp*1+eax]
 	add	eax,edi
-	rol	eax,4
 	mov	ebp,DWORD [32+esi]
+	rol	eax,4
 	mov	edi,ebx
 	; R2 33
-	lea	edx,[2272392833+ebp*1+edx]
 	add	eax,ebx
 	xor	edi,ecx
+	lea	edx,[2272392833+ebp*1+edx]
 	xor	edi,eax
 	mov	ebp,DWORD [44+esi]
 	add	edx,edi
@@ -377,13 +377,13 @@ L$000start:
 	xor	edi,edx
 	lea	ecx,[1839030562+ebp*1+ecx]
 	add	ecx,edi
-	rol	ecx,16
 	mov	ebp,DWORD [56+esi]
+	rol	ecx,16
 	mov	edi,edx
 	; R2 35
-	lea	ebx,[4259657740+ebp*1+ebx]
 	add	ecx,edx
 	xor	edi,eax
+	lea	ebx,[4259657740+ebp*1+ebx]
 	xor	edi,ecx
 	mov	ebp,DWORD [4+esi]
 	add	ebx,edi
@@ -395,13 +395,13 @@ L$000start:
 	xor	edi,ebx
 	lea	eax,[2763975236+ebp*1+eax]
 	add	eax,edi
-	rol	eax,4
 	mov	ebp,DWORD [16+esi]
+	rol	eax,4
 	mov	edi,ebx
 	; R2 37
-	lea	edx,[1272893353+ebp*1+edx]
 	add	eax,ebx
 	xor	edi,ecx
+	lea	edx,[1272893353+ebp*1+edx]
 	xor	edi,eax
 	mov	ebp,DWORD [28+esi]
 	add	edx,edi
@@ -413,13 +413,13 @@ L$000start:
 	xor	edi,edx
 	lea	ecx,[4139469664+ebp*1+ecx]
 	add	ecx,edi
-	rol	ecx,16
 	mov	ebp,DWORD [40+esi]
+	rol	ecx,16
 	mov	edi,edx
 	; R2 39
-	lea	ebx,[3200236656+ebp*1+ebx]
 	add	ecx,edx
 	xor	edi,eax
+	lea	ebx,[3200236656+ebp*1+ebx]
 	xor	edi,ecx
 	mov	ebp,DWORD [52+esi]
 	add	ebx,edi
@@ -431,13 +431,13 @@ L$000start:
 	xor	edi,ebx
 	lea	eax,[681279174+ebp*1+eax]
 	add	eax,edi
-	rol	eax,4
 	mov	ebp,DWORD [esi]
+	rol	eax,4
 	mov	edi,ebx
 	; R2 41
-	lea	edx,[3936430074+ebp*1+edx]
 	add	eax,ebx
 	xor	edi,ecx
+	lea	edx,[3936430074+ebp*1+edx]
 	xor	edi,eax
 	mov	ebp,DWORD [12+esi]
 	add	edx,edi
@@ -449,13 +449,13 @@ L$000start:
 	xor	edi,edx
 	lea	ecx,[3572445317+ebp*1+ecx]
 	add	ecx,edi
-	rol	ecx,16
 	mov	ebp,DWORD [24+esi]
+	rol	ecx,16
 	mov	edi,edx
 	; R2 43
-	lea	ebx,[76029189+ebp*1+ebx]
 	add	ecx,edx
 	xor	edi,eax
+	lea	ebx,[76029189+ebp*1+ebx]
 	xor	edi,ecx
 	mov	ebp,DWORD [36+esi]
 	add	ebx,edi
@@ -467,13 +467,13 @@ L$000start:
 	xor	edi,ebx
 	lea	eax,[3654602809+ebp*1+eax]
 	add	eax,edi
-	rol	eax,4
 	mov	ebp,DWORD [48+esi]
+	rol	eax,4
 	mov	edi,ebx
 	; R2 45
-	lea	edx,[3873151461+ebp*1+edx]
 	add	eax,ebx
 	xor	edi,ecx
+	lea	edx,[3873151461+ebp*1+edx]
 	xor	edi,eax
 	mov	ebp,DWORD [60+esi]
 	add	edx,edi
@@ -485,13 +485,13 @@ L$000start:
 	xor	edi,edx
 	lea	ecx,[530742520+ebp*1+ecx]
 	add	ecx,edi
-	rol	ecx,16
 	mov	ebp,DWORD [8+esi]
+	rol	ecx,16
 	mov	edi,edx
 	; R2 47
-	lea	ebx,[3299628645+ebp*1+ebx]
 	add	ecx,edx
 	xor	edi,eax
+	lea	ebx,[3299628645+ebp*1+ebx]
 	xor	edi,ecx
 	mov	ebp,DWORD [esi]
 	add	ebx,edi
