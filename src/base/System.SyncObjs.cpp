4f45cae25fe39e631c6de35fadebb418e0231de6
Mikhail Lukashov <michael.lukashov@gmail.com>
2023-02-25 06:06:34 +0300

    System.SyncObjs.cpp: use TINYLOG_TRACE_ENTER

--- src/base/System.SyncObjs.cpp
+++ src/base/System.SyncObjs.cpp
@@ -1,16 +1,21 @@
 #include <vcl.h>
 
+#include <tinylog/TinyLog.h>
 #include "System.SyncObjs.hpp"
 
 // TCriticalSection
 
 TCriticalSection::TCriticalSection() noexcept
 {
+  //OutputDebugStringA(LOCATION_STR.c_str());
+  //TINYLOG_TRACE_ENTER;
   InitializeCriticalSection(&FSection);
 }
 
 TCriticalSection::~TCriticalSection() noexcept
 {
+//  DEBUG_PRINTF("FAcquired: %d", FAcquired);
+//  OutputDebugStringA(LOCATION_STR.c_str());
   DebugAssert(FAcquired == 0);
   DeleteCriticalSection(&FSection);
 }
@@ -19,10 +24,12 @@ void TCriticalSection::Enter() const
 {
   ::EnterCriticalSection(&FSection);
   ++FAcquired;
+//  DEBUG_PRINTF("FAcquired: %d", FAcquired);
 }
 
 void TCriticalSection::Leave() const
 {
   --FAcquired;
+//  DEBUG_PRINTF("FAcquired: %d", FAcquired);
   ::LeaveCriticalSection(&FSection);
 }
