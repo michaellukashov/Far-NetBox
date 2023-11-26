48fcd875df05c17709a8ab613a0b30c261e25f0d
Mikhail Lukashov <michael.lukashov@gmail.com>
2023-11-26 16:28:09 +0300

    const RawByteString &

--- src/base/WideStrUtils.cpp
+++ src/base/WideStrUtils.cpp
@@ -3,7 +3,7 @@
 
 #include "WideStrUtils.hpp"
 
-nb::TEncodeType DetectUTF8Encoding(RawByteString S)
+nb::TEncodeType DetectUTF8Encoding(const RawByteString & S)
 {
   return nb::DetectUTF8Encoding(reinterpret_cast<const uint8_t *>(S.c_str()), S.Length());
 }
