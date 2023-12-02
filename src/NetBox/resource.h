cc719aabff5e32ee60248ef73acdc5dfd5473bcd
Mikhail Lukashov <michael.lukashov@gmail.com>
2023-12-02 09:24:13 +0300

    upd resource.h

--- src/NetBox/resource.h
+++ src/NetBox/resource.h
@@ -11,6 +11,6 @@
 #define WINSCP_PLUGIN_VERSION_WTXT  L"1.6.2.151"
 #define PUTTY_VERSION_WTXT          L"0.79"
 #define FILEZILLA_VERSION_WTXT      L"2.2.32"
-#define ZLIB_VERSION_WTXT           L"zlib-ng 2.1.4.devel"
+#define ZLIB_VERSION_WTXT           L"zlib-ng 2.1.5"
 #define OPENSSL_VERSION_WTXT        L"3.1.4"
 #define WINSCP_VERSION_WTXT         L"6.2.0"
