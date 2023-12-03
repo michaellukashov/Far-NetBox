f74910dcad68e3e68f610a6e0b2c096d23e3a043
Mikhail Lukashov <michael.lukashov@gmail.com>
2023-12-03 10:36:51 +0300

    ToInt32

--- src/base/FormatUtils.cpp
+++ src/base/FormatUtils.cpp
@@ -9,14 +9,14 @@ UnicodeString Format(const UnicodeString & fmt, fmt::ArgList args)
 {
   fmt::WMemoryWriter w;
   w.write(fmt.data(), args);
-  return UnicodeString(w.data(), ToIntPtr(w.size()));
+  return UnicodeString(w.data(), nb::ToInt32(w.size()));
 }
 
 UnicodeString Sprintf(const UnicodeString & fmt, fmt::ArgList args)
 {
   fmt::WMemoryWriter w;
   fmt::printf(w, fmt.data(), args);
-  return UnicodeString(w.data(), ToIntPtr(w.size()));
+  return UnicodeString(w.data(), nb::ToInt32(w.size()));
 }
 
 UnicodeString FmtLoadStr(int32_t id, fmt::ArgList args)
