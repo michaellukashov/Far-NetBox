ed8536e9cb85b0d4b21df4f792c441a694e0cace
Mikhail Lukashov <michael.lukashov@gmail.com>
2023-12-04 21:58:56 +0300

    code style

--- src/NetBox/FarInterface.h
+++ src/NetBox/FarInterface.h
@@ -21,9 +21,9 @@ TFarDialog * CreateMoreMessageDialog(const UnicodeString & Msg,
   const TQueryButtonAlias * Aliases, uintptr_t AliasesCount,
   uintptr_t TimeoutAnswer, TFarButton ** TimeoutButton,
   const UnicodeString & ImageName, const UnicodeString & NeverAskAgainCaption,
-  const UnicodeString & MoreMessagesUrl, int MoreMessagesSize,
+  const UnicodeString & MoreMessagesUrl, int32_t MoreMessagesSize,
   const UnicodeString & CustomCaption);
-TFarDialog *CreateMoreMessageDialogEx(const UnicodeString & Message, TStrings * MoreMessages,
+TFarDialog * CreateMoreMessageDialogEx(const UnicodeString & Message, TStrings * MoreMessages,
   TQueryType Type, uint32_t Answers, const UnicodeString & HelpKeyword, const TMessageParams * Params);
 uintptr_t ExecuteMessageDialog(TFarDialog * Dialog, uint32_t Answers, const TMessageParams * Params);
 #if 0
@@ -31,5 +31,5 @@ void InsertPanelToMessageDialog(TFarDialog * Form, TPanel * Panel);
 #endif //#if 0
 
 //from windows/GUITools.h
-void ValidateMaskEdit(TFarComboBox *Edit);
-void ValidateMaskEdit(TFarEdit *Edit);
+void ValidateMaskEdit(TFarComboBox * Edit);
+void ValidateMaskEdit(TFarEdit * Edit);
