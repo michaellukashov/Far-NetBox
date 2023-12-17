d6f04657007cbc56e0366833d307af014cb00bf9
Mikhail Lukashov <michael.lukashov@gmail.com>
2023-12-17 13:49:40 +0300

    Sysutils: use do() while(0)

--- src/base/Sysutils.hpp
+++ src/base/Sysutils.hpp
@@ -3,10 +3,10 @@
 #include <Global.h>
 #include <Classes.hpp>
 
-#define THROWOSIFFALSE(C) if (!(C)) { ::RaiseLastOSError(); }
-#define SAFE_DESTROY_EX(CLASS, OBJ) if ((OBJ)) { CLASS * PObj = (OBJ); (OBJ) = nullptr; delete PObj; }
+#define THROWOSIFFALSE(C) do { if (!(C)) ::RaiseLastOSError(); } while(0)
+#define SAFE_DESTROY_EX(CLASS, OBJ) do { if ((OBJ)) { CLASS * PObj = (OBJ); (OBJ) = nullptr; delete PObj; } } while(0)
 #define SAFE_DESTROY(OBJ) SAFE_DESTROY_EX(TObject, (OBJ))
-#define SAFE_CLOSE_HANDLE(H) if ((H) && (H) != INVALID_HANDLE_VALUE) { HANDLE HH = (H); (H) = nullptr; if (HH != nullptr) { ::CloseHandle(HH); } }
+#define SAFE_CLOSE_HANDLE(H) do { if ((H) && (H) != INVALID_HANDLE_VALUE) { HANDLE HH = (H); (H) = nullptr; if (HH != nullptr) { ::CloseHandle(HH); } } } while (0)
 #define NULL_TERMINATE(S) S[LENOF(S) - 1] = L'\0'
 
 #define SWAP(TYPE, FIRST, SECOND) \
