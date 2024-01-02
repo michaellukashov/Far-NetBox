e9c4112a88c4de0d7882d24650eb2b2ce67cfb8e
Mikhail Lukashov <michael.lukashov@gmail.com>
2024-01-02 03:28:10 +0300

    CopyDialog

--- src/NetBox/WinSCPDialogs.cpp
+++ src/NetBox/WinSCPDialogs.cpp
@@ -5630,7 +5630,7 @@ bool TCopyDialog::Execute(UnicodeString & TargetDirectory,
       UnicodeString NewTargetDirectory;
       if (FToRemote)
       {
-        Params->SetFileMask(base::UnixExtractFileName(DirectoryEdit->GetText()));
+        // Params->SetFileMask(base::UnixExtractFileName(DirectoryEdit->GetText()));
         NewTargetDirectory = base::UnixExtractFilePath(DirectoryEdit->GetText());
         if (!NewTargetDirectory.IsEmpty())
           TargetDirectory = NewTargetDirectory;
