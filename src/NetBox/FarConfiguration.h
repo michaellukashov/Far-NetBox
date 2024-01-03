c93f49db4ce0a5e00785e05970b39791fe6044ec
Mikhail Lukashov <michael.lukashov@gmail.com>
2024-01-03 06:02:01 +0300

    FarConfiguration

--- src/NetBox/FarConfiguration.h
+++ src/NetBox/FarConfiguration.h
@@ -7,7 +7,7 @@ class TCustomFarPlugin;
 class TBookmarks;
 class TBookmarkList;
 
-// SetCommandPrefixes("netbox,ftp,scp,sftp,ftps,http,https,webdav,s3");
+// DefaultCommandPrefixes("netbox,ftp,scp,sftp,ftps,http,https,webdav,s3");
 constexpr const wchar_t * DefaultCommandPrefixes = L"netbox,ftp,scp,sftp,ftps,webdav,s3";
 
 // NB_DEFINE_CLASS_ID(TFarConfiguration);
