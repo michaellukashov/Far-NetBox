919b2bbc3814ebc94c9975712e6bc9ffbdaaadf1
Mikhail Lukashov <michael.lukashov@gmail.com>
2024-01-05 23:58:59 +0300

    TWinSCPFileSystem::Connect: show reopen query if connect failed

--- src/NetBox/WinSCPFileSystem.cpp
+++ src/NetBox/WinSCPFileSystem.cpp
@@ -3021,17 +3021,24 @@ bool TWinSCPFileSystem::Connect(TSessionData * Data)
   }
   catch(Exception & E)
   {
+    // HandleException(&E);
+    bool Reopen = false;
     EFatal * Fatal = rtti::dyn_cast_or_null<EFatal>(&E);
     if ((Fatal == nullptr) || !Fatal->GetReopenQueried())
     {
-      FTerminal->ShowExtendedException(&E);
+      // FTerminal->ShowExtendedException(&E);
+      Reopen = FTerminal->QueryReopen(&E, 0, nullptr);
     }
-    SAFE_DESTROY(FTerminal);
-    SAFE_DESTROY(FQueue);
-    SAFE_DESTROY(FQueueStatus);
+    Result = Reopen && FTerminal && FTerminal->GetActive();
+    if (!Result)
+    {
+      SAFE_DESTROY(FTerminal);
+      SAFE_DESTROY(FQueue);
+      SAFE_DESTROY(FQueueStatus);
+    }
   }
 
-  if (FTerminal != nullptr)
+  if (FTerminal != nullptr && FTerminal->GetActive())
   {
     FSynchronisingBrowse = GetSessionData()->GetSynchronizeBrowsing();
   }
