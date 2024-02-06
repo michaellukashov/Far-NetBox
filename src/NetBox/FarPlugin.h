c94a8e8ec5ecd37065c03c3f55b8299ceef6c23f
Mikhail Lukashov <michael.lukashov@gmail.com>
2024-02-06 04:25:56 +0300

    add SHORTCUT_DELIMITER

--- src/NetBox/FarPlugin.h
+++ src/NetBox/FarPlugin.h
@@ -16,6 +16,7 @@ constexpr const DWORD RMASK = (RIGHT_ALT_PRESSED | LEFT_ALT_PRESSED | RIGHT_CTRL
 constexpr const DWORD ALTMASK = (RIGHT_ALT_PRESSED | LEFT_ALT_PRESSED);
 constexpr const DWORD CTRLMASK = (RIGHT_CTRL_PRESSED | LEFT_CTRL_PRESSED);
 constexpr const DWORD SHIFTMASK = (SHIFT_PRESSED);
+constexpr const wchar_t * SHORTCUT_DELIMITER = L"\1";
 
 class TCustomFarFileSystem;
 class TFarPanelModes;
