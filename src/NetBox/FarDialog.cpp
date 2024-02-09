b84357106b70d7caaa0f0d0b3fee0f4cc398f9b3
ssvine <79405160+ssvine@users.noreply.github.com>
2024-02-09 05:03:13 +0300

    Fix TFarList::GetFlags to use real flags

--- src/NetBox/FarDialog.cpp
+++ src/NetBox/FarDialog.cpp
@@ -2413,7 +2413,19 @@ int32_t TFarList::GetSelected() const
 
 LISTITEMFLAGS TFarList::GetFlags(int32_t Index) const
 {
-  return FListItems->Items[Index].Flags;
+  DebugAssert(Index >= 0 && Index < FListItems->ItemsNumber);
+  if (GetDialogItem() == nullptr || !GetDialogItem()->GetDialog()->GetHandle())
+  {
+    return FListItems->Items[Index].Flags;
+  }
+  else
+  {
+    FarListGetItem List;
+    List.StructSize = sizeof(FarListGetItem);
+    List.ItemIndex = Index;
+    bool Result = GetDialogItem()->SendDialogMessage(DM_LISTGETITEM, nb::ToPtr(&List));
+    return Result ? List.Item.Flags : LIF_NONE;
+  }
 }
 
 void TFarList::SetFlags(int32_t Index, LISTITEMFLAGS Value)
