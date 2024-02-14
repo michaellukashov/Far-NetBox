9418404b990712ca23fa1dc9a78b9a9ea4fcb421
Mikhail Lukashov <michael.lukashov@gmail.com>
2024-02-14 04:12:47 +0300

    use gsl::not_null

--- src/NetBox/FarPlugin.cpp
+++ src/NetBox/FarPlugin.cpp
@@ -860,7 +860,7 @@ private:
 
 private:
   bool FCheckBoxChecked{false};
-  TFarMessageParams * FParams{nullptr};
+  gsl::not_null<TFarMessageParams *> FParams;
   TDateTime FStartTime;
   TDateTime FLastTimerTime;
   TFarButton * FTimeoutButton{nullptr};
