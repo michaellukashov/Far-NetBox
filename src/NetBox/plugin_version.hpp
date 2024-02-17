39192eccdfce5533897730fbf06fd04c5009d7fe
Mikhail Lukashov <michael.lukashov@gmail.com>
2024-02-17 10:47:45 +0300

    bump version to 24.2.1.588

--- src/NetBox/plugin_version.hpp
+++ src/NetBox/plugin_version.hpp
@@ -7,4 +7,4 @@
 #define NETBOX_VERSION_MAJOR         24
 #define NETBOX_VERSION_MINOR         2
 #define NETBOX_VERSION_PATCH         1
-#define NETBOX_VERSION_BUILD         587
+#define NETBOX_VERSION_BUILD         588
