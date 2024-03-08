4588e42b659535ddec13818f6979730c25ca69f9
Mikhail Lukashov <michael.lukashov@gmail.com>
2024-03-08 19:53:47 +0300

    Fix warnings: There might be dereferencing of a potential null pointer 'S'

--- src/base/Sysutils.cpp
+++ src/base/Sysutils.cpp
@@ -752,7 +752,7 @@ UnicodeString WrapText(const UnicodeString & Line, int32_t MaxWidth)
       }
 
       /* copy as many words as will fit onto the current line */
-      while (*S && (nb::StrLength(S) + 1) <= SpaceLeft)
+      while (S && *S && (nb::StrLength(S) + 1) <= SpaceLeft)
       {
         if (Result.Length() == 0)
         {
@@ -779,12 +779,12 @@ UnicodeString WrapText(const UnicodeString & Line, int32_t MaxWidth)
           S = NextWord(nullptr);
         }
       }
-      if (!*S)
+      if (S && !*S)
       {
         S = NextWord(nullptr);
       }
 
-      if (*S)
+      if (S && *S)
       {
         /* add a new line here */
         if (Result.Length() == 0)
