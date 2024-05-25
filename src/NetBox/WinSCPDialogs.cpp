14023fae2721a1fee991404552804c3f4283b7ed
ssvine <79405160+ssvine@users.noreply.github.com>
2024-05-25 17:14:22 +0300

    Fix IP protocol selection

--- src/NetBox/WinSCPDialogs.cpp
+++ src/NetBox/WinSCPDialogs.cpp
@@ -3065,7 +3065,9 @@ void TSessionDialog::UpdateControls()
   }
   SshBufferSizeCheck->SetEnabled(lSshProtocol);
   PingNullPacketButton->SetEnabled(lSshProtocol);
-  IPAutoButton->SetEnabled(lSshProtocol);
+  IPAutoButton->SetEnabled(lSshProtocol || lFtpProtocol);
+  IPv4Button->SetEnabled(lSshProtocol || lFtpProtocol);
+  IPv6Button->SetEnabled(lSshProtocol || lFtpProtocol);
 
   // SFTP tab
   SftpTab->SetEnabled(lSftpProtocol);
