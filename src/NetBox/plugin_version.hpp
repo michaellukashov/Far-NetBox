65815323c009a73fe92dd013e6fd40f6287d54f6
Mikhail Lukashov <michael.lukashov@gmail.com>
2026-05-24 14:48:05 +0300

    chore: bump version to 26.5.3.612

--- src/NetBox/plugin_version.hpp
+++ src/NetBox/plugin_version.hpp
@@ -6,5 +6,5 @@
 
 #define NETBOX_VERSION_MAJOR         26
 #define NETBOX_VERSION_MINOR         5
-#define NETBOX_VERSION_PATCH         2
-#define NETBOX_VERSION_BUILD         611
+#define NETBOX_VERSION_PATCH         3
+#define NETBOX_VERSION_BUILD         612
