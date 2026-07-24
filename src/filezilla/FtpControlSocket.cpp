b2406a843afc1861ff4943c26ee7b6f4f25e8064
Mikhail Lukashov <michael.lukashov@gmail.com>
2026-07-24 12:25:42 +0300

    fix(filezilla): remove debug log from NOOP heartbeat command

--- src/filezilla/FtpControlSocket.cpp
+++ src/filezilla/FtpControlSocket.cpp
@@ -5352,7 +5352,6 @@ void CFtpControlSocket::SendKeepAliveCommand()
   if (GetOptionVal(OPTION_HEARTBEAT_TYPE))
   {
     ShowStatus(L"Sending NOOP to keep session alive.", FZ_LOG_PROGRESS);
-    ADF("Heartbeat: Sending NOOP command");
     Send(L"NOOP");
   }
   else
